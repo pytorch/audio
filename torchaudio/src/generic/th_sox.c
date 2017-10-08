599646fcc316d4eb56cbe23b42e313bac455c648
Grant Reaber <grant.reaber@gmail.com>
2017-10-08 06:52:44 +0300

    add get_info, enhance load, remove normalization option to load

--- torchaudio/src/generic/th_sox.c
+++ torchaudio/src/generic/th_sox.c
@@ -2,28 +2,41 @@
 #define TH_GENERIC_FILE "generic/th_sox.c"
 #else
 
-void libthsox_(read_audio)(sox_format_t *fd, THTensor* tensor,
-                         int* sample_rate, size_t nsamples)
+void libthsox_(read_audio)(sox_format_t *fd, THTensor *tensor,
+                           int *sample_rate, unsigned long *total_frames, size_t offset, long nframes)
 {
-  int nchannels = fd->signal.channels;
-  long buffer_size = fd->signal.length;
-  if (buffer_size == 0) {
-    if (nsamples != -1) {
-      buffer_size = nsamples;
+  unsigned long nchannels = fd->signal.channels;
+  unsigned long _total_frames = fd->signal.length / nchannels;
+  unsigned long buffer_size;
+  if (offset >= _total_frames) {
+    buffer_size = 0;
+  } else {
+    if (nframes == -1) {
+      if (fd->signal.length == 0)
+        THError("[read_audio] Unknown length");
+      buffer_size = (_total_frames - offset) * nchannels;
     } else {
-      THError("[read_audio] Unknown length");
+      buffer_size = nframes * nchannels;
+      if ((_total_frames - offset) * nchannels < buffer_size)
+        buffer_size = (_total_frames - offset) * nchannels;
     }
   }
   *sample_rate = (int) fd->signal.rate;
+  *total_frames = _total_frames;
   int32_t *buffer = (int32_t *)malloc(sizeof(int32_t) * buffer_size);
+  if (sox_seek(fd, offset, 0) != 0) {
+    /* Note that sox_seek seems to return 0 even if you seek
+       past the end of the file. */
+    THError("[read_audio] sox_seek returned SOX_EOF");
+  }
   size_t samples_read = sox_read(fd, buffer, buffer_size);
-  if (samples_read == 0)
+  if (samples_read == 0 && buffer_size > 0)
     THError("[read_audio] Empty file or read failed in sox_read");
   // alloc tensor
-  THTensor_(resize2d)(tensor, samples_read / nchannels, nchannels );
+  THTensor_(resize2d)(tensor, samples_read / nchannels, nchannels);
   real *tensor_data = THTensor_(data)(tensor);
   // convert audio to dest tensor
-  int x,k;
+  unsigned long x,k;
   for (x=0; x<samples_read/nchannels; x++) {
     for (k=0; k<nchannels; k++) {
       *tensor_data++ = (real)buffer[x*nchannels+k];
@@ -33,23 +46,24 @@ void libthsox_(read_audio)(sox_format_t *fd, THTensor* tensor,
   free(buffer);
 }
 
-void libthsox_(read_audio_file)(const char *file_name, THTensor* tensor, int* sample_rate)
+void libthsox_(read_audio_file)(const char *file_name, THTensor *tensor, int *sample_rate,
+                                unsigned long *total_frames, size_t offset, long nframes)
 {
   // Create sox objects and read into int32_t buffer
   sox_format_t *fd;
   fd = sox_open_read(file_name, NULL, NULL, NULL);
   if (fd == NULL)
     THError("[read_audio_file] Failure to read file");
-  libthsox_(read_audio)(fd, tensor, sample_rate, -1);
+  libthsox_(read_audio)(fd, tensor, sample_rate, total_frames, offset, nframes);
   sox_close(fd);
 }
 
-void libthsox_(write_audio)(sox_format_t *fd, THTensor* src,
-			  const char *extension, int sample_rate)
+void libthsox_(write_audio)(sox_format_t *fd, THTensor *src,
+                            const char *extension, int sample_rate)
 {
   long nchannels = src->size[1];
   long nsamples = src->size[0];
-  real* data = THTensor_(data)(src);
+  real *data = THTensor_(data)(src);
 
   // convert audio to dest tensor
   int x,k;
@@ -63,8 +77,8 @@ void libthsox_(write_audio)(sox_format_t *fd, THTensor* src,
   }
 }
 
-void libthsox_(write_audio_file)(const char *file_name, THTensor* src,
-			       const char *extension, int sample_rate)
+void libthsox_(write_audio_file)(const char *file_name, THTensor *src,
+                                 const char *extension, int sample_rate)
 {
   if (THTensor_(isContiguous)(src) == 0)
     THError("[write_audio_file] Input should be contiguous tensors");
