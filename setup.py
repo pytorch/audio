d5eadbdc28a751aba10b57cbad52f90dbbf64938
Soumith Chintala <soumith@gmail.com>
2018-05-08 01:15:06 -0400

    Revert "Bring back cpp_extension rewrite of torchaudio (#45)" (#46)
    This reverts commit cfe3f29686673fecc5d4b05ab2ed1b9bca476950.

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CppExtension
+
+import build
+
+this_file = os.path.dirname(__file__)
 
 setup(
     name="torchaudio",
     version="0.1",
     description="An audio package for PyTorch",
     url="https://github.com/pytorch/audio",
-    author="Soumith Chintala, David Pollack, Sean Naren, Peter Goldsborough",
+    author="Soumith Chintala, David Pollack, Sean Naren",
     author_email="soumith@pytorch.org",
-    install_requires=["torch>=0.4"],
-    setup_requires=["torch>=0.4"],
+    # Require cffi.
+    install_requires=["cffi>=1.0.0", "torch>=0.4"],
+    setup_requires=["cffi>=1.0.0", "torch>=0.4"],
     # Exclude the build files.
     packages=find_packages(exclude=["build"]),
-    ext_modules=[
-        CppExtension(
-            '_torch_sox', ['torchaudio/torch_sox.cpp'], libraries=['sox']),
+    # Package where to put the extensions. Has to be a prefix of build.py.
+    ext_package="",
+    # Extensions to compile.
+    cffi_modules=[
+        os.path.join(this_file, "build.py:ffi")
     ],
-    cmdclass={'build_ext': BuildExtension})
+)
