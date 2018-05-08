cfe3f29686673fecc5d4b05ab2ed1b9bca476950
Edward Z. Yang <ezyang@mit.edu>
2018-05-08 00:10:32 -0400

    Bring back cpp_extension rewrite of torchaudio (#45)
    * Revert "Revert "Rewrote C code with C++ extensions" (#39)"
    
    This reverts commit 7236e3f4b97eaeb942f07d75501e9626e80e7f85.
    
    * Fix import error of C extension
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 from setuptools import setup, find_packages
-
-import build
-
-this_file = os.path.dirname(__file__)
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 setup(
     name="torchaudio",
     version="0.1",
     description="An audio package for PyTorch",
     url="https://github.com/pytorch/audio",
-    author="Soumith Chintala, David Pollack, Sean Naren",
+    author="Soumith Chintala, David Pollack, Sean Naren, Peter Goldsborough",
     author_email="soumith@pytorch.org",
-    # Require cffi.
-    install_requires=["cffi>=1.0.0", "torch>=0.4"],
-    setup_requires=["cffi>=1.0.0", "torch>=0.4"],
+    install_requires=["torch>=0.4"],
+    setup_requires=["torch>=0.4"],
     # Exclude the build files.
     packages=find_packages(exclude=["build"]),
-    # Package where to put the extensions. Has to be a prefix of build.py.
-    ext_package="",
-    # Extensions to compile.
-    cffi_modules=[
-        os.path.join(this_file, "build.py:ffi")
+    ext_modules=[
+        CppExtension(
+            '_torch_sox', ['torchaudio/torch_sox.cpp'], libraries=['sox']),
     ],
-)
+    cmdclass={'build_ext': BuildExtension})
