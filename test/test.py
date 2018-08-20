7314b36dd5f4205826ff9570068b23cd405abd50
David Pollack <sent@da3.net>
2018-08-21 00:40:05 +0200

    allow loading with offsets and number of samples and saving specified bit precisions (#59)

--- test/test.py
+++ test/test.py
@@ -99,6 +99,15 @@ class Test_LoadSave(unittest.TestCase):
         torchaudio.save(sinewave_filepath, y, sr)
         self.assertTrue(os.path.isfile(sinewave_filepath))
 
+        # test precision
+        new_filepath = os.path.join(self.test_dirpath, "test.wav")
+        _, _, _, bp = torchaudio.info(sinewave_filepath)
+        torchaudio.save(new_filepath, y, sr, precision=16)
+        _, _, _, bp16 = torchaudio.info(new_filepath)
+        self.assertEqual(bp, 32)
+        self.assertEqual(bp16, 16)
+        os.unlink(new_filepath)
+
     def test_load_and_save_is_identity(self):
         input_path = os.path.join(self.test_dirpath, 'assets', 'sinewave.wav')
         tensor, sample_rate = torchaudio.load(input_path)
@@ -109,6 +118,48 @@ class Test_LoadSave(unittest.TestCase):
         self.assertEqual(sample_rate, sample_rate2)
         os.unlink(output_path)
 
+    def test_load_partial(self):
+        num_frames = 100
+        offset = 200
+        # load entire mono sinewave wav file, load a partial copy and then compare
+        input_sine_path = os.path.join(self.test_dirpath, 'assets', 'sinewave.wav')
+        x_sine_full, sr_sine = torchaudio.load(input_sine_path)
+        x_sine_part, _ = torchaudio.load(input_sine_path, num_frames=num_frames, offset=offset)
+        l1_error = x_sine_full[offset:(num_frames+offset)].sub(x_sine_part).abs().sum().item()
+        # test for the correct number of samples and that the correct portion was loaded
+        self.assertEqual(x_sine_part.size(0), num_frames)
+        self.assertEqual(l1_error, 0.)
+
+        # create a two channel version of this wavefile
+        x_2ch_sine = x_sine_full.repeat(1, 2)
+        out_2ch_sine_path = os.path.join(self.test_dirpath, 'assets', '2ch_sinewave.wav')
+        torchaudio.save(out_2ch_sine_path, x_2ch_sine, sr_sine)
+        x_2ch_sine_load, _ = torchaudio.load(out_2ch_sine_path, num_frames=num_frames, offset=offset)
+        os.unlink(out_2ch_sine_path)
+        l1_error = x_2ch_sine_load.sub(x_2ch_sine[offset:(offset + num_frames)]).abs().sum().item()
+        self.assertEqual(l1_error, 0.)
+
+        # test with two channel mp3
+        x_2ch_full, sr_2ch = torchaudio.load(self.test_filepath, normalization=True)
+        x_2ch_part, _ = torchaudio.load(self.test_filepath, normalization=True, num_frames=num_frames, offset=offset)
+        l1_error = x_2ch_full[offset:(offset+num_frames)].sub(x_2ch_part).abs().sum().item()
+        self.assertEqual(x_2ch_part.size(0), num_frames)
+        self.assertEqual(l1_error, 0.)
+
+        # check behavior if number of samples would exceed file length
+        offset_ns = 300
+        x_ns, _ = torchaudio.load(input_sine_path, num_frames=100000, offset=offset_ns)
+        self.assertEqual(x_ns.size(0), x_sine_full.size(0) - offset_ns)
+
+        # check when offset is beyond the end of the file
+        with self.assertRaises(RuntimeError):
+            torchaudio.load(input_sine_path, offset=100000)
+
+    def test_get_info(self):
+        input_path = os.path.join(self.test_dirpath, 'assets', 'sinewave.wav')
+        info_expected = (1, 64000, 16000, 32)
+        info_load = torchaudio.info(input_path)
+        self.assertEqual(info_load, info_expected)
 
 if __name__ == '__main__':
     unittest.main()
