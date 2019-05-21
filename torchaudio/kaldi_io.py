90dda093403eaa79595d44d289912205e8b85da3
Jason Lian <jamarshon@fb.com>
2019-05-21 12:38:57 -0700

    More docs

--- torchaudio/kaldi_io.py
+++ torchaudio/kaldi_io.py
@@ -5,32 +5,81 @@ import numpy as np
 import torch
 
 
-def default_not_imported_method():
+__all__ = [
+    'read_vec_int_ark',
+    'read_vec_flt_scp',
+    'read_vec_flt_ark',
+    'read_mat_scp',
+    'read_mat_ark',
+]
+
+
+def _default_not_imported_method():
     raise ImportError('Could not import kaldi_io')
 
 
-def wrap_method(fn, convert_contiguous=False):
+def _wrap_method(fn, convert_contiguous=False):
     # type: (Function, bool) -> Function
     """ Takes a method with the signature (file name/descriptor) -> generator(string, ndarray)
     and converts it to (file name/descriptor) -> generator(string, Tensor).
     convert_contiguous determines whether the array should be converted into a
     contiguous layout.
     """
-    def wrapped_fn(file_or_fd):
+    def _wrapped_fn(file_or_fd):
         for key, np_arr in fn(file_or_fd):
             if convert_contiguous:
                 np_arr = np.ascontiguousarray(np_arr)
             yield key, torch.from_numpy(np_arr)
-    return wrapped_fn
+    return _wrapped_fn
 
 
-read_vec_int_ark = default_not_imported_method
+#: Create generator of (key,vector<int>) tuples, which reads from the ark file/stream.
+#:
+#: file_or_fd : ark, gzipped ark, pipe or opened file descriptor.
+#:
+#: Example, read ark to a 'dictionary':
+#:
+#: >>> # generator(key,vec) = torchaudio.kaldi_io.read_vec_int_ark(file_or_fd)
+#: >>> d = { u:d for u,d in torchaudio.kaldi_io.read_vec_int_ark(file) }
+read_vec_int_ark = _default_not_imported_method
 
-read_vec_flt_scp = default_not_imported_method
-read_vec_flt_ark = default_not_imported_method
+#: Create generator of (key,vector<float32/float64>) tuples, read according to kaldi scp.
+#:
+#: file_or_fd : scp, gzipped scp, pipe or opened file descriptor.
+#:
+#: Example, read scp to a 'dictionary':
+#:
+#: >>> # generator(key,vec) = torchaudio.kaldi_io.read_vec_flt_scp(file_or_fd)
+#: >>> d = { u:d for u,d in torchaudio.kaldi_io.read_vec_flt_scp(file) }
+read_vec_flt_scp = _default_not_imported_method
+#: Create generator of (key,vector<float32/float64>) tuples, which reads from the ark file/stream.
+#:
+#: file_or_fd : ark, gzipped ark, pipe or opened file descriptor.
+#:
+#: Example, read ark to a 'dictionary':
+#:
+#: >>> # generator(key,vec) = torchaudio.kaldi_io.read_vec_flt_ark(file_or_fd)
+#: >>> d = { u:d for u,d in torchaudio.kaldi_io.read_vec_flt_ark(file) }
+read_vec_flt_ark = _default_not_imported_method
 
-read_mat_scp = default_not_imported_method
-read_mat_ark = default_not_imported_method
+#: Create generator of (key,matrix<float32/float64>) tuples, read according to kaldi scp.
+#:
+#: file_or_fd : scp, gzipped scp, pipe or opened file descriptor.
+#:
+#: Example, read scp to a 'dictionary':
+#:
+#: >>> # generator(key,mat) = torchaudio.kaldi_io.read_mat_scp(file_or_fd)
+#: >>> d = { u:d for u,d in torchaudio.kaldi_io.read_mat_scp(file) }
+read_mat_scp = _default_not_imported_method
+#: Create generator of (key,matrix<float32/float64>) tuples, which reads from the ark file/stream.
+#:
+#: file_or_fd : ark, gzipped ark, pipe or opened file descriptor.
+#:
+#: Example, read ark to a 'dictionary':
+#:
+#: >>> # generator(key,mat) = torchaudio.kaldi_io.read_mat_ark(file_or_fd)
+#: >>> d = { u:d for u,d in torchaudio.kaldi_io.read_mat_ark(file) }
+read_mat_ark = _default_not_imported_method
 
 try:
     import kaldi_io
@@ -39,12 +88,12 @@ try:
     # Elements from int32 vector are sored in tuples: (sizeof(int32), value)
     # so strides are (5,) instead of (4,) which will throw an error in from_numpy
     # as it expects strides to be a multiple of 4 (int32).
-    read_vec_int_ark = wrap_method(kaldi_io.read_vec_int_ark, convert_contiguous=True)
+    read_vec_int_ark = _wrap_method(kaldi_io.read_vec_int_ark, convert_contiguous=True)
 
-    read_vec_flt_scp = wrap_method(kaldi_io.read_vec_flt_scp)
-    read_vec_flt_ark = wrap_method(kaldi_io.read_vec_flt_ark)
+    read_vec_flt_scp = _wrap_method(kaldi_io.read_vec_flt_scp)
+    read_vec_flt_ark = _wrap_method(kaldi_io.read_vec_flt_ark)
 
-    read_mat_scp = wrap_method(kaldi_io.read_mat_scp)
-    read_mat_ark = wrap_method(kaldi_io.read_mat_ark)
+    read_mat_scp = _wrap_method(kaldi_io.read_mat_scp)
+    read_mat_ark = _wrap_method(kaldi_io.read_mat_ark)
 except ImportError:
     pass
