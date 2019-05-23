88d1b6090b1f7f96c27ff8a5eea54bca76675f89
Jason Lian <jamarshon@fb.com>
2019-05-23 07:45:37 -0700

    g

--- build_tools/travis/test_script.sh
+++ build_tools/travis/test_script.sh
@@ -10,9 +10,9 @@ python --version
 
 run_tests() {
     if [[ "$RUN_SLOW" == "true" ]]; then
-        TEST_CMD="py.test --runslow -s -v --cov=torchtext --durations=20"
+        TEST_CMD="py.test --runslow -s -v --cov=torchaudio --durations=20"
     else
-        TEST_CMD="py.test -v --cov=torchtext --durations=20"
+        TEST_CMD="py.test -v --cov=torchaudio --durations=20"
     fi
     $TEST_CMD
 }
