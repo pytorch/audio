50a6f3e95f6024f1d17ce5176611f38f3db30666
Jason Lian <jamarshon@fb.com>
2019-07-08 12:41:49 -0700

    apply feedback

--- test/test_functional.py
+++ test/test_functional.py
@@ -8,22 +8,27 @@ import test.common_utils
 
 class TestFunctional(unittest.TestCase):
     data_sizes = (2, 20)
-    number_of_trials = 10
+    number_of_trials = 100
 
     def _compare_estimate(self, sound, estimate):
         # trim sound for case when constructed signal is shorter than original
         sound = sound[..., :estimate.size(-1)]
 
         self.assertTrue(sound.shape == estimate.shape, (sound.shape, estimate.shape))
-        self.assertTrue(torch.allclose(sound, estimate, atol=1e-4))
+        self.assertTrue(torch.allclose(sound, estimate, atol=1e-6))
 
-    def _test_istft_helper(self, sound, kwargs):
-        stft = torch.stft(sound, **kwargs)
-        estimate = torchaudio.functional.istft(stft, length=sound.size(1), **kwargs)
+    def _test_istft_is_inverse_of_stft(self, kwargs):
+        # generates a random sound signal for each tril and then does the stft/istft
+        # operation to check whether we can reconstruct signal
+        for i in range(self.number_of_trials):
+            sound = torch.rand(self.data_sizes)
 
-        self._compare_estimate(sound, estimate)
+            stft = torch.stft(sound, **kwargs)
+            estimate = torchaudio.functional.istft(stft, length=sound.size(1), **kwargs)
 
-    def test_istft1(self):
+            self._compare_estimate(sound, estimate)
+
+    def test_istft_is_inverse_of_stft1(self):
         # hann_window, centered, normalized, onesided
         kwargs1 = {
             'n_fft': 12,
@@ -36,11 +41,9 @@ class TestFunctional(unittest.TestCase):
             'onesided': True,
         }
 
-        for i in range(self.number_of_trials):
-            test_data = torch.rand(self.data_sizes)
-            self._test_istft_helper(test_data, kwargs1)
+        self._test_istft_is_inverse_of_stft(kwargs1)
 
-    def test_istft2(self):
+    def test_istft_is_inverse_of_stft2(self):
         # hann_window, centered, not normalized, not onesided
         kwargs2 = {
             'n_fft': 12,
@@ -53,11 +56,9 @@ class TestFunctional(unittest.TestCase):
             'onesided': False,
         }
 
-        for i in range(self.number_of_trials):
-            test_data = torch.rand(self.data_sizes)
-            self._test_istft_helper(test_data, kwargs2)
+        self._test_istft_is_inverse_of_stft(kwargs2)
 
-    def test_istft3(self):
+    def test_istft_is_inverse_of_stft3(self):
         # hamming_window, centered, normalized, not onesided
         kwargs3 = {
             'n_fft': 15,
@@ -70,11 +71,9 @@ class TestFunctional(unittest.TestCase):
             'onesided': False,
         }
 
-        for i in range(self.number_of_trials):
-            test_data = torch.rand(self.data_sizes)
-            self._test_istft_helper(test_data, kwargs3)
+        self._test_istft_is_inverse_of_stft(kwargs3)
 
-    def test_istft4(self):
+    def test_istft_is_inverse_of_stft4(self):
         # hamming_window, not centered, not normalized, onesided
         # window same size as n_fft
         kwargs4 = {
@@ -88,11 +87,9 @@ class TestFunctional(unittest.TestCase):
             'onesided': True,
         }
 
-        for i in range(self.number_of_trials):
-            test_data = torch.rand(self.data_sizes)
-            self._test_istft_helper(test_data, kwargs4)
+        self._test_istft_is_inverse_of_stft(kwargs4)
 
-    def test_istft5(self):
+    def test_istft_is_inverse_of_stft5(self):
         # hamming_window, not centered, not normalized, not onesided
         # window same size as n_fft
         kwargs5 = {
@@ -106,11 +103,9 @@ class TestFunctional(unittest.TestCase):
             'onesided': False,
         }
 
-        for i in range(self.number_of_trials):
-            test_data = torch.rand(self.data_sizes)
-            self._test_istft_helper(test_data, kwargs5)
+        self._test_istft_is_inverse_of_stft(kwargs5)
 
-    def test_istft6(self):
+    def test_istft_of_ones(self):
         # stft = torch.stft(torch.ones(4), 4)
         stft = torch.tensor([
             [[4., 0.], [4., 0.], [4., 0.], [4., 0.], [4., 0.]],
@@ -121,12 +116,37 @@ class TestFunctional(unittest.TestCase):
         estimate = torchaudio.functional.istft(stft, n_fft=4, length=4)
         self._compare_estimate(torch.ones(4), estimate)
 
-    def test_istft7(self):
+    def test_istft_of_zeros(self):
         # stft = torch.stft(torch.zeros(4), 4)
         stft = torch.zeros((3, 5, 2))
 
         estimate = torchaudio.functional.istft(stft, n_fft=4, length=4)
         self._compare_estimate(torch.zeros(4), estimate)
+
+    def test_istft_requires_overlap_windows(self):
+        # the window is size 1 but it hops 20 so there is a gap which throw an error
+        stft = torch.rand((3, 5, 2))
+        self.assertRaises(AssertionError, torchaudio.functional.istft, stft, n_fft=4,
+                          hop_length=20, win_length=1, window=torch.ones(1))
+
+    def test_istft_requires_nola(self):
+        stft = torch.zeros((3, 5, 2))
+        kwargs_ok = {
+            'n_fft': 4,
+            'win_length': 4,
+            'window': torch.ones(4),
+        }
+
+        kwargs_not_ok = {
+            'n_fft': 4,
+            'win_length': 4,
+            'window': torch.zeros(4),
+        }
+
+        # A window of ones meets NOLA but a window of zeros does not. This should
+        # throw an error.
+        torchaudio.functional.istft(stft, **kwargs_ok)
+        self.assertRaises(AssertionError, torchaudio.functional.istft, stft, **kwargs_not_ok)
 
 
 if __name__ == '__main__':
