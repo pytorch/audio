804afe8b9a045f8a09bf5e605a5f8470f75fae57
Jason Lian <jamarshon@fb.com>
2019-07-17 08:37:07 -0700

    more

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -53,7 +53,7 @@ pip install -r requirements.txt
  # Install the following only if running tests
 if [[ "$SKIP_TESTS" != "true" ]]; then
     # PyTorch (nightly as 1.1 does not have Optional for type annotations)
-    conda install --yes pytorch-nightly-cpu=1.2.0.dev20190715 -c pytorch
+    conda install --yes pytorch-nightly-cpu=1.2.0.dev20190711 -c pytorch
 
      # TorchAudio CPP Extensions
     pip install .
