17f3054abbdb11120594ca0c60d178c15cbe836a
Edward Z. Yang <ezyang@fb.com>
2019-08-02 13:36:57 -0700

    Conda packaging updates
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- build_tools/packaging/conda/build_audio.sh
+++ build_tools/packaging/conda/build_audio.sh
@@ -10,7 +10,7 @@ retry () {
     $*  || (sleep 1 && $*) || (sleep 2 && $*) || (sleep 4 && $*) || (sleep 8 && $*)
 }
 
-export TORCHAUDIO_BUILD_VERSION="0.2.0"
+export TORCHAUDIO_BUILD_VERSION="0.4.0.dev$(date "+%Y%m%d")"
 export TORCHAUDIO_BUILD_NUMBER=1
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -30,8 +30,7 @@ ANACONDA_USER=pytorch
 conda config --set anaconda_upload no
 
 # TODO: unhardcode
-# TODO: use CPU
-export CONDA_PYTORCH_CONSTRAINT="    - pytorch-nightly ==1.2.0.dev20190801+cu100"
+export CONDA_PYTORCH_CONSTRAINT="    - pytorch-nightly ==1.2.0.dev20190802+cpu"
 export CONDA_CUDATOOLKIT_CONSTRAINT=""
 export CUDA_VERSION="None"
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -39,7 +38,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 torchaudio
-time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 torchaudio
+# Currently disabled, see https://github.com/pytorch/pytorch/issues/23738
+# time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 torchaudio
 time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 torchaudio
 time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.7 torchaudio
 
