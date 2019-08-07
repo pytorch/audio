10b3240c5dbf22d33487e51bcdaaec90f33683db
Edward Z. Yang <ezyang@fb.com>
2019-08-06 19:59:23 -0700

    Update README
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- packaging/conda/get-latest.py
+++
@@ -1,8 +0,0 @@
-# Functionally equivalent to:
-# jq -r '.["pytorch"][-1].version' | sed 's/+.*$//'
-
-import sys
-import json
-import re
-
-print(re.sub(r'\+.*$', '', json.load(sys.stdin)["pytorch"][-1]["version"]))
