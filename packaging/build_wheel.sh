33af5425e4dd01e494f17d6a58159cb00df30e4a
Edward Z. Yang <ezyang@fb.com>
2019-08-07 20:19:43 -0700

    Refactor again
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- packaging/build_wheel.sh
+++ packaging/build_wheel.sh
@@ -4,10 +4,8 @@ set -ex
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
 
+setup_env 0.4.0
 setup_python
-setup_cpuonly_cuda_suffix
-setup_build_version 0.4.0
-setup_macos
 "$script_dir/build_from_source.sh" "$(pwd)"  # Build static dependencies
 pip_install numpy future
 setup_pip_pytorch_version
