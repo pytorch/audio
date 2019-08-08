faed00e9487614be5bb7b665ce65219bd3d38828
Edward Z. Yang <ezyang@fb.com>
2019-08-08 15:24:28 -0400

    Refactor version suffix so conda packages don't get suffixes. (#223)
    * Refactor version suffix so conda packages don't get suffixes.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * typofix
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -4,6 +4,7 @@ set -ex
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
 
+export BUILD_TYPE="conda"
 export NO_CUDA_PACKAGE=1
 setup_env 0.4.0
 export SOURCE_ROOT_DIR="$PWD"
