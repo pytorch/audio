9324150ea65c6a20ca58abf5c37829be68d5d0ba
Jason Lian <jamarshon@fb.com>
2019-08-12 09:31:52 -0700

    more

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -48,7 +48,7 @@ popd
 source activate testenv
 
  # Install requirements via pip in our conda environment
-python -m pip install -vvv -r requirements.txt
+python -m pip3 install -vvv -r requirements.txt
 
  # Install the following only if running tests
 if [[ "$SKIP_TESTS" != "true" ]]; then
