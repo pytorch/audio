7da61a4beeec7b7ff9ff5f1532b2adf99220a9b1
Tomás Osório <tomas.s.osorio@gmail.com>
2020-03-25 15:32:56 +0000

    Add Functional DB_to_amplitude (#469)
    * add functional DB_to_amplitude
    
    * add test scriptmodule
    
    * add test db_to_amplitude
    
    * add tests
    
    * improve docstrings, move ref for easier use

--- torchaudio/functional.py
+++ torchaudio/functional.py
@@ -401,6 +401,21 @@ def amplitude_to_DB(x, multiplier, amin, db_multiplier, top_db=None):
     return x_db
 
 
+def DB_to_amplitude(x, ref, power):
+    # type: (Tensor, float, float) -> Tensor
+    r"""Turn a tensor from the decibel scale to the power/amplitude scale.
+
+    Args:
+        x (torch.Tensor): Input tensor before being converted to power/amplitude scale.
+        ref (float): Reference which the output will be scaled by.
+        power (float): If power equals 1, will compute DB to power. If 0.5, will compute DB to amplitude.
+
+    Returns:
+        torch.Tensor: Output tensor in power/amplitude scale.
+    """
+    return ref * torch.pow(torch.pow(10.0, 0.1 * x), power)
+
+
 def create_fb_matrix(n_freqs, f_min, f_max, n_mels, sample_rate):
     # type: (int, float, float, int, int) -> Tensor
     r"""Create a frequency bin conversion matrix.
