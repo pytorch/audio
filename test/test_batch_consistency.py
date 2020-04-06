Fix missing atol/rtol, wrong shape, argument order. Remove redundant shape assertions

test/test_batch_consistency.py
@@ -23,8 +23,7 @@ def _test_batch_shape(functional, tensor, *args, atol=1e-8, rtol=1e-5, **kwargs)
     torch.random.manual_seed(42)
     computed = functional(tensors.clone(), *args, **kwargs)
 
-    assert expected.shape == computed.shape, (expected.shape, computed.shape)
-    torch.testing.assert_allclose(expected, computed, rtol=rtol, atol=atol)
+    torch.testing.assert_allclose(computed, expected, rtol=rtol, atol=atol)
 
     return tensors, expected
 
@@ -43,8 +42,7 @@ def _test_batch(functional, tensor, *args, atol=1e-8, rtol=1e-5, **kwargs):
     torch.random.manual_seed(42)
     computed = functional(tensors.clone(), *args, **kwargs)
 
-    assert expected.shape == computed.shape, (expected.shape, computed.shape)
-    torch.testing.assert_allclose(expected, computed, rtol=rtol, atol=atol)
+    torch.testing.assert_allclose(computed, expected, rtol=rtol, atol=atol)
 
 
 class TestFunctional(unittest.TestCase):
@@ -96,7 +94,6 @@ class TestTransforms(unittest.TestCase):
         # Batch then transform
         computed = torchaudio.transforms.AmplitudeToDB()(spec.repeat(3, 1, 1))
 
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_Resample(self):
@@ -108,7 +105,6 @@ class TestTransforms(unittest.TestCase):
         # Batch then transform
         computed = torchaudio.transforms.Resample()(waveform.repeat(3, 1, 1))
 
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_MelScale(self):
@@ -121,7 +117,6 @@ class TestTransforms(unittest.TestCase):
         computed = torchaudio.transforms.MelScale()(specgram.repeat(3, 1, 1, 1))
 
         # shape = (3, 2, 201, 1394)
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_InverseMelScale(self):
@@ -136,11 +131,10 @@ class TestTransforms(unittest.TestCase):
         computed = torchaudio.transforms.InverseMelScale(n_stft, n_mels)(mel_spec.repeat(3, 1, 1, 1))
 
         # shape = (3, 2, n_mels, 32)
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
 
         # Because InverseMelScale runs SGD on randomly initialized values so they do not yield
         # exactly same result. For this reason, tolerance is very relaxed here.
-        torch.testing.assert_allclose(computed, expected, atol=1.0)
+        torch.testing.assert_allclose(computed, expected, atol=1.0, rtol=1e-5)
 
     def test_batch_compute_deltas(self):
         specgram = torch.randn(2, 31, 2786)
@@ -152,7 +146,6 @@ class TestTransforms(unittest.TestCase):
         computed = torchaudio.transforms.ComputeDeltas()(specgram.repeat(3, 1, 1, 1))
 
         # shape = (3, 2, 201, 1394)
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_mulaw(self):
@@ -169,7 +162,6 @@ class TestTransforms(unittest.TestCase):
         computed = torchaudio.transforms.MuLawEncoding()(waveform_batched)
 
         # shape = (3, 2, 201, 1394)
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
         # Single then transform then batch
@@ -180,7 +172,6 @@ class TestTransforms(unittest.TestCase):
         computed = torchaudio.transforms.MuLawDecoding()(computed)
 
         # shape = (3, 2, 201, 1394)
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_spectrogram(self):
@@ -193,8 +184,6 @@ class TestTransforms(unittest.TestCase):
 
         # Batch then transform
         computed = torchaudio.transforms.Spectrogram()(waveform.repeat(3, 1, 1))
-
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_melspectrogram(self):
@@ -207,8 +196,6 @@ class TestTransforms(unittest.TestCase):
 
         # Batch then transform
         computed = torchaudio.transforms.MelSpectrogram()(waveform.repeat(3, 1, 1))
-
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
@@ -223,9 +210,7 @@ class TestTransforms(unittest.TestCase):
 
         # Batch then transform
         computed = torchaudio.transforms.MFCC()(waveform.repeat(3, 1, 1))
-
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
-        torch.testing.assert_allclose(computed, expected, atol=1e-5)
+        torch.testing.assert_allclose(computed, expected, atol=1e-5, rtol=1e-5)
 
     def test_batch_TimeStretch(self):
         test_filepath = os.path.join(
@@ -260,8 +245,7 @@ class TestTransforms(unittest.TestCase):
             hop_length=512,
         )(complex_specgrams.repeat(3, 1, 1, 1, 1))
 
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
-        torch.testing.assert_allclose(computed, expected, atol=1e-5)
+        torch.testing.assert_allclose(computed, expected, atol=1e-5, rtol=1e-5)
 
     def test_batch_Fade(self):
         test_filepath = os.path.join(
@@ -275,8 +259,6 @@ class TestTransforms(unittest.TestCase):
 
         # Batch then transform
         computed = torchaudio.transforms.Fade(fade_in_len, fade_out_len)(waveform.repeat(3, 1, 1))
-
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
     def test_batch_Vol(self):
@@ -289,8 +271,6 @@ class TestTransforms(unittest.TestCase):
 
         # Batch then transform
         computed = torchaudio.transforms.Vol(gain=1.1)(waveform.repeat(3, 1, 1))
-
-        assert computed.shape == expected.shape, (computed.shape, expected.shape)
         torch.testing.assert_allclose(computed, expected)
 
 

test/test_compliance_kaldi.py
@@ -156,7 +156,6 @@ class Test_Kaldi(unittest.TestCase):
             output = get_output_fn(sound, args)
 
             self._print_diagnostic(output, kaldi_output)
-            self.assertTrue(output.shape, kaldi_output.shape)
             torch.testing.assert_allclose(output, kaldi_output, atol=atol, rtol=rtol)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
@@ -299,7 +298,7 @@ class Test_Kaldi(unittest.TestCase):
         ground_truth = ground_truth[..., n_to_trim:-n_to_trim]
         estimate = estimate[..., n_to_trim:-n_to_trim]
 
-        torch.testing.assert_allclose(ground_truth, estimate, atol=atol, rtol=rtol)
+        torch.testing.assert_allclose(estimate, ground_truth, atol=atol, rtol=rtol)
 
     def test_resample_waveform_downsample_accuracy(self):
         for i in range(1, 20):
@@ -324,7 +323,7 @@ class Test_Kaldi(unittest.TestCase):
         for i in range(num_channels):
             single_channel = sound * (i + 1) * 1.5
             single_channel_sampled = kaldi.resample_waveform(single_channel, sample_rate, sample_rate // 2)
-            torch.testing.assert_allclose(multi_sound_sampled[i, :], single_channel_sampled, rtol=1e-4)
+            torch.testing.assert_allclose(multi_sound_sampled[i, :], single_channel_sampled[0], rtol=1e-4, atol=1e-8)
 
 
 if __name__ == '__main__':

test/test_functional_filtering.py
@@ -25,7 +25,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         a_coeffs = torch.tensor([1, 0, 0, 0], dtype=dtype, device=device)
         output_waveform = F.lfilter(waveform, a_coeffs, b_coeffs)
 
-        torch.testing.assert_allclose(waveform[:, 0:-3], output_waveform[:, 3:], atol=1e-5)
+        torch.testing.assert_allclose(output_waveform[:, 3:], waveform[:, 0:-3], atol=1e-5, rtol=1e-5)
 
     def test_lfilter_basic(self):
         self._test_lfilter_basic(torch.float32, torch.device("cpu"))
@@ -112,7 +112,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         E.append_effect_to_chain("gain", [3])
         sox_gain_waveform = E.sox_build_flow_effects()[0]
 
-        torch.testing.assert_allclose(waveform_gain, sox_gain_waveform, atol=1e-04)
+        torch.testing.assert_allclose(waveform_gain, sox_gain_waveform, atol=1e-04, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -128,13 +128,13 @@ class TestFunctionalFiltering(unittest.TestCase):
         E.append_effect_to_chain("dither", [])
         sox_dither_waveform = E.sox_build_flow_effects()[0]
 
-        torch.testing.assert_allclose(waveform_dithered, sox_dither_waveform, atol=1e-04)
+        torch.testing.assert_allclose(waveform_dithered, sox_dither_waveform, atol=1e-04, rtol=1e-5)
         E.clear_chain()
 
         E.append_effect_to_chain("dither", ["-s"])
         sox_dither_waveform_ns = E.sox_build_flow_effects()[0]
 
-        torch.testing.assert_allclose(waveform_dithered_noiseshaped, sox_dither_waveform_ns, atol=1e-02)
+        torch.testing.assert_allclose(waveform_dithered_noiseshaped, sox_dither_waveform_ns, atol=1e-02, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -178,7 +178,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.lowpass_biquad(waveform, sample_rate, CUTOFF_FREQ)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -199,7 +199,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         output_waveform = F.highpass_biquad(waveform, sample_rate, CUTOFF_FREQ)
 
         # TBD - this fails at the 1e-4 level, debug why
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-3)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-3, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -220,7 +220,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.allpass_biquad(waveform, sample_rate, CENTRAL_FREQ, Q)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -242,7 +242,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.bandpass_biquad(waveform, sample_rate, CENTRAL_FREQ, Q, CONST_SKIRT_GAIN)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -264,7 +264,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.bandpass_biquad(waveform, sample_rate, CENTRAL_FREQ, Q, CONST_SKIRT_GAIN)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -285,7 +285,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.bandreject_biquad(waveform, sample_rate, CENTRAL_FREQ, Q)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -307,7 +307,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.band_biquad(waveform, sample_rate, CENTRAL_FREQ, Q, NOISE)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -329,7 +329,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.band_biquad(waveform, sample_rate, CENTRAL_FREQ, Q, NOISE)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -351,7 +351,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.treble_biquad(waveform, sample_rate, GAIN, CENTRAL_FREQ, Q)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -369,7 +369,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.deemph_biquad(waveform, sample_rate)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -387,7 +387,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.riaa_biquad(waveform, sample_rate)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -409,7 +409,7 @@ class TestFunctionalFiltering(unittest.TestCase):
         waveform, sample_rate = torchaudio.load(noise_filepath, normalization=True)
         output_waveform = F.equalizer_biquad(waveform, sample_rate, CENTER_FREQ, GAIN, Q)
 
-        torch.testing.assert_allclose(sox_output_waveform, output_waveform, atol=1e-4)
+        torch.testing.assert_allclose(output_waveform, sox_output_waveform, atol=1e-4, rtol=1e-5)
 
     @unittest.skipIf("sox" not in BACKENDS, "sox not available")
     @AudioBackendScope("sox")
@@ -435,7 +435,7 @@ class TestFunctionalFiltering(unittest.TestCase):
             waveform, torch.tensor([a0, a1, a2]), torch.tensor([b0, b1, b2])
         )
 
-        torch.testing.assert_allclose(waveform_sox_out, waveform_lfilter_out, atol=1e-4)
+        torch.testing.assert_allclose(waveform_lfilter_out, waveform_sox_out, atol=1e-4, rtol=1e-5)
 
 
 if __name__ == "__main__":