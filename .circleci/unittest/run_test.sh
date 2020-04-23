7f9a3f196da7b952289e2fe7e069bd5aa5a20eca
moto <855818+mthrok@users.noreply.github.com>
2020-04-23 23:38:02 +0000

    add caching

--- .circleci/unittest/run_test.sh
+++ .circleci/unittest/run_test.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-conda_location="${HOME}/miniconda3"
-root_dir="$(git rev-parse --show-toplevel)"
-
-eval "$(${conda_location}/bin/conda shell.bash hook)"
+eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
--- .circleci/unittest/setup_env.sh
+++ .circleci/unittest/setup_env.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 
 # This script is for setting up environment for running unit test on CircleCI.
+# To speed up the CI time, the result of environment is cached.
+# PyTorch is not included here, so that it won't be cached.
 
 set -e
 
-unset PYTORCH_VERSION
-# For unittest, nightly PyTorch is used, and we do not want to fixiate on the version
-
-conda_location="${HOME}/miniconda3"
-root_dir="$(git rev-parse --show-toplevel)"
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+root_dir="$(git rev-parse --show-toplevel)"
+conda_dir="${root_dir}/conda"
+env_dir="${root_dir}/env"
 
 cd "${root_dir}"
 
-printf "* Installing conda\n"
-wget -O miniconda.sh http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ./miniconda.sh -b -f -p "${conda_location}"
-eval "$(${conda_location}/bin/conda shell.bash hook)"
-conda init
+# 1. Install conda at ./conda
+if [ ! -d "${conda_dir}" ]; then
+    printf "* Installing conda\n"
+    wget -O miniconda.sh http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash ./miniconda.sh -b -f -p "${conda_dir}"
+fi
+printf "* Checking conda update\n"
+eval "$(${conda_dir}/bin/conda shell.bash hook)"
 conda update -n base -c defaults conda
 
-printf "* Creating a test environment\n"
-conda create --prefix ./env -y python="$PYTHON_VERSION"
-source activate ./env
+# 2. Create test environment at ./env
+if [ ! -d "${env_dir}" ]; then
+    printf "* Creating a test environment\n"
+    conda create --prefix "${env_dir}" -y python="$PYTHON_VERSION"
+fi
+conda activate "${env_dir}"
 conda env update --file "${this_dir}/environment.yml" --prune
 
-printf "* Setting up torchaudio\n"
-./packaging/build_from_source.sh "$PWD"
-IS_CONDA=true python setup.py develop
+# 3. Build codecs at ./third_party
+if [ ! -d "./third_party" ]; then
+    printf "* Building Codecs"
+    ./packaging/build_from_source.sh "$PWD"
+fi
