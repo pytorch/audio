a91a153884332825e6b8146741b7feeb83d0a6d2
peter <peter_jiachen@163.com>
2020-05-01 11:25:53 +0800

    Test

--- packaging/torchaudio/run_test.bat
+++ packaging/torchaudio/run_test.bat
@@ -1,7 +1,18 @@
 @echo on
 
-python -m pip install kaldi-io PySoundFile
+pip install --user kaldi-io PySoundFile
 if errorlevel 1 exit /b 1
+
+dir %APPDATA%\Python\Python36\site-packages
+echo %PYTHONPATH%
+
+if "%PYTHONPATH%" == "" (
+    set PYTHONPATH=%APPDATA%\Python\Python36\site-packages
+) else (
+    set PYTHONPATH=%PYTHONPATH%;%APPDATA%\Python\Python36\site-packages
+)
+
+echo %PYTHONPATH%
 
 pytest . --verbose --maxfail=1000000
 if errorlevel 1 exit /b 1
