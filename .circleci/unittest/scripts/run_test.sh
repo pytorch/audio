0b5c4fe44eb7f641944eb9658e033c5f4adcd41d
moto <855818+mthrok@users.noreply.github.com>
2020-05-01 21:49:04 +0000

    Show 20 slowest tests

--- .circleci/unittest/scripts/run_test.sh
+++ .circleci/unittest/scripts/run_test.sh
@@ -6,5 +6,5 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
-pytest --cov=torchaudio --junitxml=test-results/junit.xml -v test
+pytest --cov=torchaudio --junitxml=test-results/junit.xml -v --durations 20 test
 flake8 torchaudio test
