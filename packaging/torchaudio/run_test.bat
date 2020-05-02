bf34872959a589e6b2eaf5f981d18a14ccbf3e16
peter <peter_jiachen@163.com>
2020-05-02 13:01:54 +0800

    Use python scripts

--- packaging/torchaudio/run_test.bat
+++ packaging/torchaudio/run_test.bat
@@ -1,7 +1,22 @@
 @echo on
 
-pip install --no-deps kaldi-io PySoundFile
+pip install kaldi-io PySoundFile
 if errorlevel 1 exit /b 1
 
-pytest . --verbose --maxfail=1000000
+REM pytest . --verbose --maxfail=1000000
+python test\test_batch_consistency.py
+python test\test_compliance_kaldi.py
+python test\test_dataloader.py
+python test\test_datasets.py
+python test\test_functional.py
+python test\test_io.py
+python test\test_kaldi_compatibility.py
+python test\test_kaldi_io.py
+python test\test_librosa_compatibility.py
+python test\test_models.py
+python test\test_sox_compatibility.py
+python test\test_sox_effects.py
+python test\test_torchscript_consistency.py
+python test\test_transforms.py
+
 if errorlevel 1 exit /b 1
