acea9ac758116d914ff3a8eb9a0ac33d41975855
peter <peter_jiachen@163.com>
2020-05-02 13:24:33 +0800

    Turn on verbose

--- packaging/torchaudio/run_test.bat
+++ packaging/torchaudio/run_test.bat
@@ -4,19 +4,19 @@ pip install kaldi-io PySoundFile
 if errorlevel 1 exit /b 1
 
 REM pytest . --verbose --maxfail=1000000
-python test\test_batch_consistency.py
-python test\test_compliance_kaldi.py
-python test\test_dataloader.py
-python test\test_datasets.py
-python test\test_functional.py
-python test\test_io.py
-python test\test_kaldi_compatibility.py
-python test\test_kaldi_io.py
-python test\test_librosa_compatibility.py
-python test\test_models.py
-python test\test_sox_compatibility.py
-python test\test_sox_effects.py
-python test\test_torchscript_consistency.py
-python test\test_transforms.py
+python test\test_batch_consistency.py -v
+python test\test_compliance_kaldi.py -v
+python test\test_dataloader.py -v
+python test\test_datasets.py -v
+python test\test_functional.py -v
+python test\test_io.py -v
+python test\test_kaldi_compatibility.py -v
+python test\test_kaldi_io.py -v
+python test\test_librosa_compatibility.py -v
+python test\test_models.py -v
+python test\test_sox_compatibility.py -v
+python test\test_sox_effects.py -v
+python test\test_torchscript_consistency.py -v
+python test\test_transforms.py -v
 
 if errorlevel 1 exit /b 1
