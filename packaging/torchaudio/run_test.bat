c1efc4a8a3ec707cf8dfca06dd5223fe1a68379e
peter <peter_jiachen@163.com>
2020-05-02 14:50:23 +0800

    Fix tests

--- packaging/torchaudio/run_test.bat
+++ packaging/torchaudio/run_test.bat
@@ -3,20 +3,20 @@
 pip install kaldi-io PySoundFile
 if errorlevel 1 exit /b 1
 
-REM pytest . --verbose --maxfail=1000000
-python test\test_batch_consistency.py -v
-python test\test_compliance_kaldi.py -v
-python test\test_dataloader.py -v
-python test\test_datasets.py -v
-python test\test_functional.py -v
-python test\test_io.py -v
-python test\test_kaldi_compatibility.py -v
-python test\test_kaldi_io.py -v
-python test\test_librosa_compatibility.py -v
-python test\test_models.py -v
-python test\test_sox_compatibility.py -v
-python test\test_sox_effects.py -v
-python test\test_torchscript_consistency.py -v
-python test\test_transforms.py -v
+pytest . --verbose --maxfail=1000000
+REM python test\test_batch_consistency.py -v
+REM python test\test_compliance_kaldi.py -v
+REM python test\test_dataloader.py -v
+REM python test\test_datasets.py -v
+REM python test\test_functional.py -v
+REM python test\test_io.py -v
+REM python test\test_kaldi_compatibility.py -v
+REM python test\test_kaldi_io.py -v
+REM python test\test_librosa_compatibility.py -v
+REM python test\test_models.py -v
+REM python test\test_sox_compatibility.py -v
+REM python test\test_sox_effects.py -v
+REM python test\test_torchscript_consistency.py -v
+REM python test\test_transforms.py -v
 
 if errorlevel 1 exit /b 1
