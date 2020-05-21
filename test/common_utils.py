6af60bb313a1d25ca3cd0bbcaabb3c0b6c415c30
moto <855818+mthrok@users.noreply.github.com>
2020-05-21 15:59:02 +0000

    Add util to generate whitenoise

--- test/common_utils.py
+++ test/common_utils.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 import unittest
-from typing import Type, Iterable
+from typing import Type, Iterable, Union
 from contextlib import contextmanager
 from shutil import copytree
 
@@ -117,3 +117,38 @@ def define_test_suites(
             for dtype in dtypes:
                 t = define_test_suite(suite, dtype, device)
                 scope[t.__name__] = t
+
+
+def get_whitenoise(
+    *,
+    sample_rate: int = 16000,
+    duration: float = 1,  # seconds
+    n_channels: int = 1,
+    seed: int = 0,
+    dtype: Union[str, torch.dtype] = "float32",
+    device: Union[str, torch.device] = "cpu",
+):
+    """Generate pseudo audio data with whitenoise
+
+    Args:
+        sample_rate: Sampling rate
+        duration: Length of the resulting Tensor in seconds.
+        n_channels: Number of channels
+        seed: Seed value used for random number generation.
+            Note that this function does not modify global random generator state.
+        dtype: Torch dtype
+        device: device
+    Returns:
+        Tensor: shape of (n_channels, sample_rate * duration)
+    """
+    if isinstance(dtype, str):
+        dtype = getattr(torch, dtype)
+    shape = [n_channels, sample_rate * duration]
+    # According to the doc, folking rng on all CUDA devices is slow when there are many CUDA devices,
+    # so we only folk on CPU, generate values and move the data to the given device
+    with torch.random.fork_rng([]):
+        torch.random.manual_seed(seed)
+        tensor = torch.randn(shape, dtype=dtype, device='cpu')
+    tensor /= 2.0
+    tensor.clamp_(-1.0, 1.0)
+    return tensor.to(device=device)
