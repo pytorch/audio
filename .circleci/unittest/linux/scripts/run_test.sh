6c3cc16ddb2e5dd5da1dcc5bceec6b1e10e9003a
moto <855818+mthrok@users.noreply.github.com>
2020-05-28 11:31:00 -0400

    Separate style check from unit test (#664)
    * Separate style check from unit test
    
    * TEST flake8 fail
    
    * TEST clang-format fail
    
    * Revert "TEST flake8 fail"
    
    This reverts commit 88d90185cefe294b9a662129a05c2e26a9844fe4.
    
    * Revert "TEST clang-format fail"
    
    This reverts commit 93517d1ccba650a6ccd813268fd73a4fd63b252c.

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -7,4 +7,3 @@ conda activate ./env
 
 python -m torch.utils.collect_env
 pytest --cov=torchaudio --junitxml=test-results/junit.xml -v --durations 20 test
-flake8 torchaudio test build_tools/setup_helpers
