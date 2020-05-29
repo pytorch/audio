587bde33ef2195c4691b4e908671487b05735632
moto <855818+mthrok@users.noreply.github.com>
2020-05-29 20:15:52 +0000

    Fix packaging/CI script

--- packaging/torchaudio/build.sh
+++ packaging/torchaudio/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-python setup.py install --single-version-externally-managed --record=record.txt
+BUILD_SOX= python setup.py install --single-version-externally-managed --record=record.txt
