1dfa56237ddf789c0a762c0034abc6025c968696
Vincent QB <vincentqb@users.noreply.github.com>
2020-06-02 17:14:29 -0400

    flake8

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -34,7 +34,7 @@ class Tester(TestCase):
         if not waveform.is_floating_point():
             waveform = waveform.to(torch.get_default_dtype())
         waveform /= torch.abs(waveform).max()
-        
+
         self.assertTrue(waveform.min() >= -1. and waveform.max() <= 1.)
 
         waveform_mu = transforms.MuLawEncoding(quantization_channels)(waveform)
