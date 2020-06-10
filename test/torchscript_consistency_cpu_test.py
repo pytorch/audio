54a20292f73d13d7abe20884cf2eb4631a658a53
moto <855818+mthrok@users.noreply.github.com>
2020-06-10 17:21:17 +0000

    Fix style check

--- test/torchscript_consistency_cpu_test.py
+++ test/torchscript_consistency_cpu_test.py
@@ -3,7 +3,10 @@ from parameterized import parameterized_class
 from .common_utils import TestCase, common_test_class_parameters
 from .torchscript_consistency_impl import Functional, Transforms
 
+
 parameters = list(common_test_class_parameters(devices=['cpu']))
+
+
 @parameterized_class(parameters)
 class TestFunctional(Functional, TestCase):
     pass
