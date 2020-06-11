d2724481cd8db32e0460bb8ca96ae32d71fb1ad4
moto <855818+mthrok@users.noreply.github.com>
2020-06-11 14:05:48 -0400

    Change parameterized testing system to be compatible with unittest (#712)
    * Change parameterized testing system to be compatible with unittest
    
    Summary: The previous implementation of parameterized testing worked by modifying test.common_utils inplace.  This doesn't work in general because unittest's contract with test modules is such that it must be able to load the module and run the test itself.  Because the previous implementation needed to load the module and modify it, it is incompatible.
    
    Reviewed By: mthrok
    
    Differential Revision: D21964676
    
    Co-authored-by: Ben Mehne <bmehne@fb.com>

--- test/torchscript_consistency_cpu_test.py
+++ test/torchscript_consistency_cpu_test.py
@@ -1,5 +1,17 @@
-from .common_utils import define_test_suites
+from parameterized import parameterized_class
+
+from .common_utils import TestCase, common_test_class_parameters
 from .torchscript_consistency_impl import Functional, Transforms
 
 
-define_test_suites(globals(), [Functional, Transforms], devices=['cpu'])
+parameters = list(common_test_class_parameters(devices=['cpu']))
+
+
+@parameterized_class(parameters)
+class TestFunctional(Functional, TestCase):
+    pass
+
+
+@parameterized_class(parameters)
+class TestTransforms(Transforms, TestCase):
+    pass
