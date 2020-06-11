8aeb482d96d45df1b5427f7e4f82f020b3ec0f9a
moto <855818+mthrok@users.noreply.github.com>
2020-06-11 21:37:58 +0000

    Revert "Move init/shutdown to private"
    This reverts commit 2187348ff1ce3962490f03beb7e672a4c4e897a7.

--- torchaudio/__init__.py
+++ torchaudio/__init__.py
@@ -25,8 +25,8 @@ from torchaudio.backend import (
     EncodingInfo,
 )
 from torchaudio.sox_effects import (
-    _init_sox_effects,
-    _shutdown_sox_effects,
+    init_sox_effects as _init_sox_effects,
+    shutdown_sox_effects as _shutdown_sox_effects,
 )
 
 try:
