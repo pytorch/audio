ab733e7b392c00b063f92f0b6097493203def7e9
jimchen90 <65626796+jimchen90@users.noreply.github.com>
2020-06-16 10:13:10 -0400

    update wav2letter test (#722)
    Co-authored-by: Ji Chen <jimchen90@devfair0160.h2.fair>

--- test/test_models.py
+++ test/test_models.py
@@ -1,28 +1,29 @@
-import pytest
-
 import torch
 from torchaudio.models import Wav2Letter
 
 
 class TestWav2Letter:
-    @pytest.mark.parametrize('batch_size', [2])
-    @pytest.mark.parametrize('num_features', [1])
-    @pytest.mark.parametrize('num_classes', [40])
-    @pytest.mark.parametrize('input_length', [320])
-    def test_waveform(self, batch_size, num_features, num_classes, input_length):
-        model = Wav2Letter()
+
+    def test_waveform(self):
+        batch_size = 2
+        num_features = 1
+        num_classes = 40
+        input_length = 320
+
+        model = Wav2Letter(num_classes=num_classes, num_features=num_features)
 
         x = torch.rand(batch_size, num_features, input_length)
         out = model(x)
 
         assert out.size() == (batch_size, num_classes, 2)
 
-    @pytest.mark.parametrize('batch_size', [2])
-    @pytest.mark.parametrize('num_features', [13])
-    @pytest.mark.parametrize('num_classes', [40])
-    @pytest.mark.parametrize('input_length', [2])
-    def test_mfcc(self, batch_size, num_features, num_classes, input_length):
-        model = Wav2Letter(input_type="mfcc", num_features=13)
+    def test_mfcc(self):
+        batch_size = 2
+        num_features = 13
+        num_classes = 40
+        input_length = 2
+
+        model = Wav2Letter(num_classes=num_classes, input_type="mfcc", num_features=num_features)
 
         x = torch.rand(batch_size, num_features, input_length)
         out = model(x)
