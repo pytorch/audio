cd4431af149a4aea74dcee65fd45f805d688f713
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos980.prn1.facebook.com>
2020-06-17 04:30:43 -0700

    2020-06-17 nightly release (4318fc5c7238af4ec36f8e1c9fe858d75ef7c21d)

--- test/test_models.py
+++ test/test_models.py
@@ -1,30 +1,51 @@
-import pytest
-
 import torch
-from torchaudio.models import Wav2Letter
+from torchaudio.models import Wav2Letter, _MelResNet
 
 
 class TestWav2Letter:
-    @pytest.mark.parametrize('batch_size', [2])
-    @pytest.mark.parametrize('num_features', [1])
-    @pytest.mark.parametrize('num_classes', [40])
-    @pytest.mark.parametrize('input_length', [320])
-    def test_waveform(self, batch_size, num_features, num_classes, input_length):
-        model = Wav2Letter()
+
+    def test_waveform(self):
+        batch_size = 2
+        num_features = 1
+        num_classes = 40
+        input_length = 320
+
+        model = Wav2Letter(num_classes=num_classes, num_features=num_features)
 
         x = torch.rand(batch_size, num_features, input_length)
         out = model(x)
 
         assert out.size() == (batch_size, num_classes, 2)
 
-    @pytest.mark.parametrize('batch_size', [2])
-    @pytest.mark.parametrize('num_features', [13])
-    @pytest.mark.parametrize('num_classes', [40])
-    @pytest.mark.parametrize('input_length', [2])
-    def test_mfcc(self, batch_size, num_features, num_classes, input_length):
-        model = Wav2Letter(input_type="mfcc", num_features=13)
+    def test_mfcc(self):
+        batch_size = 2
+        num_features = 13
+        num_classes = 40
+        input_length = 2
+
+        model = Wav2Letter(num_classes=num_classes, input_type="mfcc", num_features=num_features)
 
         x = torch.rand(batch_size, num_features, input_length)
         out = model(x)
 
         assert out.size() == (batch_size, num_classes, 2)
+
+
+class TestMelResNet:
+
+    def test_waveform(self):
+
+        batch_size = 2
+        num_features = 200
+        input_dims = 100
+        output_dims = 128
+        res_blocks = 10
+        hidden_dims = 128
+        pad = 2
+
+        model = _MelResNet(res_blocks, input_dims, hidden_dims, output_dims, pad)
+
+        x = torch.rand(batch_size, input_dims, num_features)
+        out = model(x)
+
+        assert out.size() == (batch_size, output_dims, num_features - pad * 2)
