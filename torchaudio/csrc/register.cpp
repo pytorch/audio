pytorch/audio

commit f8eac89bb46c5602c1a259ddd1ac15055ce5ba63

Add SignalInfo typedef, and extension module (#718)
This is a part of PRs to add new "sox_io" backend. https://github.com/pytorch/audio/pull/726

This PR adds `SignalInfo` structure, which is data exchange interface between Python and C++ in coming TorchScript-based sox IO backend.
For the case, where C++ extension is not available (i.e. Windows), this PR also adds dummy class and module that will be substituted.
This logic is implemented in `torchaudio.extension` moduel.

torchaudio/csrc/register.cpp
@@ -1,0 +1,18 @@
+#ifndef TORCHAUDIO_REGISTER_H
+#define TORCHAUDIO_REGISTER_H
+
+#include <torchaudio/csrc/typedefs.h>
+
+namespace torchaudio {
+namespace {
+
+static auto registerSignalInfo =
+    torch::class_<SignalInfo>("torchaudio", "SignalInfo")
+        .def(torch::init<int64_t, int64_t, int64_t>())
+        .def("get_sample_rate", &SignalInfo::getSampleRate)
+        .def("get_num_channels", &SignalInfo::getNumChannels)
+        .def("get_num_samples", &SignalInfo::getNumSamples);
+
+} // namespace
+} // namespace torchaudio
+#endif

torchaudio/csrc/typedefs.cpp
@@ -1,0 +1,23 @@
+#include <torchaudio/csrc/typedefs.h>
+
+namespace torchaudio {
+SignalInfo::SignalInfo(
+    const int64_t sample_rate_,
+    const int64_t num_channels_,
+    const int64_t num_samples_)
+    : sample_rate(sample_rate_),
+      num_channels(num_channels_),
+      num_samples(num_samples_){};
+
+int64_t SignalInfo::getSampleRate() const {
+  return sample_rate;
+}
+
+int64_t SignalInfo::getNumChannels() const {
+  return num_channels;
+}
+
+int64_t SignalInfo::getNumSamples() const {
+  return num_samples;
+}
+} // namespace torchaudio

torchaudio/csrc/typedefs.h
@@ -1,0 +1,23 @@
+#ifndef TORCHAUDIO_TYPDEFS_H
+#define TORCHAUDIO_TYPDEFS_H
+
+#include <torch/script.h>
+
+namespace torchaudio {
+struct SignalInfo : torch::CustomClassHolder {
+  int64_t sample_rate;
+  int64_t num_channels;
+  int64_t num_samples;
+
+  SignalInfo(
+      const int64_t sample_rate_,
+      const int64_t num_channels_,
+      const int64_t num_samples_);
+  int64_t getSampleRate() const;
+  int64_t getNumChannels() const;
+  int64_t getNumSamples() const;
+};
+
+} // namespace torchaudio
+
+#endif

torchaudio/extension/extension.py
@@ -1,0 +1,49 @@
+import warnings
+import importlib
+from collections import namedtuple
+
+import torch
+from torchaudio._internal import module_utils as _mod_utils
+
+
+def _init_extension():
+    ext = 'torchaudio._torchaudio'
+    if _mod_utils.is_module_available(ext):
+        _init_script_module(ext)
+    else:
+        warnings.warn('torchaudio C++ extension is not available.')
+        _init_dummy_module()
+
+
+def _init_script_module(module):
+    path = importlib.util.find_spec(module).origin
+    torch.classes.load_library(path)
+    torch.ops.load_library(path)
+
+
+def _init_dummy_module():
+    class SignalInfo:
+        """Data class for audio format information
+
+        Used when torchaudio C++ extension is not available for annotating
+        sox_io backend functions so that torchaudio is still importable
+        without extension.
+        This class has to implement the same interface as C++ equivalent.
+        """
+        def __init__(self, sample_rate: int, num_channels: int, num_samples: int):
+            self.sample_rate = sample_rate
+            self.num_channels = num_channels
+            self.num_samples = num_samples
+
+        def get_sample_rate(self):
+            return self.sample_rate
+
+        def get_num_channels(self):
+            return self.num_channels
+
+        def get_num_samples(self):
+            return self.num_samples
+
+    DummyModule = namedtuple('torchaudio', ['SignalInfo'])
+    module = DummyModule(SignalInfo)
+    setattr(torch.classes, 'torchaudio', module)

torchaudio/__init__.py
@@ -1,3 +1,4 @@
+from . import extension
 from torchaudio._internal import module_utils as _mod_utils
 from torchaudio import (
     compliance,

torchaudio/extension/__init__.py
@@ -1,0 +1,7 @@
+from .extension import (
+    _init_extension,
+)
+
+_init_extension()
+
+del _init_extension

commit e0f4c0ecf7c08f76081053fd0cdc5b0c3e893cd8

Fix SignalInfo member name to frame (#734)
This PR fixes the wrong member name of SignalInfo introduced in #718. 

 - `num_samples` == `num_frames` * `num_channels`.

torchaudio/csrc/register.cpp
@@ -12,7 +12,7 @@ static auto registerSignalInfo =
         .def(torch::init<int64_t, int64_t, int64_t>())
         .def("get_sample_rate", &SignalInfo::getSampleRate)
         .def("get_num_channels", &SignalInfo::getNumChannels)
-        .def("get_num_samples", &SignalInfo::getNumSamples);
+        .def("get_num_frames", &SignalInfo::getNumFrames);
 
 static auto registerGetInfo = torch::RegisterOperators().op(
     torch::RegisterOperators::options()

torchaudio/csrc/typedefs.cpp
@@ -4,10 +4,10 @@ namespace torchaudio {
 SignalInfo::SignalInfo(
     const int64_t sample_rate_,
     const int64_t num_channels_,
-    const int64_t num_samples_)
+    const int64_t num_frames_)
     : sample_rate(sample_rate_),
       num_channels(num_channels_),
-      num_samples(num_samples_){};
+      num_frames(num_frames_){};
 
 int64_t SignalInfo::getSampleRate() const {
   return sample_rate;
@@ -17,7 +17,7 @@ int64_t SignalInfo::getNumChannels() const {
   return num_channels;
 }
 
-int64_t SignalInfo::getNumSamples() const {
-  return num_samples;
+int64_t SignalInfo::getNumFrames() const {
+  return num_frames;
 }
 } // namespace torchaudio

torchaudio/csrc/typedefs.h
@@ -7,15 +7,15 @@ namespace torchaudio {
 struct SignalInfo : torch::CustomClassHolder {
   int64_t sample_rate;
   int64_t num_channels;
-  int64_t num_samples;
+  int64_t num_frames;
 
   SignalInfo(
       const int64_t sample_rate_,
       const int64_t num_channels_,
-      const int64_t num_samples_);
+      const int64_t num_frames_);
   int64_t getSampleRate() const;
   int64_t getNumChannels() const;
-  int64_t getNumSamples() const;
+  int64_t getNumFrames() const;
 };
 
 } // namespace torchaudio

torchaudio/extension/extension.py
@@ -30,10 +30,10 @@ def _init_dummy_module():
         without extension.
         This class has to implement the same interface as C++ equivalent.
         """
-        def __init__(self, sample_rate: int, num_channels: int, num_samples: int):
+        def __init__(self, sample_rate: int, num_channels: int, num_frames: int):
             self.sample_rate = sample_rate
             self.num_channels = num_channels
-            self.num_samples = num_samples
+            self.num_frames = num_frames
 
         def get_sample_rate(self):
             return self.sample_rate
@@ -41,8 +41,8 @@ def _init_dummy_module():
         def get_num_channels(self):
             return self.num_channels
 
-        def get_num_samples(self):
-            return self.num_samples
+        def get_num_frames(self):
+            return self.num_frames
 
     DummyModule = namedtuple('torchaudio', ['SignalInfo'])
     module = DummyModule(SignalInfo)

test/sox_io_backend/test_info.py
@@ -35,7 +35,7 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         )
         info = sox_io_backend.info(path)
         assert info.get_sample_rate() == sample_rate
-        assert info.get_num_samples() == sample_rate * duration
+        assert info.get_num_frames() == sample_rate * duration
         assert info.get_num_channels() == num_channels
 
     @parameterized.expand(list(itertools.product(
@@ -55,7 +55,7 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         )
         info = sox_io_backend.info(path)
         assert info.get_sample_rate() == sample_rate
-        assert info.get_num_samples() == sample_rate * duration
+        assert info.get_num_frames() == sample_rate * duration
         assert info.get_num_channels() == num_channels
 
     @parameterized.expand(list(itertools.product(
@@ -74,7 +74,7 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         info = sox_io_backend.info(path)
         assert info.get_sample_rate() == sample_rate
         # mp3 does not preserve the number of samples
-        # assert info.get_num_samples() == sample_rate * duration
+        # assert info.get_num_frames() == sample_rate * duration
         assert info.get_num_channels() == num_channels
 
     @parameterized.expand(list(itertools.product(
@@ -92,7 +92,7 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         )
         info = sox_io_backend.info(path)
         assert info.get_sample_rate() == sample_rate
-        assert info.get_num_samples() == sample_rate * duration
+        assert info.get_num_frames() == sample_rate * duration
         assert info.get_num_channels() == num_channels
 
     @parameterized.expand(list(itertools.product(
@@ -110,5 +110,5 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         )
         info = sox_io_backend.info(path)
         assert info.get_sample_rate() == sample_rate
-        assert info.get_num_samples() == sample_rate * duration
+        assert info.get_num_frames() == sample_rate * duration
         assert info.get_num_channels() == num_channels

test/sox_io_backend/test_torchscript.py
@@ -44,5 +44,5 @@ class SoxIO(TempDirMixin, TorchaudioTestCase):
         ts_info = ts_info_func(audio_path)
 
         assert py_info.get_sample_rate() == ts_info.get_sample_rate()
-        assert py_info.get_num_samples() == ts_info.get_num_samples()
+        assert py_info.get_num_frames() == ts_info.get_num_frames()
         assert py_info.get_num_channels() == ts_info.get_num_channels()