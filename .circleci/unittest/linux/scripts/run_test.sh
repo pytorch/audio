11b9ecaaff5e87a2f3536e6546ceadf1c5624750
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos200.prn3.facebook.com>
2020-06-27 04:30:44 -0700

    2020-06-27 nightly release (4daf2fb70b61dc2588dc7abc3f214c34d811e162)

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,4 +6,5 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
+export PATH="${PWD}/third_party/build/bin/:${PATH}"
 pytest --cov=torchaudio --junitxml=test-results/junit.xml -v --durations 20 test
