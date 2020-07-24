c79fb2212df0c296e66c2c1fde040860592f1146
Lawrence Chen <lawrencechen@fb.com>
2020-07-24 11:56:42 -0700

    Format unittest code

--- test/datasets/ljspeech_test.py
+++ test/datasets/ljspeech_test.py
@@ -1,5 +1,5 @@
-import os
 import csv
+import os
 
 from torchaudio.datasets import ljspeech
 
@@ -7,13 +7,13 @@ from ..common_utils import (
     TempDirMixin,
     TorchaudioTestCase,
     get_whitenoise,
-    save_wav,
     normalize_wav,
+    save_wav,
 )
 
 
 class TestLJSpeech(TempDirMixin, TorchaudioTestCase):
-    backend = 'default'
+    backend = "default"
 
     root_dir = None
     data = []
@@ -21,39 +21,47 @@ class TestLJSpeech(TempDirMixin, TorchaudioTestCase):
         "Test transcript 1",
         "Test transcript 2",
         "Test transcript 3",
-        "The examination and testimony of the experts enabled the Commission to conclude that 5 shots may have been fired,"
+        "The examination and testimony of the experts enabled the Commission to conclude that 5 shots may have been fired,",
     ]
 
     normalized_transcripts = [
         "Test transcript one",
         "Test transcript two",
         "Test transcript three",
-        "The examination and testimony of the experts enabled the Commission to conclude that five shots may have been fired,"
+        "The examination and testimony of the experts enabled the Commission to conclude that five shots may have been fired,",
     ]
 
     @classmethod
     def setUpClass(cls):
         cls.root_dir = cls.get_base_temp_dir()
-        base_dir = os.path.join(cls.root_dir, 'LJSpeech-1.1')
-        archive_dir = os.path.join(base_dir, 'wavs')
+        base_dir = os.path.join(cls.root_dir, "LJSpeech-1.1")
+        archive_dir = os.path.join(base_dir, "wavs")
         os.makedirs(archive_dir, exist_ok=True)
-        metadata_path = os.path.join(base_dir, 'metadata.csv')
+        metadata_path = os.path.join(base_dir, "metadata.csv")
 
-        with open(metadata_path, mode='w') as metadata_file:
-            metadata_writer = csv.writer(metadata_file, delimiter='|', quoting=csv.QUOTE_NONE)
-            for i, (transcript, normalized_transcript) in enumerate(zip(cls.transcripts, cls.normalized_transcripts)):
-                fileid = 'LJ00' + str(i)
+        with open(metadata_path, mode="w") as metadata_file:
+            metadata_writer = csv.writer(
+                metadata_file, delimiter="|", quoting=csv.QUOTE_NONE
+            )
+            for i, (transcript, normalized_transcript) in enumerate(
+                zip(cls.transcripts, cls.normalized_transcripts)
+            ):
+                fileid = "LJ00" + str(i)
                 metadata_writer.writerow([fileid, transcript, normalized_transcript])
-                filename = fileid + '.wav'
+                filename = fileid + ".wav"
                 path = os.path.join(archive_dir, filename)
-                data = get_whitenoise(sample_rate=8000, duration=12, n_channels=1, dtype='int16', seed=i)
+                data = get_whitenoise(
+                    sample_rate=8000, duration=12, n_channels=1, dtype="int16", seed=i
+                )
                 save_wav(path, data, 8000)
                 cls.data.append(normalize_wav(data))
 
     def test_ljspeech(self):
         dataset = ljspeech.LJSPEECH(self.root_dir)
         n_ite = 0
-        for i, (waveform, sample_rate, transcript, normalized_transcript) in enumerate(dataset):
+        for i, (waveform, sample_rate, transcript, normalized_transcript) in enumerate(
+            dataset
+        ):
             expected_transcript = self.transcripts[i]
             expected_normalized_transcript = self.normalized_transcripts[i]
             expected_data = self.data[i]
