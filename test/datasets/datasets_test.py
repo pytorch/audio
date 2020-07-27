47c735826044940749f70e47b41009d035fc256a
Lawrence Chen <lawrencechen@fb.com>
2020-07-27 09:36:52 -0700

    Removed ljspeech test from datasets_test

--- test/datasets/datasets_test.py
+++ test/datasets/datasets_test.py
@@ -3,7 +3,6 @@ from torchaudio.datasets.librispeech import LIBRISPEECH
 from torchaudio.datasets.speechcommands import SPEECHCOMMANDS
 from torchaudio.datasets.utils import diskcache_iterator, bg_iterator
 from torchaudio.datasets.vctk import VCTK
-from torchaudio.datasets.ljspeech import LJSPEECH
 from torchaudio.datasets.cmuarctic import CMUARCTIC
 
 from ..common_utils import (
@@ -22,10 +21,6 @@ class TestDatasets(TorchaudioTestCase):
 
     def test_librispeech(self):
         data = LIBRISPEECH(self.path, "dev-clean")
-        data[0]
-
-    def test_ljspeech(self):
-        data = LJSPEECH(self.path)
         data[0]
 
     def test_speechcommands(self):
