577796ba112228a7965c3b1ffc277c692e7b8ce9
Edward Lu <edwardhdlu@gmail.com>
2020-07-27 05:46:10 -0700

    Add test for LibriSpeech dataset (#825)
    Co-authored-by: Edward Lu <edlu@fb.com>

--- test/datasets/datasets_test.py
+++ test/datasets/datasets_test.py
@@ -1,5 +1,4 @@
 from torchaudio.datasets.commonvoice import COMMONVOICE
-from torchaudio.datasets.librispeech import LIBRISPEECH
 from torchaudio.datasets.speechcommands import SPEECHCOMMANDS
 from torchaudio.datasets.utils import diskcache_iterator, bg_iterator
 from torchaudio.datasets.vctk import VCTK
@@ -18,10 +17,6 @@ class TestDatasets(TorchaudioTestCase):
 
     def test_vctk(self):
         data = VCTK(self.path)
-        data[0]
-
-    def test_librispeech(self):
-        data = LIBRISPEECH(self.path, "dev-clean")
         data[0]
 
     def test_ljspeech(self):
