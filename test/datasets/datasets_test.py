f74ace4f5eaf8dbfdba4dbb5eecb552d44e9733b
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos580.prn2.facebook.com>
2020-07-28 04:31:31 -0700

    2020-07-28 nightly release (0a5e29dbcc46b52bad5a5148cd8fb1bce5028a4d)

--- test/datasets/datasets_test.py
+++ test/datasets/datasets_test.py
@@ -1,9 +1,6 @@
 from torchaudio.datasets.commonvoice import COMMONVOICE
-from torchaudio.datasets.librispeech import LIBRISPEECH
-from torchaudio.datasets.speechcommands import SPEECHCOMMANDS
 from torchaudio.datasets.utils import diskcache_iterator, bg_iterator
 from torchaudio.datasets.vctk import VCTK
-from torchaudio.datasets.ljspeech import LJSPEECH
 from torchaudio.datasets.cmuarctic import CMUARCTIC
 
 from ..common_utils import (
@@ -18,18 +15,6 @@ class TestDatasets(TorchaudioTestCase):
 
     def test_vctk(self):
         data = VCTK(self.path)
-        data[0]
-
-    def test_librispeech(self):
-        data = LIBRISPEECH(self.path, "dev-clean")
-        data[0]
-
-    def test_ljspeech(self):
-        data = LJSPEECH(self.path)
-        data[0]
-
-    def test_speechcommands(self):
-        data = SPEECHCOMMANDS(self.path)
         data[0]
 
     def test_cmuarctic(self):
