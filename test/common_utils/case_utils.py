11b00d5cabe9a2996cf8893d043a5f1a20ac4b2b
moto <855818+mthrok@users.noreply.github.com>
2020-08-04 14:42:39 -0400

    Fix skipIfNoExtension (#849)

--- test/common_utils/case_utils.py
+++ test/common_utils/case_utils.py
@@ -70,9 +70,8 @@ skipIfNoCuda = unittest.skipIf(not torch.cuda.is_available(), reason='CUDA not a
 
 
 def skipIfNoExtension(test_item):
-    if (
-            not is_module_available('torchaudio._torchaudio')
-            and 'TORCHAUDIO_TEST_FAIL_IF_NO_EXTENSION' in os.environ
-    ):
+    if is_module_available('torchaudio._torchaudio'):
+        return test_item
+    if 'TORCHAUDIO_TEST_FAIL_IF_NO_EXTENSION' in os.environ:
         raise RuntimeError('torchaudio C++ extension is not available.')
     return unittest.skip('torchaudio C++ extension is not available')(test_item)
