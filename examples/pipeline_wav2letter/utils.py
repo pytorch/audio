fd59a07e9df34a30ba1003206adcda6483030767
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-09-17 14:01:13 -0700

    logger now takes care of time.

--- examples/pipeline_wav2letter/utils.py
+++ examples/pipeline_wav2letter/utils.py
@@ -1,26 +1,33 @@
 import json
 import logging
-import os
 import shutil
 from collections import defaultdict
+from time import time
 
 import torch
 
 
-class MetricLogger(defaultdict):
+class Logger(defaultdict):
     def __init__(self, name, print_freq=1, disable=False):
         super().__init__(float)
         self.disable = disable
         self.print_freq = print_freq
-        self._iter = 0
-        self["name"] = name
+
+        self._name = "name"
+        self._start = "start time"
+        self._time = "current time"
+        self._iteration = "iteration"
+
+        self[self._name] = name
+        self[self._start] = time()
 
     def __str__(self):
+        self[self._time] = time()
         return json.dumps(self)
 
-    def __call__(self):
-        self._iter = (self._iter + 1) % self.print_freq
-        if not self.disable and not self._iter:
+    def flush(self):
+        self[self._iteration] += 1
+        if not self.disable and not self[self._iteration] % self.print_freq:
             print(self, flush=True)
 
 
