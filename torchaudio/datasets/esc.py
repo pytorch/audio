a03ba26b9ad7adef5740228d7d858ca2f6a90d83
leocances <leocances@gmail.com>
2020-09-18 13:28:25 +0200

    remove the dependance on pandas and refactoring

--- torchaudio/datasets/esc.py
+++ torchaudio/datasets/esc.py
@@ -1,11 +1,9 @@
 import os
-import time
 import torchaudio
 from torch import Tensor
 from torch.nn import Module
 from torch.utils.data import Dataset
-from torchaudio.datasets.utils import download_url, extract_archive, walk_files
-import pandas
+from torchaudio.datasets.utils import download_url, extract_archive
 import numpy
 from typing import Tuple
 
@@ -45,16 +43,17 @@ class ESC50(Dataset):
             not downloaded again.
     """
     NB_CLASS = 50
+
     def __init__(self,
-                root: str,
-                folds: tuple = FOLDS,
-                download: bool = False,
-                transform: Module = None) -> None:
+                 root: str,
+                 folds: tuple = FOLDS,
+                 download: bool = False,
+                 transform: Module = None) -> None:
 
         super().__init__()
 
         self.root = root
-        self.folds = folds
+        self.required_folds = folds
         self.transform = transform
 
         self.url = URL["esc-50"]
@@ -68,7 +67,12 @@ class ESC50(Dataset):
         #     raise RuntimeError("Dataset not found or corrupted. \n\
         #         You can use download=True to download it.")
 
-        self.metadata = self._load_metadata()
+        # Prepare the medata
+        self._filenames = []
+        self._folds = []
+        self._targets = []
+        self._esc10s = []
+        self._load_metadata()
 
     def __getitem__(self, index: int) -> Tuple[Tensor, int]:
         """
@@ -78,8 +82,7 @@ class ESC50(Dataset):
         Returns:
             tuple: (raw_audio, sr, target).
         """
-        filename = self.metadata.iloc[index].name
-        data, sampling_rate, target = self.load_item(filename)
+        data, sampling_rate, target = self.load_item(index)
 
         if self.transform is not None:
             data = self.transform(data)
@@ -88,24 +91,46 @@ class ESC50(Dataset):
         return data, sampling_rate, target
 
     def __len__(self) -> int:
-        return len(self.metadata)
+        return len(self._filenames)
 
-    def _load_metadata(self) -> pandas.DataFrame:
-        """Return the dataset medata into a pandas DataFrame."""
+    def _load_metadata(self) -> None:
+        """Read the metadata csv file and gather the information needed."""
+        # HEADER COLUMN NUMBER
+        c_filename = 0
+        c_fold = 1
+        c_target = 2
+        c_esc10 = 4
+
+        # Read the csv file and remove header
         path = os.path.join(self.target_directory, META_FOLDER, "esc50.csv")
-        total = pandas.read_csv(path, sep=",")
-        total = total.set_index("filename")
+        with open(path, "r") as fp:
+            data = fp.read().splitlines()[1:]
 
-        # Keep only the selected folds
-        total = total.loc[total.fold.isin(self.folds)]
+            for line in data:
+                items = line.split(",")
 
-        return total
+                self._filenames.append(items[c_filename])
+                self._folds.append(int(items[c_fold]))
+                self._targets.append(int(items[c_target]))
+                self._esc10s.append(eval(items[c_esc10]))
+
+        self._filenames = numpy.asarray(self._filenames)
+        self._folds = numpy.asarray(self._folds)
+        self._targets = numpy.asarray(self._targets)
+        self._esc10s = numpy.asarray(self._esc10s)
+
+        # Keep only the required folds
+        folds_mask = sum([self._folds == f for f in self.required_folds]) >= 1
+
+        self._filenames = self._filenames[folds_mask]
+        self._targets = self._targets[folds_mask]
+        self._esc10s = self._esc10s[folds_mask]
 
     def download(self) -> None:
         """Download the dataset and extract the archive"""
         if self.check_integrity(self.target_directory):
             print("Dataset already downloaded and verified.")
-            
+
         else:
             archive_path = os.path.join(self.root, FOLDER_IN_ARCHIVE + ".zip")
 
@@ -124,36 +149,36 @@ class ESC50(Dataset):
         # TODO add checksum verification
         return True
 
-    def load_item(self, filename) -> Tuple[Tensor, int]:
-        path = os.path.join(self.target_directory, AUDIO_FOLDER, filename)
+    def load_item(self, index: int) -> Tuple[Tensor, int]:
+        filename = self._filenames[index]
+        target = self._targets[index]
 
+        path = os.path.join(self.target_directory, AUDIO_FOLDER, filename)
         waveform, sample_rate = torchaudio.load(path)
-        target = self.metadata.at[filename, "target"]
 
         return waveform, sample_rate, target
 
 
 class ESC10(ESC50):
     TARGET_MAPPER = {0: 0, 1: 1, 38: 2, 40: 3, 41: 4, 10: 5, 11: 6, 12: 7, 20: 8, 21: 9}
-    
-    def __init__(self, 
-                root: str,
-                folds: tuple = FOLDS,
-                download: bool = False,
-                transform: Module = None) -> None:
+
+    def __init__(self,
+                 root: str,
+                 folds: tuple = FOLDS,
+                 download: bool = False,
+                 transform: Module = None) -> None:
         super().__init__(root, folds, download, transform)
 
         self.url = URL["esc-10"]
         self.nb_class = 10
         self.mapper = None  # Map the ESC-50 target to range(0, 10)
 
-    def _load_metadata(self) -> pandas.DataFrame:
-        meta = super()._load_metadata()
+    def _load_metadata(self) -> None:
+        super()._load_metadata()
 
         # Keep only the esc-10 relevant files
-        meta = meta.loc[meta.esc10 == True]
-
-        return meta
+        self._filenames = self._filenames[self._esc10s]
+        self._targets = self._targets[self._esc10s]
 
     def __getitem__(self, index: int) -> Tuple[Tensor, int]:
         data, sampling_rate, target = super().__getitem__(index)
