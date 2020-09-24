84eb2a93d2b28cd2b923a90740c8a9d029ab219c
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos494.prn2.facebook.com>
2020-09-24 04:30:37 -0700

    2020-09-24 nightly release (95d9f2d272b2814010db7fc803a7d4dc6cf0c3b4)

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-case "$(uname -s)" in
-    Darwin*) os=MacOSX;;
-    *) os=Linux
-esac
-
-
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
@@ -15,15 +9,12 @@ python -m torch.utils.collect_env
 export TORCHAUDIO_TEST_FAIL_IF_NO_EXTENSION=1
 export PATH="${PWD}/third_party/install/bin/:${PATH}"
 
-declare -a common_args=(
+declare -a args=(
+    '-v'
     '--cov=torchaudio'
     "--junitxml=${PWD}/test-results/junit.xml"
     '--durations' '20'
 )
-if [ "${os}" == MacOSX ] ; then
-    declare -a args=('-q' '-n' 'auto' '--dist=loadscope')
-else
-    declare -a args=('-v')
-fi
+
 cd test
-pytest "${args[@]}" "${common_args[@]}" torchaudio_unittest
+pytest "${args[@]}" torchaudio_unittest
