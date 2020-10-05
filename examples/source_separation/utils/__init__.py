f46b6a252e9ea25527a3ed051d9a677c2ee17711
moto <855818+mthrok@users.noreply.github.com>
2020-10-05 21:30:17 +0000

    Move test scripts to unit test

--- examples/source_separation/utils/__init__.py
+++ examples/source_separation/utils/__init__.py
@@ -1,6 +1,3 @@
 from . import (
-    dataset,
-    dataset_utils,
-    dist_utils,
     metrics,
 )
---
+++ test/torchaudio_unittest/example/souce_sepration/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+sys.path.append(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..', '..', '..', '..', 'examples', 'source_separation'))
