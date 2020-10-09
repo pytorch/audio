4f0acc588347333e53cc29e76956f001d54df973
Vincent QB <vincentqb@users.noreply.github.com>
2020-10-09 13:30:15 -0400

    fix tedlium load_audio (#934)
    and add test on other backend.

--- test/torchaudio_unittest/common_utils/backend_utils.py
+++ test/torchaudio_unittest/common_utils/backend_utils.py
@@ -6,11 +6,15 @@ import torchaudio
 def set_audio_backend(backend):
     """Allow additional backend value, 'default'"""
     backends = torchaudio.list_audio_backends()
-    if backend == 'default':
+    if backend == 'soundfile-new':
+        be = 'soundfile'
+        torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE = False
+    elif backend == 'default':
         if 'sox_io' in backends:
             be = 'sox_io'
         elif 'soundfile' in backends:
             be = 'soundfile'
+            torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE = True
         else:
             raise unittest.SkipTest('No default backend available')
     else:
