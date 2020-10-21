3b3d2ecd43163f2018fb6707025c399e65cbc17e
moto <855818+mthrok@users.noreply.github.com>
2020-10-21 02:53:52 +0000

    Remove SoxEffectsChain impl

--- torchaudio/__init__.py
+++ torchaudio/__init__.py
@@ -24,36 +24,8 @@ from torchaudio.backend import (
     SignalInfo,
     EncodingInfo,
 )
-from torchaudio.sox_effects import (
-    init_sox_effects as _init_sox_effects,
-    shutdown_sox_effects as _shutdown_sox_effects,
-)
 
 try:
     from .version import __version__, git_version  # noqa: F401
 except ImportError:
     pass
-
-
-@_mod_utils.deprecated(
-    "Please remove the function call to initialize_sox. "
-    "Resource initialization is now automatically handled.")
-def initialize_sox():
-    """Initialize sox effects.
-
-    This function is deprecated. See :py:func:`torchaudio.sox_effects.init_sox_effects`
-    """
-    _init_sox_effects()
-
-
-@_mod_utils.deprecated(
-    "Please remove the function call to torchaudio.shutdown_sox. "
-    "Resource clean up is now automatically handled. "
-    "In the unlikely event that you need to manually shutdown sox, "
-    "please use torchaudio.sox_effects.shutdown_sox_effects.")
-def shutdown_sox():
-    """Shutdown sox effects.
-
-    This function is deprecated. See :py:func:`torchaudio.sox_effects.shutdown_sox_effects`
-    """
-    _shutdown_sox_effects()
--- torchaudio/csrc/sox.h
+++ torchaudio/csrc/sox.h
@@ -44,26 +44,4 @@ void write_audio_file(
 /// error occurred during reading of the audio data.
 std::tuple<sox_signalinfo_t, sox_encodinginfo_t> get_info(
     const std::string& file_name);
-
-// Struct for build_flow_effects function
-struct SoxEffect {
-  SoxEffect() : ename(""), eopts({""})  { }
-  std::string ename;
-  std::vector<std::string> eopts;
-};
-
-/// Build a SoX chain, flow the effects, and capture the results in a tensor.
-/// An audio file from the given `path` flows through an effects chain given
-/// by a list of effects and effect options to an output buffer which is encoded
-/// into memory to a target signal type and target signal encoding.  The resulting
-/// buffer is then placed into a tensor.  This function returns the output tensor
-/// and the sample rate of the output tensor.
-int build_flow_effects(const std::string& file_name,
-                       at::Tensor otensor,
-                       bool ch_first,
-                       sox_signalinfo_t* target_signal,
-                       sox_encodinginfo_t* target_encoding,
-                       const char* file_type,
-                       std::vector<SoxEffect> pyeffs,
-                       int max_num_eopts);
 }} // namespace torch::audio
