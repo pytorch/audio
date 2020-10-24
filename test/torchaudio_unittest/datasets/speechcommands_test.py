cb228174b4470b199384275a52880930375bb60c
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-10-24 00:17:40 -0400

    add train test. fix setUpClass which should be the class method.

--- test/torchaudio_unittest/datasets/speechcommands_test.py
+++ test/torchaudio_unittest/datasets/speechcommands_test.py
@@ -53,11 +53,12 @@ class TestSpeechCommands(TempDirMixin, TorchaudioTestCase):
 
     root_dir = None
     samples = []
+    train_samples = []
     valid_samples = []
     test_samples = []
 
     @classmethod
-    def setUp(cls):
+    def setUpClass(cls):
         cls.root_dir = cls.get_base_temp_dir()
         dataset_dir = os.path.join(
             cls.root_dir, speechcommands.FOLDER_IN_ARCHIVE, speechcommands.URL
@@ -102,6 +103,8 @@ class TestSpeechCommands(TempDirMixin, TorchaudioTestCase):
                         elif 4 <= j < 6:
                             test.write(f'{label_filename}\n')
                             cls.test_samples.append(sample)
+                        else:
+                            cls.train_samples.append(sample)
 
     def testSpeechCommands(self):
         dataset = speechcommands.SPEECHCOMMANDS(self.root_dir)
@@ -119,6 +122,22 @@ class TestSpeechCommands(TempDirMixin, TorchaudioTestCase):
             num_samples += 1
 
         assert num_samples == len(self.samples)
+
+    def testSpeechCommandsSubsetTrain(self):
+        dataset = speechcommands.SPEECHCOMMANDS(self.root_dir, subset="training")
+
+        num_samples = 0
+        for i, (data, sample_rate, label, speaker_id, utterance_number) in enumerate(
+            dataset
+        ):
+            self.assertEqual(data, self.train_samples[i][0], atol=5e-5, rtol=1e-8)
+            assert sample_rate == self.train_samples[i][1]
+            assert label == self.train_samples[i][2]
+            assert speaker_id == self.train_samples[i][3]
+            assert utterance_number == self.train_samples[i][4]
+            num_samples += 1
+
+        assert num_samples == len(self.train_samples)
 
     def testSpeechCommandsSubsetValid(self):
         dataset = speechcommands.SPEECHCOMMANDS(self.root_dir, subset="validation")
