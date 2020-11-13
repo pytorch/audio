8d0c08dbc97bdbb9033a7de25f779abfcf971ee9
Matti Picus <matti.picus@gmail.com>
2020-11-13 03:39:41 +0200

    [Doc] Build docs and push to branch in CI (#1006)

---
+++ .circleci/build_docs/build_docs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+source ./packaging/pkg_helpers.bash
+export NO_CUDA_PACKAGE=1
+setup_env 0.8.0
+setup_wheel_python
+
+pushd docs
+pip install -r requirements.txt
+make html
+popd
+
