f44d9854bf3bcbdc347863d63d8c6b8462d4f9c1
krishnakalyan3 <skalyan@cloudera.com>
2020-12-12 22:10:53 +0100

    change to Path libritts

--- torchaudio/datasets/libritts.py
+++ torchaudio/datasets/libritts.py
@@ -8,7 +8,6 @@ from torch.utils.data import Dataset
 from torchaudio.datasets.utils import (
     download_url,
     extract_archive,
-    walk_files,
 )
 
 URL = "train-clean-100"
@@ -126,9 +125,7 @@ class LIBRITTS(Dataset):
                     download_url(url, root, hash_value=checksum)
                 extract_archive(archive)
 
-        walker = walk_files(
-            self._path, suffix=self._ext_audio, prefix=False, remove_suffix=True
-        )
+        walker = sorted([str(p.stem) for p in Path(lt_data._path).glob('*/*/*' + self._ext_audio)])
         self._walker = list(walker)
 
     def __getitem__(self, n: int) -> Tuple[Tensor, int, str, str, int, int, str]:
