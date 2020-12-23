b0292e3c1dee510d6acdca4ceb53f0e58b77ef4b
AzizCode92 <azizbenothman76@gmail.com>
2020-12-23 21:40:20 +0100

    remove white space from docstings

--- torchaudio/backend/_soundfile_backend.py
+++ torchaudio/backend/_soundfile_backend.py
@@ -101,7 +101,7 @@ def load(
             Otherwise, the returned Tensor's dimension is ``[time, channel]``.
 
     Returns:
-        torch.Tensor:
+        Tuple[torch.Tensor, int]: Resulting Tensor and sample rate.
             If the input file has integer wav format and normalization is off, then it has
             integer type, else ``float32`` type. If ``channels_first=True``, it has
             ``[channel, time]`` else ``[time, channel]``.
