faddb352f34c58e163e06a4e6d97dd6b6d30965b
krishnakalyan3 <skalyan@cloudera.com>
2020-12-24 21:01:53 +0100

    initial draft

--- torchaudio/datasets/yesno.py
+++ torchaudio/datasets/yesno.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 from torch import Tensor
 from torch.utils.data import Dataset
@@ -11,23 +11,14 @@ from torchaudio.datasets.utils import (
     extract_archive,
 )
 
-URL = "http://www.openslr.org/resources/1/waves_yesno.tar.gz"
-FOLDER_IN_ARCHIVE = "waves_yesno"
-_CHECKSUMS = {
-    "http://www.openslr.org/resources/1/waves_yesno.tar.gz":
-        "962ff6e904d2df1126132ecec6978786"
+
+_RELEASE_CONFIGS = {
+    "release1": {
+        "folder_in_archive": "waves_yesno",
+        "url": "http://www.openslr.org/resources/1/waves_yesno.tar.gz",
+        "checksum": "30301975fd8c5cac4040c261c0852f57cfa8adbbad2ce78e77e4986957445f27",
+    }
 }
-
-
-def load_yesno_item(fileid: str, path: str, ext_audio: str) -> Tuple[Tensor, int, List[int]]:
-    # Read label
-    labels = [int(c) for c in fileid.split("_")]
-
-    # Read wav
-    file_audio = os.path.join(path, fileid + ext_audio)
-    waveform, sample_rate = torchaudio.load(file_audio)
-
-    return waveform, sample_rate, labels
 
 
 class YESNO(Dataset):
@@ -43,52 +34,44 @@ class YESNO(Dataset):
             Whether to download the dataset if it is not found at root path. (default: ``False``).
     """
 
-    _ext_audio = ".wav"
-
-    def __init__(self,
-                 root: Union[str, Path],
-                 url: str = URL,
-                 folder_in_archive: str = FOLDER_IN_ARCHIVE,
-                 download: bool = False) -> None:
-
-        # Get string representation of 'root' in case Path object is passed
-        root = os.fspath(root)
-
+    def __init__(self, 
+                root: str,
+                url: str = _RELEASE_CONFIGS["release1"]["url"],
+                folder_in_archive: str = _RELEASE_CONFIGS["release1"]["folder_in_archive"],
+                download: bool = False):
+        self._ext_audio=".wav"
+        self._parse_filesystem(root, url, folder_in_archive, download)
+    
+    def _parse_filesystem(self, root: str, url: str, folder_in_archive: str, download: bool):
+        root = Path(root)
         archive = os.path.basename(url)
-        archive = os.path.join(root, archive)
+        archive = root / archive
+        
         self._path = os.path.join(root, folder_in_archive)
-
         if download:
             if not os.path.isdir(self._path):
                 if not os.path.isfile(archive):
                     checksum = _CHECKSUMS.get(url, None)
                     download_url(url, root, hash_value=checksum, hash_type="md5")
                 extract_archive(archive)
-
+        
         if not os.path.isdir(self._path):
             raise RuntimeError(
                 "Dataset not found. Please use `download=True` to download it."
             )
-
+        
         self._walker = sorted(str(p.stem) for p in Path(self._path).glob('*' + self._ext_audio))
-
-    def __getitem__(self, n: int) -> Tuple[Tensor, int, List[int]]:
-        """Load the n-th sample from the dataset.
-
-        Args:
-            n (int): The index of the sample to be loaded
-
-        Returns:
-            tuple: ``(waveform, sample_rate, labels)``
-        """
-        fileid = self._walker[n]
-        item = load_yesno_item(fileid, self._path, self._ext_audio)
-
-        # TODO Upon deprecation, uncomment line below and remove following code
-        # return item
-
-        waveform, sample_rate, labels = item
+    
+    def _load_item(self, fileid: str, path: str, ext_audio: str):
+        labels = [int(c) for c in fileid.split("_")]
+        file_audio = os.path.join(path, fileid + ext_audio)
+        waveform, sample_rate = torchaudio.load(file_audio)
         return waveform, sample_rate, labels
-
-    def __len__(self) -> int:
+    
+    def __getitem__(self, n: int):
+        fileid = self._walker[n]
+        item = self._load_item(fileid, self._path, self._ext_audio)
+        return item
+    
+    def __len__(self):
         return len(self._walker)
