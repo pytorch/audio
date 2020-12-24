a410474d31788efcdfffb706ad50c6c4b49fde1a
krishnakalyan3 <skalyan@cloudera.com>
2020-12-24 21:13:40 +0100

    style checks and checksum fix

--- torchaudio/datasets/yesno.py
+++ torchaudio/datasets/yesno.py
@@ -34,44 +34,44 @@ class YESNO(Dataset):
             Whether to download the dataset if it is not found at root path. (default: ``False``).
     """
 
-    def __init__(self, 
+    def __init__(self,
                 root: str,
                 url: str = _RELEASE_CONFIGS["release1"]["url"],
                 folder_in_archive: str = _RELEASE_CONFIGS["release1"]["folder_in_archive"],
                 download: bool = False):
-        self._ext_audio=".wav"
+        self._ext_audio = ".wav"
         self._parse_filesystem(root, url, folder_in_archive, download)
-    
+
     def _parse_filesystem(self, root: str, url: str, folder_in_archive: str, download: bool):
         root = Path(root)
         archive = os.path.basename(url)
         archive = root / archive
-        
+
         self._path = os.path.join(root, folder_in_archive)
         if download:
             if not os.path.isdir(self._path):
                 if not os.path.isfile(archive):
-                    checksum = _CHECKSUMS.get(url, None)
+                    checksum = _RELEASE_CONFIGS["release1"]["checksum"]
                     download_url(url, root, hash_value=checksum, hash_type="md5")
                 extract_archive(archive)
-        
+
         if not os.path.isdir(self._path):
             raise RuntimeError(
                 "Dataset not found. Please use `download=True` to download it."
             )
-        
+
         self._walker = sorted(str(p.stem) for p in Path(self._path).glob('*' + self._ext_audio))
-    
+
     def _load_item(self, fileid: str, path: str, ext_audio: str):
         labels = [int(c) for c in fileid.split("_")]
         file_audio = os.path.join(path, fileid + ext_audio)
         waveform, sample_rate = torchaudio.load(file_audio)
         return waveform, sample_rate, labels
-    
+
     def __getitem__(self, n: int):
         fileid = self._walker[n]
         item = self._load_item(fileid, self._path, self._ext_audio)
         return item
-    
+
     def __len__(self):
         return len(self._walker)
