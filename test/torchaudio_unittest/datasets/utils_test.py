43c09ed0c09da74e48e943b6930d984c301b4e46
AzizCode92 <azizbenothman76@gmail.com>
2020-12-29 02:19:09 +0100

    first commit

--- test/torchaudio_unittest/datasets/utils_test.py
+++ test/torchaudio_unittest/datasets/utils_test.py
@@ -1,6 +1,8 @@
+import torch
 from torchaudio_unittest.common_utils import (
     TorchaudioTestCase,
-    get_asset_path,
+    TempDirMixin,
+    get_asset_path
 )
 
 from torchaudio.datasets import utils as dataset_utils
@@ -9,28 +11,30 @@ from torchaudio.datasets.commonvoice import COMMONVOICE
 original_ext_audio = COMMONVOICE._ext_audio
 
 
-class TestIterator(TorchaudioTestCase):
+class Dataset(torch.utils.data.Dataset):
+    def __getitem__(self, n):
+        return torch.randn(32, 2, 256)
+
+    def __len__(self) -> int:
+        return 32
+
+
+class TestIterator(TorchaudioTestCase, TempDirMixin):
     @classmethod
     def setUpClass(cls):
-        COMMONVOICE._ext_audio = ".wav"
-
-    @classmethod
-    def tearDownClass(cls):
-        COMMONVOICE._ext_audio = original_ext_audio
+        cls.location = cls.get_base_temp_dir()
+        cls.dataset = Dataset()
 
     backend = 'default'
-    path = get_asset_path('CommonVoice', 'cv-corpus-4-2019-12-10', 'tt')
 
     def test_disckcache_iterator(self):
-        data = COMMONVOICE(self.path, url="tatar")
-        data = dataset_utils.diskcache_iterator(data)
+        data = dataset_utils.diskcache_iterator(self.dataset, location=self.location)
         # Save
         data[0]
         # Load
         data[0]
 
     def test_bg_iterator(self):
-        data = COMMONVOICE(self.path, url="tatar")
-        data = dataset_utils.bg_iterator(data, 5)
+        data = dataset_utils.bg_iterator(self.dataset, 2)
         for _ in data:
             pass
