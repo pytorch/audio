93c3025f935b9dae47ba8b8f78a8003eb3bb1ffd
Aziz <azizbenothman76@gmail.com>
2020-12-30 20:45:57 +0100

    Make Dataset utility test independent of CommonVoice (#1132)

--- test/torchaudio_unittest/datasets/utils_test.py
+++ test/torchaudio_unittest/datasets/utils_test.py
@@ -1,36 +1,37 @@
+import torch
 from torchaudio_unittest.common_utils import (
     TorchaudioTestCase,
-    get_asset_path,
+    TempDirMixin
 )
 
 from torchaudio.datasets import utils as dataset_utils
-from torchaudio.datasets.commonvoice import COMMONVOICE
-
-original_ext_audio = COMMONVOICE._ext_audio
 
 
-class TestIterator(TorchaudioTestCase):
-    @classmethod
-    def setUpClass(cls):
-        COMMONVOICE._ext_audio = ".wav"
+class Dataset(torch.utils.data.Dataset):
+    def __getitem__(self, n):
+        sample_rate = 8000
+        waveform = n * torch.ones(2, 256)
+        return waveform, sample_rate
 
-    @classmethod
-    def tearDownClass(cls):
-        COMMONVOICE._ext_audio = original_ext_audio
+    def __len__(self) -> int:
+        return 2
 
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+
+class TestIterator(TorchaudioTestCase, TempDirMixin):
     backend = 'default'
-    path = get_asset_path('CommonVoice', 'cv-corpus-4-2019-12-10', 'tt')
 
     def test_disckcache_iterator(self):
-        data = COMMONVOICE(self.path, url="tatar")
-        data = dataset_utils.diskcache_iterator(data)
+        data = dataset_utils.diskcache_iterator(Dataset(), self.get_base_temp_dir())
         # Save
         data[0]
         # Load
         data[0]
 
     def test_bg_iterator(self):
-        data = COMMONVOICE(self.path, url="tatar")
-        data = dataset_utils.bg_iterator(data, 5)
+        data = dataset_utils.bg_iterator(Dataset(), 5)
         for _ in data:
             pass
