7bd5b61c18260abbd1efbc1a547f8abeea19a4fe
Nicolas Hug <nicolashug@fb.com>
2021-01-18 10:41:15 +0000

    Update setup.py to remove 2.7 ref

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Programming Language :: C++",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Multimedia :: Sound/Audio",
