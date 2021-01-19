04480b5847b112df4794c9ed25ff72ebdfe6276d
Nicolas Hug <nicolashug@fb.com>
2021-01-19 18:42:00 +0000

    Addressed comments

--- test/torchaudio_unittest/soundfile_backend/info_test.py
+++ test/torchaudio_unittest/soundfile_backend/info_test.py
@@ -8,7 +8,6 @@ from torchaudio_unittest.common_utils import (
     skipIfNoModule,
     get_wav_data,
     save_wav,
-    sox_utils,
 )
 from .common import skipIfFormatNotSupported, parameterize
 
@@ -22,10 +21,11 @@ if _mod_utils.is_module_available("soundfile"):
 @skipIfNoModule("soundfile")
 class TestInfo(TempDirMixin, PytorchTestCase):
     @parameterize(
-        ["float32", "int32", "int16", "uint8"], [8000, 16000], [1, 2],
+        [("float32", 32), ("int32", 32), ("int16", 16), ("uint8", 8)], [8000, 16000], [1, 2],
     )
-    def test_wav(self, dtype, sample_rate, num_channels):
+    def test_wav(self, dtype_and_bit_depth, sample_rate, num_channels):
         """`soundfile_backend.info` can check wav file correctly"""
+        dtype, bits_per_sample = dtype_and_bit_depth
         duration = 1
         path = self.get_temp_path("data.wav")
         data = get_wav_data(
@@ -36,13 +36,14 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         assert info.sample_rate == sample_rate
         assert info.num_frames == sample_rate * duration
         assert info.num_channels == num_channels
-        assert info.bits_per_sample == sox_utils.get_bit_depth(dtype)
+        assert info.bits_per_sample == bits_per_sample
 
     @parameterize(
-        ["float32", "int32", "int16", "uint8"], [8000, 16000], [4, 8, 16, 32],
+        [("float32", 32), ("int32", 32), ("int16", 16), ("uint8", 8)], [8000, 16000], [1, 2],
     )
-    def test_wav_multiple_channels(self, dtype, sample_rate, num_channels):
+    def test_wav_multiple_channels(self, dtype_and_bit_depth, sample_rate, num_channels):
         """`soundfile_backend.info` can check wav file with channels more than 2 correctly"""
+        dtype, bits_per_sample = dtype_and_bit_depth
         duration = 1
         path = self.get_temp_path("data.wav")
         data = get_wav_data(
@@ -53,7 +54,7 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         assert info.sample_rate == sample_rate
         assert info.num_frames == sample_rate * duration
         assert info.num_channels == num_channels
-        assert info.bits_per_sample == sox_utils.get_bit_depth(dtype)
+        assert info.bits_per_sample == bits_per_sample
 
     @parameterize([8000, 16000], [1, 2])
     @skipIfFormatNotSupported("FLAC")
@@ -104,30 +105,29 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         assert info.num_channels == num_channels
         assert info.bits_per_sample == bits_per_sample
 
-    def test_unknown_subtype_warning(self):
-        """soundfile_backend.info issues a warning when the subtype is unknown
 
-        This will happen if a new subtype is supported in SoundFile: the _SUBTYPE_TO_BITS_PER_SAMPLE
-        dict should be updated.
-        """
+def test_unknown_subtype_warning(tmp_path, monkeypatch):
+    """soundfile_backend.info issues a warning when the subtype is unknown
 
-        soundfile_info_original = soundfile.info
+    This will happen if a new subtype is supported in SoundFile: the _SUBTYPE_TO_BITS_PER_SAMPLE
+    dict should be updated.
+    """
 
-        def info_wrapper(filepath):
-            # Wraps soundfile.info and sets the subtype to some unknown value
-            sinfo = soundfile_info_original(filepath)
-            sinfo.subtype = 'SOME_UNKNOWN_SUBTYPE'
-            return sinfo
+    soundfile_info_original = soundfile.info
 
-        mp = MonkeyPatch()
-        mp.setattr(soundfile, "info", info_wrapper)
+    def info_wrapper(filepath):
+        # Wraps soundfile.info and sets the subtype to some unknown value
+        sinfo = soundfile_info_original(filepath)
+        sinfo.subtype = 'SOME_UNKNOWN_SUBTYPE'
+        return sinfo
 
-        path = self.get_temp_path("data.wav")
-        data = get_wav_data(
-            dtype='float32', num_channels=1, normalize=False, num_frames=16000
-        )
-        save_wav(path, data, sample_rate=16000)
-        with pytest.warns(UserWarning, match="subtype is unknown to TorchAudio"):
-            info = soundfile_backend.info(path)
-        assert info.bits_per_sample == 0
-        mp.undo()
+    monkeypatch.setattr(soundfile, "info", info_wrapper)
+
+    data = get_wav_data(
+        dtype='float32', num_channels=1, normalize=False, num_frames=16000
+    )
+    path = tmp_path / 'data.wav'
+    save_wav(path, data, sample_rate=16000)
+    with pytest.warns(UserWarning, match="subtype is unknown to TorchAudio"):
+        info = soundfile_backend.info(path)
+    assert info.bits_per_sample == 0
