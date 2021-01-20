1d30db6716a2352bfc6c54debe5899d86b6d8d3d
Nicolas Hug <nicolashug@fb.com>
2021-01-20 08:48:14 +0000

    remove usage of pytest

--- test/torchaudio_unittest/soundfile_backend/info_test.py
+++ test/torchaudio_unittest/soundfile_backend/info_test.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+import warnings
+
 import torch
 from torchaudio.backend import _soundfile_backend as soundfile_backend
 from torchaudio._internal import module_utils as _mod_utils
@@ -10,8 +13,6 @@ from torchaudio_unittest.common_utils import (
     save_wav,
 )
 from .common import skipIfFormatNotSupported, parameterize
-
-import pytest
 
 if _mod_utils.is_module_available("soundfile"):
     import soundfile
@@ -104,29 +105,25 @@ class TestInfo(TempDirMixin, PytorchTestCase):
         assert info.num_channels == num_channels
         assert info.bits_per_sample == bits_per_sample
 
+    def test_unknown_subtype_warning(self):
+        """soundfile_backend.info issues a warning when the subtype is unknown
 
-def test_unknown_subtype_warning(tmp_path, monkeypatch):
-    """soundfile_backend.info issues a warning when the subtype is unknown
+        This will happen if a new subtype is supported in SoundFile: the _SUBTYPE_TO_BITS_PER_SAMPLE
+        dict should be updated.
+        """
+        dtype, sample_rate, duration, num_channels = 'float32', 16000, 1, 1
+        duration = 1
+        path = self.get_temp_path("data.wav")
+        data = get_wav_data(
+            dtype, num_channels, normalize=False, num_frames=duration * sample_rate
+        )
+        save_wav(path, data, sample_rate=16000)
 
-    This will happen if a new subtype is supported in SoundFile: the _SUBTYPE_TO_BITS_PER_SAMPLE
-    dict should be updated.
-    """
-
-    soundfile_info_original = soundfile.info
-
-    def info_wrapper(filepath):
-        # Wraps soundfile.info and sets the subtype to some unknown value
-        sinfo = soundfile_info_original(filepath)
-        sinfo.subtype = 'SOME_UNKNOWN_SUBTYPE'
-        return sinfo
-
-    monkeypatch.setattr(soundfile, "info", info_wrapper)
-
-    data = get_wav_data(
-        dtype='float32', num_channels=1, normalize=False, num_frames=16000
-    )
-    path = tmp_path / 'data.wav'
-    save_wav(path, data, sample_rate=16000)
-    with pytest.warns(UserWarning, match="subtype is unknown to TorchAudio"):
-        info = soundfile_backend.info(path)
-    assert info.bits_per_sample == 0
+        # We pretend the _SUBTYPE_TO_BITS_PER_SAMPLE dict knows no subtype / is empty.
+        # It's easier to mock the internal dict rather than to mock the entire soundfile.info function
+        with patch.dict(soundfile_backend._SUBTYPE_TO_BITS_PER_SAMPLE, {}, clear=True):
+            with warnings.catch_warnings(record=True) as w:
+                info = soundfile_backend.info(path)
+        assert len(w) == 1
+        assert "subtype is unknown to TorchAudio" in str(w[-1].message)
+        assert info.bits_per_sample == 0
