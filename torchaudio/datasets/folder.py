925adc8bdeeeeba065f9e792ba6982f7905419e4
Cassie Breviu <cassieb@Microsoft.com>
2021-02-01 08:53:04 -0600

    changed walk files to glob

--- torchaudio/datasets/folder.py
+++ torchaudio/datasets/folder.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 import torchaudio
 from torch.utils.data import Dataset
 from torch import Tensor
-from torchaudio.datasets.utils import walk_files
+from pathlib import Path
 
 def load_audio_item(filepath: str, path: str, label_name: str) -> Tuple[Tensor, int, str, str]:
     relpath = os.path.relpath(filepath, path)
@@ -17,31 +17,34 @@ class AudioFolder(Dataset):
     """Create a Dataset from Local Files.
 
     Args:
-        root (str): Path to the directory where the dataset is found or downloaded.
+        path (str): Path to the directory where the dataset is found or downloaded.
         suffix (str) : Audio file type, defaulted to ".WAV".
+        pattern (str) : Find pathnames matching this pattern. Defaulted to "*/*" 
         new_sample_rate (int) : Resample audio to new sample rate specified.
-        spectrogram_transform (bool): If True transform the audio waveform and returns it  
+        spectrogram_transform (bool): If `True` transform the audio waveform and returns it  
         transformed into a spectrogram tensor.
-        label_name (str): The label is pulled from the folders in the path, this allows you to statically
+        label (str): The label is pulled from the folders in the path, this allows you to statically
         define the label string.
     """
 
 
     def __init__(
             self,
-            root: str,
+            path: str,
             suffix: str = ".wav",
+            pattern: str = "*/*",
             new_sample_rate: int = None,
             spectrogram_transform: bool = False,
-            label_name: str = None
+            label: str = None
         ):
         
-        self._path = root
+        self._path = path
         self._spectrogram_transform = spectrogram_transform
         self._new_sample_rate = new_sample_rate
-        self._label_name = label_name
+        self._label = label
 
-        walker = walk_files(self._path, suffix=suffix, prefix=True)
+        #walker = torchaudio.datasets.utils.walk_files(self._path, suffix=suffix, prefix=True)
+        walker = sorted(str(p) for p in Path(self._path).glob(f'{pattern}{suffix}'))
         self._walker = list(walker)
 
     def __getitem__(self, n: int) -> Tuple[Tensor, int, str, str]:
@@ -55,7 +58,7 @@ class AudioFolder(Dataset):
         """
         fileid = self._walker[n]
 
-        waveform, sample_rate, label, filename =  load_audio_item(fileid, self._path, self._label_name)
+        waveform, sample_rate, label, filename =  load_audio_item(fileid, self._path, self._label)
 
         if self._new_sample_rate is not None:
             waveform = torchaudio.transforms.Resample(sample_rate, self._new_sample_rate)(waveform)
