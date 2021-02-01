f59f1ecc32d1602be045b64ca459dda1333aa7b0
Cassie Breviu <cassieb@Microsoft.com>
2021-01-31 18:29:22 -0600

    fixed args on AudioFolder

--- torchaudio/datasets/folder.py
+++ torchaudio/datasets/folder.py
@@ -19,13 +19,11 @@ class AudioFolder(Dataset):
     Args:
         root (str): Path to the directory where the dataset is found or downloaded.
         suffix (str) : Audio file type, defaulted to ".WAV".
-        transform (callable, optional): A function/transform that  takes in the audio waveform
-            and returns a transformed version. E.g, ``transforms.Spectrogram``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        loader (callable, optional): A function to load an image given its path.
-        is_valid_file (callable, optional): A function that takes path of an Image file
-            and check if the file is a valid file (used to check of corrupt files)
+        new_sample_rate (int) : Resample audio to new sample rate specified.
+        spectrogram_transform (bool): If True transform the audio waveform and returns it  
+        transformed into a spectrogram tensor.
+        label_name (str): The label is pulled from the folders in the path, this allows you to statically
+        define the label string.
     """
 
 
@@ -50,7 +48,7 @@ class AudioFolder(Dataset):
         """Load the n-th sample from the dataset.
 
         Args:
-            n (int): The index of the sample to be loaded
+            n (int): The index of the file to be loaded
 
         Returns:
             tuple: ``(waveform, sample_rate, label, filename)``
