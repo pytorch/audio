c0aad0021a26065418ed81c5c8200f9ef5f10e4f
moto <855818+mthrok@users.noreply.github.com>
2021-02-01 08:53:46 -0800

    Print CPU info before running unit test
    So that when a test fails in flaky manner, we can look into the difference.

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,6 +6,7 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
+cat /proc/cpuinfo
 export TORCHAUDIO_TEST_FAIL_IF_NO_EXTENSION=1
 export PATH="${PWD}/third_party/install/bin/:${PATH}"
 
