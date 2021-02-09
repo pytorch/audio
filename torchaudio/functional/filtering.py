7744398c7bf50abaefb2f95d0c79fdf5c17f4441
Parmeet Bhatia <bhatia.parmeet@gmail.com>
2021-02-09 10:38:09 -0800

    guarding core loop operator call for cpu device only

--- torchaudio/functional/filtering.py
+++ torchaudio/functional/filtering.py
@@ -877,8 +877,20 @@ def lfilter(
 
     input_signal_windows.div_(a_coeffs[0])
     a_coeffs_flipped.div_(a_coeffs[0])
+
+    if input_signal_windows.device.type=='cpu' and\
+       a_coeffs_flipped.device.type=='cpu' and\
+       padded_output_waveform.device.type=='cpu':
+        torch.ops.torchaudio._lfilter_core_loop(input_signal_windows, a_coeffs_flipped, padded_output_waveform)
+    else:
+        for i_sample, o0 in enumerate(input_signal_windows.t()):
+            windowed_output_signal = padded_output_waveform[
+                :, i_sample:i_sample + n_order
+            ]
+            o0.addmv_(windowed_output_signal, a_coeffs_flipped, alpha=-1)
+            padded_output_waveform[:, i_sample + n_order - 1] = o0
+
     
-    torch.ops.torchaudio._lfilter_core_loop(input_signal_windows, a_coeffs_flipped, padded_output_waveform)
 
     output = padded_output_waveform[:, n_order - 1:]
 
