d56063943cbe4d0465f9764ff33310f30ff8194e
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-02-19 11:30:16 -0800

    Add Python 3.9 support (#1242)
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Multimedia :: Sound/Audio",
         "Topic :: Scientific/Engineering :: Artificial Intelligence"
