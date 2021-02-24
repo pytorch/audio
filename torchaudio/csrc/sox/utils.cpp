086467a17259dad2dd23ff5707b43e0dceb713ff
Prabhat Roy <prabhatroy@fb.com>
2021-02-24 20:42:46 +0000

    Removed check_length from validate_input_file() (#1312)
    Some audio formats like `gsm` does not have valid frame numbers when opened. But `libsox` can properly handle these audios, so checking if `length > 0` is not necessary and too strict.

--- torchaudio/csrc/sox/utils.cpp
+++ torchaudio/csrc/sox/utils.cpp
@@ -81,15 +81,12 @@ void SoxFormat::close() {
   }
 }
 
-void validate_input_file(const SoxFormat& sf, bool check_length) {
+void validate_input_file(const SoxFormat& sf) {
   if (static_cast<sox_format_t*>(sf) == nullptr) {
     throw std::runtime_error("Error loading audio file: failed to open file.");
   }
   if (sf->encoding.encoding == SOX_ENCODING_UNKNOWN) {
     throw std::runtime_error("Error loading audio file: unknown encoding.");
-  }
-  if (check_length && sf->signal.length == 0) {
-    throw std::runtime_error("Error reading audio file: unknown length.");
   }
 }
 
