33dc817c60b957e9c929ed6071d4d21fd00e4845
Prabhat Roy <prabhatroy@fb.com>
2021-02-24 21:22:37 +0000

    Remove legacy backends (#1311)

--- torchaudio/backend/utils.py
+++ torchaudio/backend/utils.py
@@ -6,10 +6,8 @@ import torchaudio
 from torchaudio._internal.module_utils import is_module_available
 from . import (
     no_backend,
-    sox_backend,
     sox_io_backend,
     soundfile_backend,
-    _soundfile_backend,
 )
 
 __all__ = [
@@ -29,7 +27,6 @@ def list_audio_backends() -> List[str]:
     if is_module_available('soundfile'):
         backends.append('soundfile')
     if is_module_available('torchaudio._torchaudio'):
-        backends.append('sox')
         backends.append('sox_io')
     return backends
 
@@ -39,15 +36,9 @@ def set_audio_backend(backend: Optional[str]):
 
     Args:
         backend (Optional[str]): Name of the backend.
-            One of ``"sox"``, ``"sox_io"`` or ``"soundfile"`` based on availability
+            One of ``"sox_io"`` or ``"soundfile"`` based on availability
             of the system. If ``None`` is provided the  current backend is unassigned.
     """
-    if torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE is not None:
-        warnings.warn(
-            '"torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE" flag is deprecated and will be removed in 0.9.0. '
-            'Please remove the use of flag.'
-        )
-
     if backend is not None and backend not in list_audio_backends():
         raise RuntimeError(
             f'Backend "{backend}" is not one of '
@@ -55,23 +46,10 @@ def set_audio_backend(backend: Optional[str]):
 
     if backend is None:
         module = no_backend
-    elif backend == 'sox':
-        warnings.warn(
-            '"sox" backend is deprecated and will be removed in 0.9.0. '
-            'Please use "sox_io" backend.'
-        )
-        module = sox_backend
     elif backend == 'sox_io':
         module = sox_io_backend
     elif backend == 'soundfile':
-        if torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE:
-            warnings.warn(
-                'The legacy interface of "soundfile" backend is deprecated and will be removed in 0.9.0. '
-                'Please migrate to the new interface.'
-            )
-            module = soundfile_backend
-        else:
-            module = _soundfile_backend
+        module = soundfile_backend
     else:
         raise NotImplementedError(f'Unexpected backend "{backend}"')
 
@@ -98,10 +76,8 @@ def get_audio_backend() -> Optional[str]:
     """
     if torchaudio.load == no_backend.load:
         return None
-    if torchaudio.load == sox_backend.load:
-        return 'sox'
     if torchaudio.load == sox_io_backend.load:
         return 'sox_io'
-    if torchaudio.load in [soundfile_backend.load, _soundfile_backend.load]:
+    if torchaudio.load == soundfile_backend.load:
         return 'soundfile'
     raise ValueError('Unknown backend.')
