aea8cc4777ce250569975635fe5b2584f2342369
Chin Yun Yu <ya70201@gmail.com>
2021-02-25 11:45:38 +0800

    refactor autograd test

---
+++ test/torchaudio_unittest/lfilter_autograd_cpu_test.py
@@ -0,0 +1,9 @@
+import torch
+from .lfilter_autograd_impl import Autograd
+from torchaudio_unittest import common_utils
+
+
+@common_utils.skipIfNoExtension
+class TestAutogradLfilterCPU(Autograd, common_utils.PytorchTestCase):
+    dtype = torch.float64
+    device = torch.device('cpu')
