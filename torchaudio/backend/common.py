4fe246e0d3f68738780924281eef0a007a5bb790
Artyom Astafurov <asta@fb.com>
2021-02-26 15:01:51 -0500

    Update AudioMetaData docstring with encoding values

--- torchaudio/backend/common.py
+++ torchaudio/backend/common.py
@@ -9,7 +9,21 @@ class AudioMetaData:
     :ivar int num_channels: The number of channels
     :ivar int bits_per_sample: The number of bits per sample. This is 0 for lossy formats,
         or when it cannot be accurately inferred.
-    :ivar str encoding: Audio encoding.
+    :ivar str encoding: Audio encoding
+        The values encoding can take are one of the following:
+
+            * ``PCM_S``: Signed integer linear PCM
+            * ``PCM_U``: Unsigned integer linear PCM
+            * ``PCM_F``: Floating point linear PCM
+            * ``FLAC``: Flac, Free Lossless Audio Codec
+            * ``ULAW``: Mu-law, [wikipedia]
+            * ``ALAW``: A-law [wikipedia]
+            * ``MP3`` : MP3, MPEG-1 Audio Layer III
+            * ``VORBIS``: OGG Vorbis [xiph.org]
+            * ``AMR_WB``: Adaptive Multi-Rate [wikipedia]
+            * ``AMR_NB``: Adaptive Multi-Rate Wideband [wikipedia]
+            * ``OPUS``: Opus [opus-codec.org]
+            * ``UNKNOWN`` : None of avobe
     """
     def __init__(
             self,
