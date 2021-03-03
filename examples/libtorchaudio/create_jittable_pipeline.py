3bd3d821fc1896de20c9d5f155c7938aab8bf624
moto <855818+mthrok@users.noreply.github.com>
2021-03-03 23:11:26 +0000

    Update documents

--- examples/libtorchaudio/create_jittable_pipeline.py
+++ examples/libtorchaudio/create_jittable_pipeline.py
@@ -9,39 +9,59 @@ import torch
 import torchaudio
 
 
+class Pipeline(torch.nn.Module):
+    """Example audio process pipeline.
+
+    This example load waveform from a file then apply effects and save it to a file.
+    """
+    def __init__(self):
+        super().__init__()
+        rir, sample_rate = _load_rir()
+        self.register_buffer('rir', rir)
+        self.rir_sample_rate: int = sample_rate
+
+    def forward(self, input_path: str, output_path: str):
+        torchaudio.sox_effects.init_sox_effects()
+
+        # 1. load audio
+        waveform, sample_rate = torchaudio.load(input_path)
+
+        # 2. Add background noise
+        alpha = 0.01
+        waveform = alpha * torch.randn_like(waveform) + (1 - alpha) * waveform
+
+        # 3. Reample the RIR filter to much the audio sample rate
+        rir, _ = torchaudio.sox_effects.apply_effects_tensor(
+            self.rir, self.rir_sample_rate, effects=[["rate", str(sample_rate)]])
+        rir = rir / torch.norm(rir, p=2)
+        rir = torch.flip(rir, [1])
+
+        # 4. Apply RIR filter
+        waveform = torch.nn.functional.pad(waveform, (rir.shape[1] - 1, 0))
+        waveform = torch.nn.functional.conv1d(waveform[None, ...], rir[None, ...])[0]
+
+        # Save
+        torchaudio.save(output_path, waveform, sample_rate)
+
+
+def _create_jit_pipeline(output_path):
+    module = torch.jit.script(Pipeline())
+    print("*" * 40)
+    print("* Pipeline code")
+    print("*" * 40)
+    print()
+    print(module.code)
+    print("*" * 40)
+    module.save(output_path)
+
+
 def _get_path(*paths):
     return os.path.join(os.path.dirname(__file__), *paths)
 
 
 def _load_rir():
     path = _get_path("data", "rir.wav")
-    waveform, sample_rate = torchaudio.load(path)
-    rir = torch.flip(waveform, [1])
-    return rir, sample_rate
-
-
-class Pipeline(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        rir, sample_rate = _load_rir()
-        self.register_buffer('rir', rir)
-        self.sample_rate: int = sample_rate
-
-    def forward(self, input_path: str, output_path: str):
-        torchaudio.sox_effects.init_sox_effects()
-        waveform, _ = torchaudio.sox_effects.apply_effects_file(
-            input_path, effects=[["rate", str(self.sample_rate)]])
-
-        waveform = torch.nn.functional.pad(waveform, (self.rir.shape[1]-1, 0))
-        augmented = torch.nn.functional.conv1d(waveform[None, ...], self.rir[None, ...])[0]
-
-        torchaudio.save(output_path, augmented, self.sample_rate)
-
-
-def _create_jit_pipeline(output_path):
-    module = torch.jit.script(Pipeline())
-    print(module.code)
-    module.save(output_path)
+    return torchaudio.load(path)
 
 
 def _parse_args():
