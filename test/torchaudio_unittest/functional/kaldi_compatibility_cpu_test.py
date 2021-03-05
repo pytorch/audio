162f49e82a5c2a8d27ae3190aed61603072bb9e3
Jeff Hwang <jeffhwang@fb.com>
2021-03-04 22:47:14 -0800

    Refactor Kaldi compatibility tests
    [ghstack-poisoned]

--- test/torchaudio_unittest/functional/kaldi_compatibility_cpu_test.py
+++ test/torchaudio_unittest/functional/kaldi_compatibility_cpu_test.py
@@ -1,9 +1,19 @@
 import torch
 
 from torchaudio_unittest.common_utils import PytorchTestCase
-from .kaldi_compatibility_test_impl import KaldiCPUOnly
+from .kaldi_compatibility_test_impl import Kaldi, KaldiCPUOnly
 
 
 class TestKaldiCPUOnly(KaldiCPUOnly, PytorchTestCase):
     dtype = torch.float32
+    device = torch.device('cpu')
+
+
+class TestKaldiFloat32(Kaldi, PytorchTestCase):
+    dtype = torch.float32
+    device = torch.device('cpu')
+
+
+class TestKaldiFloat64(Kaldi, PytorchTestCase):
+    dtype = torch.float64
     device = torch.device('cpu')
--- test/torchaudio_unittest/functional/kaldi_compatibility_test_impl.py
+++ test/torchaudio_unittest/functional/kaldi_compatibility_test_impl.py
@@ -1,4 +1,5 @@
 from parameterized import parameterized
+import torch
 import torchaudio.functional as F
 
 from torchaudio_unittest.common_utils import (
@@ -13,6 +14,24 @@ from torchaudio_unittest.common_utils.kaldi_utils import (
     convert_args,
     run_kaldi,
 )
+
+
+class Kaldi(TempDirMixin, TestBaseMixin):
+    @skipIfNoExec('apply-cmvn-sliding')
+    def test_sliding_window_cmn(self):
+        """sliding_window_cmn should be numerically compatible with apply-cmvn-sliding"""
+        kwargs = {
+            'cmn_window': 600,
+            'min_cmn_window': 100,
+            'center': False,
+            'norm_vars': False,
+        }
+
+        tensor = torch.randn(40, 10, dtype=self.dtype, device=self.device)
+        result = F.sliding_window_cmn(tensor, **kwargs)
+        command = ['apply-cmvn-sliding'] + convert_args(**kwargs) + ['ark:-', 'ark:-']
+        kaldi_result = run_kaldi(command, 'ark', tensor)
+        self.assert_equal(result, expected=kaldi_result)
 
 
 class KaldiCPUOnly(TempDirMixin, TestBaseMixin):
