617d23d1cecd804884e2fbea77f2edd324ab0aca
Chin Yun Yu <ya70201@gmail.com>
2021-03-06 14:51:30 +0800

    feat: enable autograd unittest on windows platform

--- test/torchaudio_unittest/functional/autograd_cuda_test.py
+++ test/torchaudio_unittest/functional/autograd_cuda_test.py
@@ -1,11 +1,8 @@
 import torch
 from .autograd_impl import Autograd
 from torchaudio_unittest import common_utils
-import platform
-import unittest
 
 
-@unittest.skipIf(platform.system() == 'Windows', "not supported on windows")
 @common_utils.skipIfNoCuda
 class TestAutogradLfilterCUDA(Autograd, common_utils.PytorchTestCase):
     dtype = torch.float64
