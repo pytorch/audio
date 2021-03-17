0cb1e3d25a10413bc322b7b07c741c361a07bfb0
moto <855818+mthrok@users.noreply.github.com>
2021-03-17 16:53:08 +0000

    Add nondet_tol for spectrogram

--- test/torchaudio_unittest/transforms/autograd_test_impl.py
+++ test/torchaudio_unittest/transforms/autograd_test_impl.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from parameterized import parameterized
 import torch
 from torch.autograd import gradcheck, gradgradcheck
@@ -10,7 +12,13 @@ from torchaudio_unittest.common_utils import (
 
 
 class AutogradTestMixin(TestBaseMixin):
-    def assert_grad(self, transform, *inputs):
+    def assert_grad(
+            self,
+            transform: torch.nn.Module,
+            inputs: List[torch.Tensor],
+            *,
+            nondet_tol: float = 0.0,
+    ):
         transform = transform.to(dtype=torch.float64, device=self.device)
 
         inputs_ = []
@@ -18,7 +26,7 @@ class AutogradTestMixin(TestBaseMixin):
             i.requires_grad = True
             inputs_.append(i.to(dtype=torch.float64, device=self.device))
         assert gradcheck(transform, inputs_)
-        assert gradgradcheck(transform, inputs_)
+        assert gradgradcheck(transform, inputs_, nondet_tol=nondet_tol)
 
     @parameterized.expand([
         ({'pad': 0, 'normalized': False, 'power': None}, ),
@@ -35,12 +43,20 @@ class AutogradTestMixin(TestBaseMixin):
         ({'pad': 3, 'normalized': True, 'power': 2.0}, ),
     ])
     def test_spectrogram(self, kwargs):
+        # replication_pad1d_backward_cuda is not deteministic and
+        # gives very small (~2.7756e-17) difference.
+        #
+        # See https://github.com/pytorch/pytorch/issues/54093
         transform = T.Spectrogram(**kwargs)
         waveform = get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2)
-        self.assert_grad(transform, waveform)
+        self.assert_grad(transform, [waveform], nondet_tol=1e-10)
 
     def test_melspectrogram(self):
+        # replication_pad1d_backward_cuda is not deteministic and
+        # gives very small (~2.7756e-17) difference.
+        #
+        # See https://github.com/pytorch/pytorch/issues/54093
         sample_rate = 8000
         transform = T.MelSpectrogram(sample_rate=sample_rate)
         waveform = get_whitenoise(sample_rate=sample_rate, duration=0.05, n_channels=2)
-        self.assert_grad(transform, waveform)
+        self.assert_grad(transform, [waveform], nondet_tol=1e-10)
