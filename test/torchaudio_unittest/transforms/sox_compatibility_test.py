512c2fa46d112fde4b6a35fda20944d4bf33cec2
Krishna Kalyan <krishnakalyan3@gmail.com>
2021-03-29 17:25:43 +0200

    Remove skipIfNoSoxBackend (#1390)
    Co-authored-by: krishnakalyan3 <skalyan@cloudera.com>

--- test/torchaudio_unittest/transforms/sox_compatibility_test.py
+++ test/torchaudio_unittest/transforms/sox_compatibility_test.py
@@ -2,17 +2,19 @@ import torchaudio.transforms as T
 from parameterized import parameterized
 
 from torchaudio_unittest.common_utils import (
-    skipIfNoSoxBackend,
+    skipIfNoSox,
     skipIfNoExec,
     TempDirMixin,
     TorchaudioTestCase,
     get_asset_path,
     sox_utils,
     load_wav,
+    save_wav,
+    get_whitenoise,
 )
 
 
-@skipIfNoSoxBackend
+@skipIfNoSox
 @skipIfNoExec('sox')
 class TestFunctionalFiltering(TempDirMixin, TorchaudioTestCase):
     def run_sox_effect(self, input_file, effect):
@@ -23,6 +25,14 @@ class TestFunctionalFiltering(TempDirMixin, TorchaudioTestCase):
     def assert_sox_effect(self, result, input_path, effects, atol=1e-04, rtol=1e-5):
         expected, _ = self.run_sox_effect(input_path, effects)
         self.assertEqual(result, expected, atol=atol, rtol=rtol)
+
+    def get_whitenoise(self, sample_rate=8000):
+        noise = get_whitenoise(
+            sample_rate=sample_rate, duration=3, scale_factor=0.9,
+        )
+        path = self.get_temp_path("whitenoise.wav")
+        save_wav(path, noise, sample_rate)
+        return noise, path
 
     @parameterized.expand([
         ('q', 'quarter_sine'),
