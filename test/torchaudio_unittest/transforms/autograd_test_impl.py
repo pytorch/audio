39adb063f616a610c2a7595bf4d1e157193a83d1
krishnakalyan3 <skalyan@cloudera.com>
2021-04-01 17:49:02 +0200

    MFCC grad test

--- test/torchaudio_unittest/transforms/autograd_test_impl.py
+++ test/torchaudio_unittest/transforms/autograd_test_impl.py
@@ -60,3 +60,9 @@ class AutogradTestMixin(TestBaseMixin):
         transform = T.MelSpectrogram(sample_rate=sample_rate)
         waveform = get_whitenoise(sample_rate=sample_rate, duration=0.05, n_channels=2)
         self.assert_grad(transform, [waveform], nondet_tol=1e-10)
+
+    def test_melspectrogram(self):
+        sample_rate = 8000
+        transform = T.MFCC(sample_rate=sample_rate)
+        waveform = get_whitenoise(sample_rate=sample_rate, duration=0.05, n_channels=2)
+        self.assert_grad(transform, [waveform])
