31c57c3296afe736059f798ecef1382c90b4c112
krishnakalyan3 <krishnakalyan3@gmail.com>
2021-04-05 19:45:52 +0200

    fade param

--- test/torchaudio_unittest/transforms/autograd_test_impl.py
+++ test/torchaudio_unittest/transforms/autograd_test_impl.py
@@ -78,7 +78,8 @@ class AutogradTestMixin(TestBaseMixin):
         waveform = get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2)
         self.assert_grad(transform, [waveform])
 
-    def test_fade(self):
-        transform = T.Fade()
+    @parameterized.expand([("linear", ), ("exponential", ), ("logarithmic", ), ("quarter_sine", ), ("half_sine", )])
+    def test_fade(self, fade_shape):
+        transform = T.Fade(fade_shape=fade_shape)
         waveform = get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2)
         self.assert_grad(transform, [waveform], nondet_tol=1e-10)
