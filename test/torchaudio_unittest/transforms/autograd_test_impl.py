25f846c534f888174c305b3daadce53916e80a8d
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared39039.03.ftw1.facebook.com>
2021-04-06 04:30:28 -0700

    2021-04-06 nightly release (c6eeebeb7e39c84d5834372d5a5b3416688d8fd9)

--- test/torchaudio_unittest/transforms/autograd_test_impl.py
+++ test/torchaudio_unittest/transforms/autograd_test_impl.py
@@ -71,3 +71,26 @@ class AutogradTestMixin(TestBaseMixin):
         transform = T.MFCC(sample_rate=sample_rate, log_mels=log_mels)
         waveform = get_whitenoise(sample_rate=sample_rate, duration=0.05, n_channels=2)
         self.assert_grad(transform, [waveform])
+
+    def test_compute_deltas(self):
+        transform = T.ComputeDeltas()
+        spec = torch.rand(10, 20)
+        self.assert_grad(transform, [spec])
+
+    @parameterized.expand([(8000, 8000), (8000, 4000), (4000, 8000)])
+    def test_resample(self, orig_freq, new_freq):
+        transform = T.Resample(orig_freq=orig_freq, new_freq=new_freq)
+        waveform = get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2)
+        self.assert_grad(transform, [waveform])
+
+    @parameterized.expand([("linear", ), ("exponential", ), ("logarithmic", ), ("quarter_sine", ), ("half_sine", )])
+    def test_fade(self, fade_shape):
+        transform = T.Fade(fade_shape=fade_shape)
+        waveform = get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2)
+        self.assert_grad(transform, [waveform], nondet_tol=1e-10)
+
+    def test_spectral_centroid(self):
+        sample_rate = 8000
+        transform = T.SpectralCentroid(sample_rate=sample_rate)
+        waveform = get_whitenoise(sample_rate=sample_rate, duration=0.05, n_channels=2)
+        self.assert_grad(transform, [waveform], nondet_tol=1e-10)
