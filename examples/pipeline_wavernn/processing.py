e061b268d284a6f43acc44314b83ec6ffe58e91a
discort <odiscort@gmail.com>
2021-04-16 02:04:32 +0300

    Use torchaudio melscale 'slaney' instead of librosa in WaveRNN pipeline preprocessing (#1444)
    * Use torchaudio melscale instead of librosa

--- examples/pipeline_wavernn/processing.py
+++ examples/pipeline_wavernn/processing.py
@@ -1,30 +1,5 @@
-import librosa
 import torch
 import torch.nn as nn
-
-
-# TODO Replace by torchaudio, once https://github.com/pytorch/audio/pull/593 is resolved
-class LinearToMel(nn.Module):
-    def __init__(self, sample_rate, n_fft, n_mels, fmin, htk=False, norm="slaney"):
-        super().__init__()
-        self.sample_rate = sample_rate
-        self.n_fft = n_fft
-        self.n_mels = n_mels
-        self.fmin = fmin
-        self.htk = htk
-        self.norm = norm
-
-    def forward(self, specgram):
-        specgram = librosa.feature.melspectrogram(
-            S=specgram.squeeze(0).numpy(),
-            sr=self.sample_rate,
-            n_fft=self.n_fft,
-            n_mels=self.n_mels,
-            fmin=self.fmin,
-            htk=self.htk,
-            norm=self.norm,
-        )
-        return torch.from_numpy(specgram)
 
 
 class NormalizeDB(nn.Module):
@@ -37,7 +12,7 @@ class NormalizeDB(nn.Module):
         self.normalization = normalization
 
     def forward(self, specgram):
-        specgram = torch.log10(torch.clamp(specgram, min=1e-5))
+        specgram = torch.log10(torch.clamp(specgram.squeeze(0), min=1e-5))
         if self.normalization:
             return torch.clamp(
                 (self.min_level_db - 20 * specgram) / self.min_level_db, min=0, max=1
