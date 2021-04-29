4d98df7bccfd5516503fd63c4635e19e2bcbe393
Caroline Chen <carolinechen@fb.com>
2021-04-29 16:03:29 -0700

    Resolve build warnings

--- torchaudio/csrc/rnnt/macros.h
+++ torchaudio/csrc/rnnt/macros.h
@@ -19,10 +19,3 @@
 typedef enum { INFO = 0, WARNING = 1, ERROR = 2, FATAL = 3 } level_t;
 
 const char* ToString(level_t level);
-
-#define DCHECK(x)
-#define DCHECK_EQ(x, y)
-#define DCHECK_NE(x, y)
-#define CHECK(x)
-#define CHECK_EQ(x, y)
-#define CHECK_NE(x, y)
