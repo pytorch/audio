5c65489edd15e3b4db9e3869f1b6a5ef9c380bf8
Caroline Chen <carolinechen@fb.com>
2021-05-10 10:57:14 -0700

    update warning message

--- torchaudio/functional/functional.py
+++ torchaudio/functional/functional.py
@@ -1389,12 +1389,15 @@ def resample(
 
     if not (int(orig_freq) == orig_freq and int(new_freq) == new_freq):
         warnings.warn(
-            "Non-integer frequencies are cast to integers and may result in poor resampling quality."
-            "The algorithm is dependent on the integer ratio between `orig_freq` and `new_freq`,"
-            "so please manually convert frequencies to integer values that maintain the ratio"
-            "prior to passing them into the function"
-            "Example: To downsample a 44100 hz waveform by a factor of 8, you can use"
-            "`orig_freq=8` and `new_freq=1` instead of `orig_freq=44100` and `new_freq=5512.5`"
+            "Non-integer frequencies are being cast to ints and may result in poor resampling quality "
+            "because the underlying algorithm requires an integer ratio between `orig_freq` and `new_freq`. "
+            "Using non-integer valued frequencies will throw an error in the next release. "
+            "To work around this issue, manually convert both frequencies to integer values "
+            "that maintain their resampling rate ratio before passing them into the function "
+            "Example: To downsample a 44100 hz waveform by a factor of 8, use "
+            "`orig_freq=8` and `new_freq=1` instead of `orig_freq=44100` and `new_freq=5512.5` "
+            "For more information or to leave feedback about this change, please refer to "
+            "https://github.com/pytorch/audio/issues/1487."
         )
 
     orig_freq = int(orig_freq)
