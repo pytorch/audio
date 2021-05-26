05aae0578db0055c9bc3c4cdaaf0271becab798b
moto <855818+mthrok@users.noreply.github.com>
2021-05-26 03:00:13 +0000

    Add huggingface importer

--- torchaudio/models/wav2vec2/__init__.py
+++ torchaudio/models/wav2vec2/__init__.py
@@ -4,10 +4,12 @@ from .model import (
     wav2vec2_large,
     wav2vec2_large_lv60k,
 )
+from . import utils
 
 __all__ = [
     'Wav2Vec2Model',
     'wav2vec2_base',
     'wav2vec2_large',
     'wav2vec2_large_lv60k',
+    'utils',
 ]
