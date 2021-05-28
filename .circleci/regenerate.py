3bb3287f49e0a389bcf3860fc595a5bca7b39a10
Michael Melesse <micmelesse@gmail.com>
2021-05-27 17:51:58 -0700

    [ROCm] add rocm wheels (#1477)
    * add rocm binary
    
    use the term cu_version
    
    update pkg_helpers
    
    add docker wheel option
    
    update config yml
    
    add force_cuda option
    
    * update config.yml
    
    * remove conda builds

--- .circleci/regenerate.py
+++ .circleci/regenerate.py
@@ -21,7 +21,7 @@ import os.path
 
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
-CU_VERSIONS_DICT = {"linux": ["cpu", "cu102", "cu111"],
+CU_VERSIONS_DICT = {"linux": ["cpu", "cu102", "cu111","rocm4.1"],
                     "windows": ["cpu", "cu102", "cu111"],
                     "macos": ["cpu"]}
 
@@ -36,7 +36,10 @@ def build_workflows(prefix='', upload=False, filter_branch=None, indentation=6):
         for os_type in ["linux", "macos", "windows"]:
             for python_version in PYTHON_VERSIONS:
                 for cu_version in CU_VERSIONS_DICT[os_type]:
-                    w += build_workflow_pair(btype, os_type, python_version, cu_version, filter_branch, prefix, upload)
+                    if cu_version.startswith("rocm") and btype=="conda":
+                        pass
+                    else:
+                        w += build_workflow_pair(btype, os_type, python_version, cu_version, filter_branch, prefix, upload)
 
     if not filter_branch:
         # Build on every pull request, but upload only on nightly and tags
@@ -124,8 +127,10 @@ def generate_base_workflow(base_workflow_name, python_version, cu_version, filte
         d['requires'] = ['download_third_parties_nix']
     if btype == 'conda':
         d['conda_docker_image'] = f'pytorch/conda-builder:{cu_version.replace("cu1","cuda1")}'
-    elif cu_version != 'cpu':
+    elif cu_version.startswith('cu'):
         d['wheel_docker_image'] = f'pytorch/manylinux-{cu_version.replace("cu1","cuda1")}'
+    elif cu_version.startswith('rocm'):
+        d["wheel_docker_image"] = f"pytorch/manylinux-rocm:{cu_version[len('rocm'):]}"
 
     if filter_branch:
         d["filters"] = gen_filter_branch_tree(filter_branch)
