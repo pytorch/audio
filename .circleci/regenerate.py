e21ffa646dde3efe2f9a870524f90753b8111c66
Nikita Shulga <nshulga@fb.com>
2021-06-01 12:58:09 -0700

    Disable GPU builds for release/0.9

--- .circleci/regenerate.py
+++ .circleci/regenerate.py
@@ -21,8 +21,8 @@ import os.path
 
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
-CU_VERSIONS_DICT = {"linux": ["cpu", "cu102", "cu111","rocm4.1"],
-                    "windows": ["cpu", "cu102", "cu111"],
+CU_VERSIONS_DICT = {"linux": ["cpu"],
+                    "windows": ["cpu"],
                     "macos": ["cpu"]}
 
 
