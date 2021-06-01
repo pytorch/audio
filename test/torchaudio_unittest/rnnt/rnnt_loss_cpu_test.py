08750508c380e104834b0f59523170e00bdf0eab
Caroline Chen <carolinechen@fb.com>
2021-06-01 13:14:55 -0700

    remove prototype from 0.9 release

--- test/torchaudio_unittest/rnnt/rnnt_loss_cpu_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchaudio_unittest import common_utils
-from .utils import skipIfNoTransducer
-from .rnnt_loss_impl import RNNTLossTest
-
-
-@skipIfNoTransducer
-class TestRNNTLoss(RNNTLossTest, common_utils.PytorchTestCase):
-    device = torch.device('cpu')
