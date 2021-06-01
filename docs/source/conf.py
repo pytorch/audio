5932ec1e38cd9f5be69de2f389ba5729f2a3f77a
moto <855818+mthrok@users.noreply.github.com>
2021-06-01 18:08:54 +0000

    Fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import torchaudio
 import pytorch_sphinx_theme
 
 # -- General configuration ------------------------------------------------
