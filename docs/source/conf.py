afb6626c440e92423549818401afa54f4addfb39
moto <855818+mthrok@users.noreply.github.com>
2021-06-04 18:33:00 -0400

    Remove unnecessary override CSS (#1554)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,7 +137,6 @@ html_logo = '_static/img/pytorch-logo-dark.svg'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = [
-    'css/override.css',
     'https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css'
 ]
 
