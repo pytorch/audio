0c60dffa847a9cdeb1bb21ae18e2fee9698c3434
Caroline Chen <carolinechen@fb.com>
2021-06-10 15:27:11 -0700

    0.9.0 generate new docs

--- 0.9.0/_static/documentation_options.js
+++ 0.9.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0a0+fc2d10f',
+    VERSION: '0.9.0a0+1437208',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
