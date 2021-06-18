f3872ae6af06f021779bdb2af0820ad616c6260b
harish <harishsdev@gmail.com>
2021-06-18 09:34:44 +0530

    modified for space

--- torchaudio/transforms.py
+++ torchaudio/transforms.py
@@ -71,7 +71,7 @@ class Spectrogram(torch.nn.Module):
             Example
 
             >>> waveform, sample_rate = torchaudio.load('test.wav', normalization=True)
-            >>> trans = torchaudio.transforms.Spectrogram(n_fft=800)(waveform)
+            >>> trans = torchaudio.transforms.Spectrogram(n_fft = 800)(waveform)
     """
     __constants__ = ['n_fft', 'win_length', 'hop_length', 'pad', 'power', 'normalized']
 
