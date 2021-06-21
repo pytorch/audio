4a885fbfc4438b3428c261e9ea437294106cb05a
moto <855818+mthrok@users.noreply.github.com>
2021-06-21 21:09:42 +0000

    Pin nightly version on Linux/macOS unittest

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -23,20 +23,33 @@ eval "$("${conda_dir}/bin/conda" shell.bash hook)"
 conda activate "${env_dir}"
 
 # 1. Install PyTorch
-if [ -z "${CUDA_VERSION:-}" ] ; then
-    if [ "${os}" == MacOSX ] ; then
-        cudatoolkit=''
-    else
-        cudatoolkit="cpuonly"
-    fi
+# if [ -z "${CUDA_VERSION:-}" ] ; then
+#     if [ "${os}" == MacOSX ] ; then
+#         cudatoolkit=''
+#     else
+#         cudatoolkit="cpuonly"
+#     fi
+# else
+#     version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
+#     cudatoolkit="cudatoolkit=${version}"
+# fi
+# printf "Installing PyTorch with %s\n" "${cudatoolkit}"
+# (
+#     set -x
+#     conda install ${CONDA_CHANNEL_FLAGS:-} -y -c "pytorch-${UPLOAD_CHANNEL}" "pytorch-${UPLOAD_CHANNEL}::pytorch" ${cudatoolkit}
+# )
+
+
+if [ "${os}" == MacOSX ] | [ -z "${CUDA_VERSION:-}" ] ; then
+    device="cpu"
 else
-    version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
-    cudatoolkit="cudatoolkit=${version}"
+    version="$(python -c "print(''.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
+    device=cu"${version}"
 fi
-printf "Installing PyTorch with %s\n" "${cudatoolkit}"
+printf "Installing PyTorch with %s\n" "$device}"
 (
     set -x
-    conda install ${CONDA_CHANNEL_FLAGS:-} -y -c "pytorch-${UPLOAD_CHANNEL}" "pytorch-${UPLOAD_CHANNEL}::pytorch" ${cudatoolkit}
+    pip install --pre torch==1.10.0.dev20210618 -f "https://download.pytorch.org/whl/nightly/${device}/torch_nightly.html"
 )
 
 # 2. Install torchaudio
