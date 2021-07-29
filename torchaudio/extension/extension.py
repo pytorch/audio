a577d7fe1327677646f71228db919d03b49a7de6
moto <855818+mthrok@users.noreply.github.com>
2021-07-29 15:21:02 +0000

    Simplify extension initialization

--- torchaudio/extension/extension.py
+++ torchaudio/extension/extension.py
@@ -1,20 +1,17 @@
 import warnings
-import importlib
 
 import torch
 from torchaudio._internal import module_utils as _mod_utils
 
 
 def _init_extension():
-    ext = 'torchaudio._torchaudio'
-    if _mod_utils.is_module_available(ext):
-        _init_script_module(ext)
+    if _mod_utils.is_module_available('torchaudio._torchaudio'):
         import torchaudio._torchaudio  # noqa
+        _init_script_module(torchaudio._torchaudio.__file__)
     else:
         warnings.warn('torchaudio C++ extension is not available.')
 
 
-def _init_script_module(module):
-    path = importlib.util.find_spec(module).origin
+def _init_script_module(path):
     torch.classes.load_library(path)
     torch.ops.load_library(path)
