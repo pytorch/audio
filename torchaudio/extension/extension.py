fcd077c254fc943f7e791a0fab46f506270977a8
moto <855818+mthrok@users.noreply.github.com>
2021-08-02 16:57:19 +0000

    Update comment

--- torchaudio/extension/extension.py
+++ torchaudio/extension/extension.py
@@ -6,7 +6,13 @@ from torchaudio._internal import module_utils as _mod_utils
 
 def _init_extension():
     if _mod_utils.is_module_available('torchaudio._torchaudio'):
-        import torchaudio._torchaudio  # noqa
+        # Note this import has two purposes
+        # 1. to extract the path of the extension module so that
+        #    we can initialize the script module with the path.
+        # 2. so that torchaudio._torchaudio is accessible in other modules.
+        #    Look at sox_io_backend which uses `torchaudio._torchaudio.XXX`,
+        #    assuming that the module `_torchaudio` is accessible.
+        import torchaudio._torchaudio
         _init_script_module(torchaudio._torchaudio.__file__)
     else:
         warnings.warn('torchaudio C++ extension is not available.')
