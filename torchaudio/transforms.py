ab65f136f32c5124808f5eec5a7198d157990818
Arijit Roy <roy.arijit2001@gmail.com>
2021-08-11 18:17:29 +0530

    added example for Fade transform

--- torchaudio/transforms.py
+++ torchaudio/transforms.py
@@ -811,6 +811,10 @@ class Fade(torch.nn.Module):
         fade_out_len (int, optional): Length of fade-out (time frames). (Default: ``0``)
         fade_shape (str, optional): Shape of fade. Must be one of: "quarter_sine",
             "half_sine", "linear", "logarithmic", "exponential". (Default: ``"linear"``)
+    Example
+        >>> waveform, sample_rate = torchaudio.load('test.wav', normalize=True)
+        >>> transform = transforms.Fade(fade_in_len=1, fade_out_len=2, fade_shape='linear')
+        >>> faded_waveform = transform(waveform)
     """
 
     def __init__(self,
