b64061a073b4d8e1c476e7cbfcc42420da0ddbff
Yao-Yuan Yang <yangarbiter@gmail.com>
2021-08-12 10:16:40 -0700

    Fix tacotron2 pretrained model

--- torchaudio/prototype/tacotron2.py
+++ torchaudio/prototype/tacotron2.py
@@ -1218,7 +1218,7 @@ def tacotron2(checkpoint_name: str) -> Tacotron2:
 
     url, configs = _MODEL_CONFIG_AND_URLS[checkpoint_name]
     model = Tacotron2(**configs)
-    state_dict = load_state_dict_from_url(url, progress=False)
+    state_dict = load_state_dict_from_url(url, progress=False, map_location=torch.device('cpu'))
     state_dict = state_dict['state_dict']
     new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
     model.load_state_dict(new_state_dict)
