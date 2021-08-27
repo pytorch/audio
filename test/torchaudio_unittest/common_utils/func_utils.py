b91354691656b769e58945abe05db89781b0b0fc
moto <855818+mthrok@users.noreply.github.com>
2021-08-27 16:35:26 +0000

    clean up

--- test/torchaudio_unittest/common_utils/func_utils.py
+++ test/torchaudio_unittest/common_utils/func_utils.py
@@ -4,7 +4,7 @@ import torch
 
 def torch_script(obj):
     """TorchScript the given function or Module"""
-    buffer_ = io.BytesIO()
-    torch.jit.save(torch.jit.script(obj), buffer_)
-    buffer_.seek(0)
-    return torch.jit.load(buffer_)
+    buffer = io.BytesIO()
+    torch.jit.save(torch.jit.script(obj), buffer)
+    buffer.seek(0)
+    return torch.jit.load(buffer)
