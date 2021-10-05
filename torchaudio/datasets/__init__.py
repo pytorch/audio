fc4f481bda4053dbf35ef2d23e663fc28a04c885
moto <855818+mthrok@users.noreply.github.com>
2021-10-05 16:45:38 -0400

    [BC-Breaking] Remove deprecated VCTK (#1825)

--- torchaudio/datasets/__init__.py
+++ torchaudio/datasets/__init__.py
@@ -2,7 +2,7 @@ from .commonvoice import COMMONVOICE
 from .librispeech import LIBRISPEECH
 from .speechcommands import SPEECHCOMMANDS
 from .utils import bg_iterator, diskcache_iterator
-from .vctk import VCTK, VCTK_092
+from .vctk import VCTK_092
 from .gtzan import GTZAN
 from .yesno import YESNO
 from .ljspeech import LJSPEECH
@@ -16,7 +16,6 @@ __all__ = [
     "COMMONVOICE",
     "LIBRISPEECH",
     "SPEECHCOMMANDS",
-    "VCTK",
     "VCTK_092",
     "YESNO",
     "LJSPEECH",
