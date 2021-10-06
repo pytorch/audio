fe7da6672088c39e4358a1667064912ace63b42b
moto <855818+mthrok@users.noreply.github.com>
2021-10-06 17:05:46 -0400

    Make the core wav2vec2 factory function public

--- torchaudio/models/wav2vec2/utils/import_fairseq.py
+++ torchaudio/models/wav2vec2/utils/import_fairseq.py
@@ -6,7 +6,7 @@ import re
 
 from torch.nn import Module
 
-from ..model import Wav2Vec2Model, _get_model
+from ..model import Wav2Vec2Model, wav2vec2_model
 
 
 def _parse_config(w2v_model):
@@ -190,27 +190,27 @@ def import_fairseq_model(original: Module) -> Wav2Vec2Model:
 
 def _import_wav2vec2_finetuning(original: Module) -> Wav2Vec2Model:
     config = _parse_config(original.w2v_model)
-    model = _get_model(**config, aux_num_out=original.proj.out_features)
+    model = wav2vec2_model(**config, aux_num_out=original.proj.out_features)
     model.load_state_dict(_convert_state_dict(original.state_dict()))
     return model
 
 
 def _import_wav2vec2_pretraining(original: Module) -> Wav2Vec2Model:
     config = _parse_config(original)
-    model = _get_model(**config, aux_num_out=None)
+    model = wav2vec2_model(**config, aux_num_out=None)
     model.load_state_dict(_convert_state_dict(original.state_dict()), strict=False)
     return model
 
 
 def _import_hubert_finetuning(original: Module) -> Wav2Vec2Model:
     config = _parse_config(original.w2v_model)
-    model = _get_model(**config, aux_num_out=original.proj.out_features)
+    model = wav2vec2_model(**config, aux_num_out=original.proj.out_features)
     model.load_state_dict(_convert_state_dict(original.state_dict()), strict=False)
     return model
 
 
 def _import_hubert_pretraining(original: Module) -> Wav2Vec2Model:
     config = _parse_config(original)
-    model = _get_model(**config, aux_num_out=None)
+    model = wav2vec2_model(**config, aux_num_out=None)
     model.load_state_dict(_convert_state_dict(original.state_dict()), strict=False)
     return model
