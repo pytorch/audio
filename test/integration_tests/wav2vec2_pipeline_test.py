3a59931599a87055ce937f9d347df1e4a1fac447
moto <855818+mthrok@users.noreply.github.com>
2021-10-26 22:05:59 -0400

    Add wav2vec2 ASR Spanish pretrained model from voxpopuli (#1924)

--- test/integration_tests/wav2vec2_pipeline_test.py
+++ test/integration_tests/wav2vec2_pipeline_test.py
@@ -18,6 +18,7 @@ from torchaudio.pipelines import (
     HUBERT_XLARGE,
     HUBERT_ASR_LARGE,
     HUBERT_ASR_XLARGE,
+    VOXPOPULI_ASR_BASE_10K_ES,
     VOXPOPULI_ASR_BASE_10K_FR,
 )
 import pytest
@@ -54,6 +55,7 @@ def test_pretraining_models(bundle):
         (WAV2VEC2_ASR_LARGE_LV60K_960H, 'en', 'I|HAVE|THAT|CURIOSITY|BESIDE|ME|AT|THIS|MOMENT|'),
         (HUBERT_ASR_LARGE, 'en', 'I|HAVE|THAT|CURIOSITY|BESIDE|ME|AT|THIS|MOMENT|'),
         (HUBERT_ASR_XLARGE, 'en', 'I|HAVE|THAT|CURIOSITY|BESIDE|ME|AT|THIS|MOMENT|'),
+        (VOXPOPULI_ASR_BASE_10K_ES, 'es', "la|primera|que|es|imprescindible|pensar|a|pequeña|a|escala|para|implicar|y|complementar|así|la|actuación|global"),  # noqa: E501
         (VOXPOPULI_ASR_BASE_10K_FR, 'fr', 'la|commission|va|faire|des|propositions|sur|ce|sujet|comment|mettre|en|place|cette|capacité|fiscale|et|le|conseil|européen|y|reviendra|sour|les|sujets|au|moins|de|mars'),  # noqa: E501
     ]
 )
