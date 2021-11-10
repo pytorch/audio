2d0ccf7ad252389dafe0f44f0879fbf2ff9299f5
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared8292.10.ftw2.facebook.com>
2021-11-10 04:31:14 -0800

    2021-11-10 nightly release (8202c7983a90fa9cc58f0e3ba49fa6148ce18be8)

--- examples/libtorchaudio/augmentation/main.cpp
+++ examples/libtorchaudio/augmentation/main.cpp
@@ -1,8 +1,9 @@
 #include <torch/script.h>
 
 int main(int argc, char* argv[]) {
-  if (argc !=4) {
-    std::cerr << "Usage: " << argv[0] << " <JIT_OBJECT> <INPUT_FILE> <OUTPUT_FILE>" << std::endl;
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0]
+              << " <JIT_OBJECT> <INPUT_FILE> <OUTPUT_FILE>" << std::endl;
     return -1;
   }
 
@@ -10,12 +11,12 @@ int main(int argc, char* argv[]) {
   std::cout << "Loading module from: " << argv[1] << std::endl;
   try {
     module = torch::jit::load(argv[1]);
-  } catch (const c10::Error &error) {
+  } catch (const c10::Error& error) {
     std::cerr << "Failed to load the module:" << error.what() << std::endl;
     return -1;
   }
 
   std::cout << "Performing the process ..." << std::endl;
   module.forward({c10::IValue(argv[2]), c10::IValue(argv[3])});
-  std::cout << "Done." << std::endl; 
+  std::cout << "Done." << std::endl;
 }
