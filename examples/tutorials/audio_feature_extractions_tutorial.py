4550c9bc881c72f41cc2be68d8c5558004599532
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared8967.11.atn3.facebook.com>
2021-11-12 04:32:37 -0800

    2021-11-12 nightly release (0ac196c62fbe6851aa3a0827f4bb58ecacbbe3df)

--- examples/tutorials/audio_feature_extractions_tutorial.py
+++ examples/tutorials/audio_feature_extractions_tutorial.py
@@ -38,16 +38,17 @@ print(torchaudio.__version__)
 # --------------------------------------------------------
 #
 
-#@title Prepare data and utility functions. {display-mode: "form"}
-#@markdown
-#@markdown You do not need to look into this cell.
-#@markdown Just execute once and you are good to go.
-#@markdown
-#@markdown In this tutorial, we will use a speech data from [VOiCES dataset](https://iqtlabs.github.io/voices/), which is licensed under Creative Commos BY 4.0.
+# @title Prepare data and utility functions. {display-mode: "form"}
+# @markdown
+# @markdown You do not need to look into this cell.
+# @markdown Just execute once and you are good to go.
+# @markdown
+# @markdown In this tutorial, we will use a speech data from [VOiCES dataset](https://iqtlabs.github.io/voices/),
+# @markdown which is licensed under Creative Commos BY 4.0.
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Preparation of data and helper functions.
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 import os
 import requests
@@ -59,143 +60,154 @@ from IPython.display import Audio, display
 
 _SAMPLE_DIR = "_assets"
 
-SAMPLE_WAV_SPEECH_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
+SAMPLE_WAV_SPEECH_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"  # noqa: E501
 SAMPLE_WAV_SPEECH_PATH = os.path.join(_SAMPLE_DIR, "speech.wav")
 
 os.makedirs(_SAMPLE_DIR, exist_ok=True)
 
 
 def _fetch_data():
-  uri = [
-    (SAMPLE_WAV_SPEECH_URL, SAMPLE_WAV_SPEECH_PATH),
-  ]
-  for url, path in uri:
-    with open(path, 'wb') as file_:
-      file_.write(requests.get(url).content)
+    uri = [
+        (SAMPLE_WAV_SPEECH_URL, SAMPLE_WAV_SPEECH_PATH),
+    ]
+    for url, path in uri:
+        with open(path, "wb") as file_:
+            file_.write(requests.get(url).content)
+
 
 _fetch_data()
 
+
 def _get_sample(path, resample=None):
-  effects = [
-    ["remix", "1"]
-  ]
-  if resample:
-    effects.extend([
-      ["lowpass", f"{resample // 2}"],
-      ["rate", f'{resample}'],
-    ])
-  return torchaudio.sox_effects.apply_effects_file(path, effects=effects)
+    effects = [["remix", "1"]]
+    if resample:
+        effects.extend(
+            [
+                ["lowpass", f"{resample // 2}"],
+                ["rate", f"{resample}"],
+            ]
+        )
+    return torchaudio.sox_effects.apply_effects_file(path, effects=effects)
+
 
 def get_speech_sample(*, resample=None):
-  return _get_sample(SAMPLE_WAV_SPEECH_PATH, resample=resample)
+    return _get_sample(SAMPLE_WAV_SPEECH_PATH, resample=resample)
+
 
 def print_stats(waveform, sample_rate=None, src=None):
-  if src:
-    print("-" * 10)
-    print("Source:", src)
-    print("-" * 10)
-  if sample_rate:
-    print("Sample Rate:", sample_rate)
-  print("Shape:", tuple(waveform.shape))
-  print("Dtype:", waveform.dtype)
-  print(f" - Max:     {waveform.max().item():6.3f}")
-  print(f" - Min:     {waveform.min().item():6.3f}")
-  print(f" - Mean:    {waveform.mean().item():6.3f}")
-  print(f" - Std Dev: {waveform.std().item():6.3f}")
-  print()
-  print(waveform)
-  print()
+    if src:
+        print("-" * 10)
+        print("Source:", src)
+        print("-" * 10)
+    if sample_rate:
+        print("Sample Rate:", sample_rate)
+    print("Shape:", tuple(waveform.shape))
+    print("Dtype:", waveform.dtype)
+    print(f" - Max:     {waveform.max().item():6.3f}")
+    print(f" - Min:     {waveform.min().item():6.3f}")
+    print(f" - Mean:    {waveform.mean().item():6.3f}")
+    print(f" - Std Dev: {waveform.std().item():6.3f}")
+    print()
+    print(waveform)
+    print()
 
-def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Spectrogram (db)')
-  axs.set_ylabel(ylabel)
-  axs.set_xlabel('frame')
-  im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
-  if xmax:
-    axs.set_xlim((0, xmax))
-  fig.colorbar(im, ax=axs)
-  plt.show(block=False)
+
+def plot_spectrogram(spec, title=None, ylabel="freq_bin", aspect="auto", xmax=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Spectrogram (db)")
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel("frame")
+    im = axs.imshow(librosa.power_to_db(spec), origin="lower", aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(im, ax=axs)
+    plt.show(block=False)
+
 
 def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-  waveform = waveform.numpy()
+    waveform = waveform.numpy()
 
-  num_channels, num_frames = waveform.shape
-  time_axis = torch.arange(0, num_frames) / sample_rate
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
 
-  figure, axes = plt.subplots(num_channels, 1)
-  if num_channels == 1:
-    axes = [axes]
-  for c in range(num_channels):
-    axes[c].plot(time_axis, waveform[c], linewidth=1)
-    axes[c].grid(True)
-    if num_channels > 1:
-      axes[c].set_ylabel(f'Channel {c+1}')
-    if xlim:
-      axes[c].set_xlim(xlim)
-    if ylim:
-      axes[c].set_ylim(ylim)
-  figure.suptitle(title)
-  plt.show(block=False)
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+        if xlim:
+            axes[c].set_xlim(xlim)
+        if ylim:
+            axes[c].set_ylim(ylim)
+    figure.suptitle(title)
+    plt.show(block=False)
+
 
 def play_audio(waveform, sample_rate):
-  waveform = waveform.numpy()
+    waveform = waveform.numpy()
 
-  num_channels, num_frames = waveform.shape
-  if num_channels == 1:
-    display(Audio(waveform[0], rate=sample_rate))
-  elif num_channels == 2:
-    display(Audio((waveform[0], waveform[1]), rate=sample_rate))
-  else:
-    raise ValueError("Waveform with more than 2 channels are not supported.")
+    num_channels, num_frames = waveform.shape
+    if num_channels == 1:
+        display(Audio(waveform[0], rate=sample_rate))
+    elif num_channels == 2:
+        display(Audio((waveform[0], waveform[1]), rate=sample_rate))
+    else:
+        raise ValueError("Waveform with more than 2 channels are not supported.")
+
 
 def plot_mel_fbank(fbank, title=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Filter bank')
-  axs.imshow(fbank, aspect='auto')
-  axs.set_ylabel('frequency bin')
-  axs.set_xlabel('mel bin')
-  plt.show(block=False)
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Filter bank")
+    axs.imshow(fbank, aspect="auto")
+    axs.set_ylabel("frequency bin")
+    axs.set_xlabel("mel bin")
+    plt.show(block=False)
+
 
 def plot_pitch(waveform, sample_rate, pitch):
-  figure, axis = plt.subplots(1, 1)
-  axis.set_title("Pitch Feature")
-  axis.grid(True)
+    figure, axis = plt.subplots(1, 1)
+    axis.set_title("Pitch Feature")
+    axis.grid(True)
 
-  end_time = waveform.shape[1] / sample_rate
-  time_axis = torch.linspace(0, end_time,  waveform.shape[1])
-  axis.plot(time_axis, waveform[0], linewidth=1, color='gray', alpha=0.3)
+    end_time = waveform.shape[1] / sample_rate
+    time_axis = torch.linspace(0, end_time, waveform.shape[1])
+    axis.plot(time_axis, waveform[0], linewidth=1, color="gray", alpha=0.3)
 
-  axis2 = axis.twinx()
-  time_axis = torch.linspace(0, end_time, pitch.shape[1])
-  ln2 = axis2.plot(
-      time_axis, pitch[0], linewidth=2, label='Pitch', color='green')
+    axis2 = axis.twinx()
+    time_axis = torch.linspace(0, end_time, pitch.shape[1])
+    axis2.plot(time_axis, pitch[0], linewidth=2, label="Pitch", color="green")
 
-  axis2.legend(loc=0)
-  plt.show(block=False)
+    axis2.legend(loc=0)
+    plt.show(block=False)
+
 
 def plot_kaldi_pitch(waveform, sample_rate, pitch, nfcc):
-  figure, axis = plt.subplots(1, 1)
-  axis.set_title("Kaldi Pitch Feature")
-  axis.grid(True)
+    figure, axis = plt.subplots(1, 1)
+    axis.set_title("Kaldi Pitch Feature")
+    axis.grid(True)
 
-  end_time = waveform.shape[1] / sample_rate
-  time_axis = torch.linspace(0, end_time,  waveform.shape[1])
-  axis.plot(time_axis, waveform[0], linewidth=1, color='gray', alpha=0.3)
+    end_time = waveform.shape[1] / sample_rate
+    time_axis = torch.linspace(0, end_time, waveform.shape[1])
+    axis.plot(time_axis, waveform[0], linewidth=1, color="gray", alpha=0.3)
 
-  time_axis = torch.linspace(0, end_time, pitch.shape[1])
-  ln1 = axis.plot(time_axis, pitch[0], linewidth=2, label='Pitch', color='green')
-  axis.set_ylim((-1.3, 1.3))
+    time_axis = torch.linspace(0, end_time, pitch.shape[1])
+    ln1 = axis.plot(time_axis, pitch[0], linewidth=2, label="Pitch", color="green")
+    axis.set_ylim((-1.3, 1.3))
 
-  axis2 = axis.twinx()
-  time_axis = torch.linspace(0, end_time, nfcc.shape[1])
-  ln2 = axis2.plot(
-      time_axis, nfcc[0], linewidth=2, label='NFCC', color='blue', linestyle='--')
+    axis2 = axis.twinx()
+    time_axis = torch.linspace(0, end_time, nfcc.shape[1])
+    ln2 = axis2.plot(
+        time_axis, nfcc[0], linewidth=2, label="NFCC", color="blue", linestyle="--"
+    )
 
-  lns = ln1 + ln2
-  labels = [l.get_label() for l in lns]
-  axis.legend(lns, labels, loc=0)
-  plt.show(block=False)
+    lns = ln1 + ln2
+    labels = [l.get_label() for l in lns]
+    axis.legend(lns, labels, loc=0)
+    plt.show(block=False)
+
 
 ######################################################################
 # Spectrogram
@@ -204,7 +216,6 @@ def plot_kaldi_pitch(waveform, sample_rate, pitch, nfcc):
 # To get the frequency make-up of an audio signal as it varies with time,
 # you can use ``Spectrogram``.
 #
-
 
 
 waveform, sample_rate = get_speech_sample()
@@ -226,7 +237,7 @@ spectrogram = T.Spectrogram(
 spec = spectrogram(waveform)
 
 print_stats(spec)
-plot_spectrogram(spec[0], title='torchaudio')
+plot_spectrogram(spec[0], title="torchaudio")
 
 ######################################################################
 # GriffinLim
@@ -280,10 +291,10 @@ sample_rate = 6000
 mel_filters = F.melscale_fbanks(
     int(n_fft // 2 + 1),
     n_mels=n_mels,
-    f_min=0.,
-    f_max=sample_rate/2.,
+    f_min=0.0,
+    f_max=sample_rate / 2.0,
     sample_rate=sample_rate,
-    norm='slaney'
+    norm="slaney",
 )
 plot_mel_fbank(mel_filters, "Mel Filter Bank - torchaudio")
 
@@ -300,16 +311,16 @@ mel_filters_librosa = librosa.filters.mel(
     sample_rate,
     n_fft,
     n_mels=n_mels,
-    fmin=0.,
-    fmax=sample_rate/2.,
-    norm='slaney',
+    fmin=0.0,
+    fmax=sample_rate / 2.0,
+    norm="slaney",
     htk=True,
 ).T
 
 plot_mel_fbank(mel_filters_librosa, "Mel Filter Bank - librosa")
 
 mse = torch.square(mel_filters - mel_filters_librosa).mean().item()
-print('Mean Square Difference: ', mse)
+print("Mean Square Difference: ", mse)
 
 ######################################################################
 # MelSpectrogram
@@ -336,15 +347,14 @@ mel_spectrogram = T.MelSpectrogram(
     center=True,
     pad_mode="reflect",
     power=2.0,
-    norm='slaney',
+    norm="slaney",
     onesided=True,
     n_mels=n_mels,
     mel_scale="htk",
 )
 
 melspec = mel_spectrogram(waveform)
-plot_spectrogram(
-    melspec[0], title="MelSpectrogram - torchaudio", ylabel='mel freq')
+plot_spectrogram(melspec[0], title="MelSpectrogram - torchaudio", ylabel="mel freq")
 
 ######################################################################
 # Comparison against librosa
@@ -365,14 +375,13 @@ melspec_librosa = librosa.feature.melspectrogram(
     pad_mode="reflect",
     power=2.0,
     n_mels=n_mels,
-    norm='slaney',
+    norm="slaney",
     htk=True,
 )
-plot_spectrogram(
-    melspec_librosa, title="MelSpectrogram - librosa", ylabel='mel freq')
+plot_spectrogram(melspec_librosa, title="MelSpectrogram - librosa", ylabel="mel freq")
 
 mse = torch.square(melspec - melspec_librosa).mean().item()
-print('Mean Square Difference: ', mse)
+print("Mean Square Difference: ", mse)
 
 ######################################################################
 # MFCC
@@ -391,11 +400,11 @@ mfcc_transform = T.MFCC(
     sample_rate=sample_rate,
     n_mfcc=n_mfcc,
     melkwargs={
-      'n_fft': n_fft,
-      'n_mels': n_mels,
-      'hop_length': hop_length,
-      'mel_scale': 'htk',
-    }
+        "n_fft": n_fft,
+        "n_mels": n_mels,
+        "hop_length": hop_length,
+        "mel_scale": "htk",
+    },
 )
 
 mfcc = mfcc_transform(waveform)
@@ -409,18 +418,27 @@ plot_spectrogram(mfcc[0])
 
 
 melspec = librosa.feature.melspectrogram(
-  y=waveform.numpy()[0], sr=sample_rate, n_fft=n_fft,
-  win_length=win_length, hop_length=hop_length,
-  n_mels=n_mels, htk=True, norm=None)
+    y=waveform.numpy()[0],
+    sr=sample_rate,
+    n_fft=n_fft,
+    win_length=win_length,
+    hop_length=hop_length,
+    n_mels=n_mels,
+    htk=True,
+    norm=None,
+)
 
 mfcc_librosa = librosa.feature.mfcc(
-  S=librosa.core.spectrum.power_to_db(melspec),
-  n_mfcc=n_mfcc, dct_type=2, norm='ortho')
+    S=librosa.core.spectrum.power_to_db(melspec),
+    n_mfcc=n_mfcc,
+    dct_type=2,
+    norm="ortho",
+)
 
 plot_spectrogram(mfcc_librosa)
 
 mse = torch.square(mfcc - mfcc_librosa).mean().item()
-print('Mean Square Difference: ', mse)
+print("Mean Square Difference: ", mse)
 
 ######################################################################
 # Pitch
