8c68cf585c3dfd2a68f3f59444156f533f1c117b
Zhaoheng Ni <zni@fb.com>
2021-11-19 14:06:51 +0000

    disable openmp

--- tools/setup_helpers/extension.py
+++ tools/setup_helpers/extension.py
@@ -39,7 +39,7 @@ _BUILD_KALDI = False if platform.system() == 'Windows' else _get_build("BUILD_KA
 _BUILD_RNNT = _get_build("BUILD_RNNT", True)
 _USE_ROCM = _get_build("USE_ROCM", torch.cuda.is_available() and torch.version.hip is not None)
 _USE_CUDA = _get_build("USE_CUDA", torch.cuda.is_available() and torch.version.hip is None)
-_USE_OPENMP = _get_build("USE_OPENMP", True) and \
+_USE_OPENMP = _get_build("USE_OPENMP", False) and \
     'ATen parallel backend: OpenMP' in torch.__config__.parallel_info()
 _TORCH_CUDA_ARCH_LIST = os.environ.get('TORCH_CUDA_ARCH_LIST', None)
 
