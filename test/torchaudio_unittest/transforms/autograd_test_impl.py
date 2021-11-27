e1142cbdc9534dce5d18e3a9b60f0ae283d7d0a7
hwangjeff <iamjeffhwang@gmail.com>
2021-11-27 17:16:28 +0000

    Revise Griffin-Lim transform test to reduce execution time

--- test/torchaudio_unittest/transforms/autograd_test_impl.py
+++ test/torchaudio_unittest/transforms/autograd_test_impl.py
@@ -101,11 +101,12 @@ class AutogradTestMixin(TestBaseMixin):
         [False, True],
     )
     def test_griffinlim(self, momentum, rand_init):
-        n_fft = 400
+        n_fft = 80
         power = 1
-        n_iter = 3
+        n_iter = 2
+
         spec = get_spectrogram(
-            get_whitenoise(sample_rate=8000, duration=0.05, n_channels=2),
+            get_whitenoise(sample_rate=8000, duration=0.01, n_channels=2),
             n_fft=n_fft, power=power)
         transform = _DeterministicWrapper(
             T.GriffinLim(n_fft=n_fft, n_iter=n_iter, momentum=momentum, rand_init=rand_init, power=power))
