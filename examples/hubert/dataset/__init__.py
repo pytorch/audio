5000edb47dbc5a6738a5dbade0137b6e5a9aafef
nateanl <nizhaoheng@gmail.com>
2021-12-08 13:05:42 +0000

    refactor init file

---
+++ examples/hubert/dataset/__init__.py
@@ -0,0 +1,12 @@
+from .hubert_dataset import (
+    BucketizeSampler,
+    HuBERTDataSet,
+    collate_fn_hubert
+)
+
+
+__all__ = [
+    "BucketizeSampler",
+    "HuBERTDataSet",
+    "collate_fn_hubert",
+]
