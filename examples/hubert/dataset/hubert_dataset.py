b4da1cea4f813c00449326928898001e6f73a461
nateanl <nizhaoheng@gmail.com>
2021-12-08 12:56:51 +0000

    add docstring, change list to ndarray or tensor

--- examples/hubert/dataset/hubert_dataset.py
+++ examples/hubert/dataset/hubert_dataset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import (
+    Dict,
     Iterator,
     List,
     Optional,
@@ -7,13 +8,14 @@ from typing import (
     Union,
 )
 from torch import Tensor
+import numpy as np
 import random
 import torch
 import torchaudio
-from torch.utils.data import Dataset, Sampler
+from torch.utils.data import Dataset, BatchSampler
 
 
-class BucketizeSampler(Sampler):
+class BucketizeSampler(BatchSampler):
     """Buketize sampler for data with different lengths to reduce number of paddings.
 
     Args:
@@ -47,7 +49,16 @@ class BucketizeSampler(Sampler):
         self,
         data_source: Dataset,
         num_buckets: int
-    ) -> List[int]:
+    ) -> Dict[int, Tensor]:
+        """Generate buckets based on the dataset.
+        Args:
+            data_source (Dataset): The dataset object to bucketize.
+            num_buckets (int): The number of buckets.
+
+        Returns:
+            (dict[int, Tensor]): A dictionary in which the key is the bucket index, the value is
+                the Tensor of corresponding sample indices.
+        """
         buckets = {}
         len_list = data_source.len_list
         min_len, max_len = min(len_list), max(len_list)
@@ -66,6 +77,7 @@ class BucketizeSampler(Sampler):
                 buckets[bucket_id] = [i]
         for k in buckets:
             random.shuffle(buckets[k])
+            buckets[k] = torch.as_tensor(buckets[k], dtype=torch.int)
         return buckets
 
     def __iter__(self) -> Iterator[List[int]]:
@@ -75,7 +87,8 @@ class BucketizeSampler(Sampler):
         len_list = self.data_source.len_list
         if self.max_token_count:
             for k in self.buckets.keys():
-                for index in self.buckets[k]:
+                for i in range(self.buckets[k].size(0)):
+                    index = self.buckets[k][i]
                     if total_len + len_list[index] > self.max_token_count:
                         iter_list.append(batch)
                         batch = [index]
@@ -84,7 +97,8 @@ class BucketizeSampler(Sampler):
                         batch.append(index)
                         total_len += len_list[index]
         else:
-            for index in self.buckets[k]:
+            for i in range(self.buckets[k].size(0)):
+                index = self.buckets[k][i]
                 if len(batch) == self.batch_size:
                     iter_list.append(batch)
                     batch = [index]
@@ -104,8 +118,11 @@ class HuBERTDataSet(Dataset):
     """Create a Dataset for HuBERT model training and fine-tuning.
 
     Args:
-        tsv_dir (str or Path): The root directory of the ``.tsv`` file list.
-        phase_type (str): The type of the dataset. Options: [``train``, ``valid``, ``test``].
+        exp_dir (str or Path): The root directory of the ``.tsv`` file list.
+        dataset (str): The dataset for training. Options: [``librispeech``, ``librilight``].
+        phase_type (str): The type of the dataset. Options: [``train``, ``valid``].
+        min_sample (int): The minimum number of audio samples in the dataset. (Default: 32000)
+        max_sample (int): The maximum number of audio samples in the dataset. (Default: 250000)
     """
     def __init__(
         self,
@@ -126,7 +143,7 @@ class HuBERTDataSet(Dataset):
             max_sample
         )
         self.f_list, self.ind_list, self.len_list = f_list, ind_list, len_list
-        self.labels = self._load_label(label_dir, dataset, phase_type)
+        self.labels = self._load_labels(label_dir, dataset, phase_type)
 
     def __len__(self):
         return len(self.f_list)
@@ -148,9 +165,9 @@ class HuBERTDataSet(Dataset):
             max_sample (int): The maximum number of audio samples in the dataset.
 
         Returns:
-            (list) List of file paths.
-            (list) List of indices that qualify ``min_sample`` <= length <= ``max_sample``.
-            (list) List of waveform lengths.
+            (numpy.array) List of file paths.
+            (numpy.array) List of indices that qualify ``min_sample`` <= length <= ``max_sample``.
+            (numpy.array) List of waveform lengths.
         """
         f_ind_len_list = []
         with open(tsv_dir / f"{dataset}_{phase_type}.tsv") as f:
@@ -167,48 +184,134 @@ class HuBERTDataSet(Dataset):
             f_list.append(ele[0])
             ind_list.append(ele[1])
             len_list.append(ele[2])
-        return f_list, ind_list, len_list
+        return np.asarray(f_list), np.asarray(ind_list), np.asarray(len_list)
 
     def _load_audio(
         self,
         index: int
     ) -> Tensor:
+        """Load waveform given the sample index of the dataset.
+        Args:
+            index (int): The sample index.
+
+        Returns:
+            (Tensor): The corresponding waveform Tensor.
+        """
         wav_path = self.f_list[index]
         waveform, sample_rate = torchaudio.load(wav_path)
         assert waveform.shape[1] == self.len_list[index]
         return waveform
 
-    def _load_label(
+    def _load_labels(
         self,
         label_dir: Path,
         dataset: str,
         phase_type: str
-    ) -> List[List[int]]:
+    ) -> np.array:
+        """Load all labels to memory into a numpy array.
+        Args:
+            label_dir (Path): The directory that contains the label file.
+            dataset (str): The dataset for training. Options: [``librispeech``, ``librilight``].
+            phase_type (str): The type of the dataset. Options: [``train``, ``valid``].
+
+        Returns:
+            (np.array): The numpy arrary that contains the labels for each audio file.
+        """
         with open(label_dir / f"{dataset}_{phase_type}.pt") as f:
             labels = [line.rstrip() for line in f]
             labels = [[int(ele) for ele in labels[i].split()] for i in self.ind_list]
-        return labels
+        return np.asarray(labels, dtype=object)
 
     def __getitem__(self, index):
         waveform = self._load_audio(index)
         length = waveform.shape[1]
         label = torch.Tensor(self.labels[index])
-        return (waveform, length, label)
+        return (waveform, label, length)
 
 
 def collate_fn_hubert(
-    batch
+    batch,
+    feature_type: str,
+    pad: bool = False,
+    rand_crop: bool = True,
 ) -> Tuple[Tensor, Tensor, Tensor]:
-    waveforms, lengths, labels = [], [], []
+    """The collate function for HuBERT pre-training and fine-tuning.
+    Args:
+        feature_type (str): The type of features for KMeans clustering.
+            Options: [``mfcc``, ``hubert``].
+        pad (bool): If ``pad`` is True, the waveforms and labels will be padded
+            to the max length in the mini-batch. If ``pad`` is False, the waveforms
+            and labels will be cropped to the minimum length in the mini-batch.
+            (Default: False)
+        rand_crop (bool): if ``rand_crop`` is True, the starting index of the
+            waveform and label is random if the length is longer than the minimum
+            length in the mini-batch.
+
+    Returns:
+        (Tuple(Tensor, Tensor, Tensor)): Returns the Tensors for the waveforms,
+            labels, and the waveform lengths.
+    """
+    audio_sizes = [sample[0].shape[1] for sample in batch]
+    if pad:
+        audio_size = max(audio_sizes)
+    else:
+        audio_size = min(audio_sizes)
+    waveforms, labels, lengths = [], [], []
     for sample in batch:
-        waveform, length, label = sample
+        waveform, label, length = sample
+        if feature_type == "mfcc":
+            label = label[::2]
+        waveform, label, length = _collate_audio_label(waveform, label, length, audio_size, rand_crop)
         waveforms.append(waveform)
         lengths.append(length)
         labels.append(label)
 
-    data = torch.zeros(len(batch), max(lengths))
+    data = torch.zeros(len(batch), audio_size)
     for i in range(len(waveforms)):
         data[i][0:waveforms[i].shape[1]] = waveforms[i][0]
     lengths = torch.Tensor(lengths)
     labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True)
-    return data, lengths, labels
+    return data, labels, lengths
+
+
+def _collate_audio_label(
+    waveform: Tensor,
+    label: Tensor,
+    length: Tensor,
+    audio_size: int,
+    rand_crop: bool,
+) -> Tuple[Tensor, Tensor, Tensor]:
+    """Collate the audio and label at the same time.
+    Args:
+        waveform (Tensor): The waveform Tensor of dimension `[1, time]`.
+        label (Tensor): The label Tensor of dimension `[1, seq]`.
+        length (Tensor): The length Tensor of dimension `[1,]`.
+        audio_size (int): The final length of the waveform.
+        rand_crop (bool): if ``rand_crop`` is True, the starting index of the
+            waveform and label is random if the length is longer than the minimum
+            length in the mini-batch.
+
+    Returns:
+        (Tuple(Tensor, Tensor, Tensor)): Returns the Tensors for the waveform,
+            label, and the waveform length.
+    """
+    kernel_size = 25
+    stride = 20
+    sample_rate = 16  # 16 per millisecond
+    if waveform.shape[1] > audio_size:
+        diff = waveform.size(1) - audio_size
+        audio_start = torch.randint(diff, size=(1,)) if rand_crop else 0
+        label_start = torch.div(
+            audio_start - kernel_size * sample_rate,
+            stride * sample_rate,
+            rounding_mode='floor'
+        )
+        label_size = torch.div(
+            audio_size - kernel_size * sample_rate,
+            stride * sample_rate,
+            rounding_mode='floor'
+        )
+        waveform = waveform[:, audio_start:audio_start + audio_size]
+        label = label[label_start:label_start + label_size]
+        length = audio_size
+    return waveform, label, length
