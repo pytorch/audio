ddb9fb5bbf26ec8cbf50004dfc252792ab13bd54
nateanl <nizhaoheng@gmail.com>
2021-12-10 10:17:15 -0800

    Add bucketize sampler and dataset for HuBERT Base model training pipeline (#2000)
    Summary:
    The PR adds PyTorch Lightning based training script for HuBERT Base model. There are two iterations of pre-training and 1 iteration of ASR fine-tuning on LibriSpeech dataset.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2000
    
    Reviewed By: carolineechen
    
    Differential Revision: D33021467
    
    Pulled By: nateanl
    
    fbshipit-source-id: 77fe5a751943b56b63d5f1fb4e6ef35946e081db

---
+++ examples/hubert/dataset/__init__.py
@@ -0,0 +1,12 @@
+from .hubert_dataset import (
+    BucketizeSampler,
+    CollateFnHubert,
+    HuBERTDataSet,
+)
+
+
+__all__ = [
+    "BucketizeSampler",
+    "CollateFnHubert",
+    "HuBERTDataSet",
+]
