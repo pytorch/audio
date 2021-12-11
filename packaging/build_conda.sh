0a701058b432dd602bba3461866bfb3c3a352e04
Andrey Talman <atalman@fb.com>
2021-12-10 18:28:23 -0800

    Add CUDA-11.5 builds to torchaudio (#2067)
    Summary:
    cc peterjc123 maxluk nbcsm guyang3532 gunandrose4u smartcat2010 mszhanyi
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2067
    
    Reviewed By: seemethere
    
    Differential Revision: D33032607
    
    Pulled By: atalman
    
    fbshipit-source-id: a5767e9af27690d3a7ab762ddf30178b3069cd35

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -10,5 +10,11 @@ export SOURCE_ROOT_DIR="$PWD"
 setup_conda_pytorch_constraint
 setup_conda_cudatoolkit_constraint
 setup_visual_studio_constraint
-# nvidia channel included for cudatoolkit >= 11
-conda build -c defaults -c nvidia $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
+
+# nvidia channel included for cudatoolkit >= 11 however for 11.5 we use conda-forge
+export CUDATOOLKIT_CHANNEL="nvidia"
+if [[ "$CU_VERSION" == cu115 ]]; then
+    export CUDATOOLKIT_CHANNEL="conda-forge"
+fi
+
+conda build -c defaults -c $CUDATOOLKIT_CHANNEL $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
