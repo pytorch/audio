5ff191cc4e981a658a5a31ec61d12ce7588ab36f
moto <855818+mthrok@users.noreply.github.com>
2021-12-17 16:00:25 -0500

    Add FL Decoder / KenLM integration to build process

--- tools/setup_helpers/extension.py
+++ tools/setup_helpers/extension.py
@@ -37,7 +37,7 @@ def _get_build(var, default=False):
 _BUILD_SOX = False if platform.system() == 'Windows' else _get_build("BUILD_SOX", True)
 _BUILD_KALDI = False if platform.system() == 'Windows' else _get_build("BUILD_KALDI", True)
 _BUILD_RNNT = _get_build("BUILD_RNNT", True)
-_BUILD_KENLM = False if platform.system() == 'Windows' else _get_build("BUILD_KENLM", True)
+_BUILD_CTC_DECODER = False if platform.system() == 'Windows' else _get_build("BUILD_CTC_DECODER", True)
 _USE_ROCM = _get_build("USE_ROCM", torch.cuda.is_available() and torch.version.hip is not None)
 _USE_CUDA = _get_build("USE_CUDA", torch.cuda.is_available() and torch.version.hip is None)
 _USE_OPENMP = _get_build("USE_OPENMP", True) and \
@@ -46,10 +46,16 @@ _TORCH_CUDA_ARCH_LIST = os.environ.get('TORCH_CUDA_ARCH_LIST', None)
 
 
 def get_ext_modules():
-    return [
+    modules = [
         Extension(name='torchaudio.lib.libtorchaudio', sources=[]),
         Extension(name='torchaudio._torchaudio', sources=[]),
     ]
+    if _BUILD_CTC_DECODER:
+        modules.extend([
+            Extension(name='torchaudio.lib.libtorchaudio_decoder', sources=[]),
+            Extension(name='torchaudio._torchaudio_decoder', sources=[]),
+        ])
+    return modules
 
 
 # Based off of
@@ -90,7 +96,7 @@ class CMakeBuild(build_ext):
             f"-DBUILD_SOX:BOOL={'ON' if _BUILD_SOX else 'OFF'}",
             f"-DBUILD_KALDI:BOOL={'ON' if _BUILD_KALDI else 'OFF'}",
             f"-DBUILD_RNNT:BOOL={'ON' if _BUILD_RNNT else 'OFF'}",
-            f"-DBUILD_KENLM:BOOL={'ON' if _BUILD_KENLM else 'OFF'}",
+            f"-DBUILD_CTC_DECODER:BOOL={'ON' if _BUILD_CTC_DECODER else 'OFF'}",
             "-DBUILD_TORCHAUDIO_PYTHON_EXTENSION:BOOL=ON",
             f"-DUSE_ROCM:BOOL={'ON' if _USE_ROCM else 'OFF'}",
             f"-DUSE_CUDA:BOOL={'ON' if _USE_CUDA else 'OFF'}",
