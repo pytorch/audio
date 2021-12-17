adc559a817c2cfa3a8c24389b5e31d4b1d50cbc6
moto <855818+mthrok@users.noreply.github.com>
2021-12-17 10:51:44 -0800

    Add static build of KenLM (#2076)
    Summary:
    Add KenLM and its dependencies required for static build (`zlib`, `bzip2`, `lzma` and `boost-thread`).
    
    The KenLM and its dependencies are build but since no corresponding code on torchaudio side is changed, the resulting torchaudio extension module is not changed. (therefore, as long as build process passes on CI this PR should be good to go.)
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2076
    
    Reviewed By: carolineechen
    
    Differential Revision: D33189980
    
    Pulled By: mthrok
    
    fbshipit-source-id: 6096113128b939f3cf70990c99aacc4aaa954584

--- tools/setup_helpers/extension.py
+++ tools/setup_helpers/extension.py
@@ -37,6 +37,7 @@ def _get_build(var, default=False):
 _BUILD_SOX = False if platform.system() == 'Windows' else _get_build("BUILD_SOX", True)
 _BUILD_KALDI = False if platform.system() == 'Windows' else _get_build("BUILD_KALDI", True)
 _BUILD_RNNT = _get_build("BUILD_RNNT", True)
+_BUILD_KENLM = False if platform.system() == 'Windows' else _get_build("BUILD_KENLM", True)
 _USE_ROCM = _get_build("USE_ROCM", torch.cuda.is_available() and torch.version.hip is not None)
 _USE_CUDA = _get_build("USE_CUDA", torch.cuda.is_available() and torch.version.hip is None)
 _USE_OPENMP = _get_build("USE_OPENMP", True) and \
@@ -89,6 +90,7 @@ class CMakeBuild(build_ext):
             f"-DBUILD_SOX:BOOL={'ON' if _BUILD_SOX else 'OFF'}",
             f"-DBUILD_KALDI:BOOL={'ON' if _BUILD_KALDI else 'OFF'}",
             f"-DBUILD_RNNT:BOOL={'ON' if _BUILD_RNNT else 'OFF'}",
+            f"-DBUILD_KENLM:BOOL={'ON' if _BUILD_KENLM else 'OFF'}",
             "-DBUILD_TORCHAUDIO_PYTHON_EXTENSION:BOOL=ON",
             f"-DUSE_ROCM:BOOL={'ON' if _USE_ROCM else 'OFF'}",
             f"-DUSE_CUDA:BOOL={'ON' if _USE_CUDA else 'OFF'}",
