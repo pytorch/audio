3f1b1928054dee06b0dc24b18247ce978225c02c
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared17911.09.vll2.facebook.com>
2021-12-28 14:07:28 -0800

    2021-12-28 nightly release (133d00654cd52a43158e235c2003ed7f3322d70d)

--- torchaudio/models/__init__.py
+++ torchaudio/models/__init__.py
@@ -4,6 +4,7 @@ from .tacotron2 import Tacotron2
 from .wav2letter import Wav2Letter
 from .wav2vec2 import (
     Wav2Vec2Model,
+    HuBERTPretrainModel,
     wav2vec2_model,
     wav2vec2_base,
     wav2vec2_large,
@@ -11,6 +12,10 @@ from .wav2vec2 import (
     hubert_base,
     hubert_large,
     hubert_xlarge,
+    hubert_pretrain_model,
+    hubert_pretrain_base,
+    hubert_pretrain_large,
+    hubert_pretrain_xlarge,
 )
 from .wavernn import WaveRNN
 
@@ -20,6 +25,7 @@ __all__ = [
     "ConvTasNet",
     "DeepSpeech",
     "Wav2Vec2Model",
+    "HuBERTPretrainModel",
     "wav2vec2_model",
     "wav2vec2_base",
     "wav2vec2_large",
@@ -27,5 +33,9 @@ __all__ = [
     "hubert_base",
     "hubert_large",
     "hubert_xlarge",
+    "hubert_pretrain_model",
+    "hubert_pretrain_base",
+    "hubert_pretrain_large",
+    "hubert_pretrain_xlarge",
     "Tacotron2",
 ]
