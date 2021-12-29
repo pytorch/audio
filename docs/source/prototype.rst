72a98a86c6bc17573cbe61a26061689b3830e5af
hwangjeff <iamjeffhwang@gmail.com>
2021-12-28 16:18:28 -0800

    Add pretrained Emformer RNN-T streaming ASR inference pipeline (#2093)
    Summary:
    Adds pretrained Emformer RNN-T inference pipeline that's capable of performing streaming and non-streaming ASR.
    
    Includes demo script that uses pipeline to alternately perform streaming and non-streaming ASR on LibriSpeech test samples (video below).
    
    https://user-images.githubusercontent.com/8345689/147590753-d5126557-d575-4551-8dfe-5977276cb4ad.mov
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2093
    
    Reviewed By: mthrok
    
    Differential Revision: D33340776
    
    Pulled By: hwangjeff
    
    fbshipit-source-id: fbb3b1d471b4e9f1b93fa9dea9c464154537a8ac

--- docs/source/prototype.rst
+++ docs/source/prototype.rst
@@ -73,6 +73,34 @@ Hypothesis
 .. autoclass:: Hypothesis
 
 
+RNNTBundle
+~~~~~~~~~~
+
+.. autoclass:: RNNTBundle
+  :members: sample_rate, n_fft, n_mels, hop_length, segment_length, right_context_length
+
+  .. automethod:: get_decoder
+
+  .. automethod:: get_feature_extractor
+
+  .. automethod:: get_streaming_feature_extractor
+
+  .. automethod:: get_token_processor
+
+  .. autoclass:: torchaudio.prototype::RNNTBundle.FeatureExtractor
+    :special-members: __call__
+
+  .. autoclass:: torchaudio.prototype::RNNTBundle.TokenProcessor
+    :special-members: __call__
+
+
+EMFORMER_RNNT_BASE_LIBRISPEECH
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autodata:: EMFORMER_RNNT_BASE_LIBRISPEECH
+  :no-value:
+
+
 KenLMLexiconDecoder
 ~~~~~~~~~~~~~~~~~~~
 
---
+++ examples/asr/librispeech_emformer_rnnt/pipeline_demo.py
@@ -0,0 +1,54 @@
+from argparse import ArgumentParser
+import pathlib
+import torch
+import torchaudio
+from torchaudio.prototype.rnnt_pipeline import EMFORMER_RNNT_BASE_LIBRISPEECH
+
+
+def cli_main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--librispeech_path", type=pathlib.Path, required=True, help="Path to LibriSpeech datasets.",
+    )
+    args = parser.parse_args()
+
+    dataset = torchaudio.datasets.LIBRISPEECH(args.librispeech_path, url="test-clean")
+    decoder = EMFORMER_RNNT_BASE_LIBRISPEECH.get_decoder()
+    token_processor = EMFORMER_RNNT_BASE_LIBRISPEECH.get_token_processor()
+    feature_extractor = EMFORMER_RNNT_BASE_LIBRISPEECH.get_feature_extractor()
+    streaming_feature_extractor = EMFORMER_RNNT_BASE_LIBRISPEECH.get_streaming_feature_extractor()
+
+    hop_length = EMFORMER_RNNT_BASE_LIBRISPEECH.hop_length
+    num_samples_segment = EMFORMER_RNNT_BASE_LIBRISPEECH.segment_length * hop_length
+    num_samples_segment_right_context = (
+        num_samples_segment + EMFORMER_RNNT_BASE_LIBRISPEECH.right_context_length * hop_length
+    )
+
+    for idx in range(10):
+        sample = dataset[idx]
+        waveform = sample[0].squeeze()
+
+        # Streaming decode.
+        state, hypothesis = None, None
+        for idx in range(0, len(waveform), num_samples_segment):
+            segment = waveform[idx: idx + num_samples_segment_right_context]
+            segment = torch.nn.functional.pad(segment, (0, num_samples_segment_right_context - len(segment)))
+            with torch.no_grad():
+                features, length = streaming_feature_extractor(segment)
+                hypos, state = decoder.infer(features, length, 10, state=state, hypothesis=hypothesis)
+            hypothesis = hypos[0]
+            transcript = token_processor(hypothesis.tokens)
+            if transcript:
+                print(transcript, end=" ", flush=True)
+        print()
+
+        # Non-streaming decode.
+        with torch.no_grad():
+            features, length = feature_extractor(waveform)
+            hypos = decoder(features, length, 10)
+        print(token_processor(hypos[0].tokens))
+        print()
+
+
+if __name__ == "__main__":
+    cli_main()
