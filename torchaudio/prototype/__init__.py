c51dc5e07f95589d686b948877a0dc936a9915b2
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared39736.26.prn2.facebook.com>
2021-12-28 16:23:02 -0800

    2021-12-28 nightly release (72a98a86c6bc17573cbe61a26061689b3830e5af)

--- torchaudio/prototype/__init__.py
+++ torchaudio/prototype/__init__.py
@@ -2,6 +2,7 @@ from .conformer import Conformer
 from .emformer import Emformer
 from .rnnt import RNNT, emformer_rnnt_base, emformer_rnnt_model
 from .rnnt_decoder import Hypothesis, RNNTBeamSearch
+from .rnnt_pipeline import EMFORMER_RNNT_BASE_LIBRISPEECH, RNNTBundle
 
 
 __all__ = [
@@ -12,4 +13,6 @@ __all__ = [
     "RNNTBeamSearch",
     "emformer_rnnt_base",
     "emformer_rnnt_model",
+    "EMFORMER_RNNT_BASE_LIBRISPEECH",
+    "RNNTBundle",
 ]
