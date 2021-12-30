ece03edc3fc28a1ce2c28ef438d2898ed0a78d3f
moto <855818+mthrok@users.noreply.github.com>
2021-12-30 08:31:19 -0800

    Add a switch to build ffmpeg binding (#2048)
    Summary:
    This PR adds `BUILD_FFMPEG` switch to torchaudio build process so that features related to ffmpeg are built.
    The flag is false by default, so no CI jobs or development flow are affected.
    
    This is because handling the dependencies around ffmpeg is a bit tricky.
    Currently, the CMake file uses `pkg-config` to find an ffmpeg installation in the system.
    This works fine for both conda-based installation and system-managed installation (like `apt`).
    
    In subsequent PRs, I will find a solution that works for local development and binary distributions.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2048
    
    Reviewed By: hwangjeff, nateanl
    
    Differential Revision: D33367260
    
    Pulled By: mthrok
    
    fbshipit-source-id: 94517acecb62bd6d4e96d4b7cbc3ab3c2a25706c

--- tools/setup_helpers/extension.py
+++ tools/setup_helpers/extension.py
@@ -36,6 +36,7 @@ _BUILD_SOX = False if platform.system() == "Windows" else _get_build("BUILD_SOX"
 _BUILD_KALDI = False if platform.system() == "Windows" else _get_build("BUILD_KALDI", True)
 _BUILD_RNNT = _get_build("BUILD_RNNT", True)
 _BUILD_CTC_DECODER = False if platform.system() == "Windows" else _get_build("BUILD_CTC_DECODER", True)
+_BUILD_FFMPEG = _get_build("BUILD_FFMPEG", False)
 _USE_ROCM = _get_build("USE_ROCM", torch.cuda.is_available() and torch.version.hip is not None)
 _USE_CUDA = _get_build("USE_CUDA", torch.cuda.is_available() and torch.version.hip is None)
 _USE_OPENMP = _get_build("USE_OPENMP", True) and "ATen parallel backend: OpenMP" in torch.__config__.parallel_info()
@@ -54,6 +55,8 @@ def get_ext_modules():
                 Extension(name="torchaudio._torchaudio_decoder", sources=[]),
             ]
         )
+    if _BUILD_FFMPEG:
+        modules.append(Extension(name="torchaudio.lib.libtorchaudio_ffmpeg", sources=[]))
     return modules
 
 
@@ -92,6 +95,7 @@ class CMakeBuild(build_ext):
             "-DCMAKE_VERBOSE_MAKEFILE=ON",
             f"-DPython_INCLUDE_DIR={distutils.sysconfig.get_python_inc()}",
             f"-DBUILD_SOX:BOOL={'ON' if _BUILD_SOX else 'OFF'}",
+            f"-DBUILD_FFMPEG:BOOL={'ON' if _BUILD_FFMPEG else 'OFF'}",
             f"-DBUILD_KALDI:BOOL={'ON' if _BUILD_KALDI else 'OFF'}",
             f"-DBUILD_RNNT:BOOL={'ON' if _BUILD_RNNT else 'OFF'}",
             f"-DBUILD_CTC_DECODER:BOOL={'ON' if _BUILD_CTC_DECODER else 'OFF'}",
