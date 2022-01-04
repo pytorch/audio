832f055a9b44c31c70aed39f72abf5e3eb6d7584
moto <855818+mthrok@users.noreply.github.com>
2022-01-04 12:36:35 -0800

    Add custom CSS to make signatures appear in multi-line (#2123)
    Summary:
    * Before
    
    https://pytorch.org/audio/main/models.html
    
    <img width="852" alt="Screen Shot 2022-01-04 at 11 00 12 AM" src="https://user-images.githubusercontent.com/855818/148087255-3b94e63b-9870-4c7e-95c6-17acc1e65fef.png">
    
    *After
    
    https://503135-90321822-gh.circle-artifacts.com/0/docs/models.html
    
    <img width="842" alt="Screen Shot 2022-01-04 at 10 59 40 AM" src="https://user-images.githubusercontent.com/855818/148087148-b951c7b0-d9cf-4014-8a50-b88c749f12ba.png">
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2123
    
    Reviewed By: carolineechen
    
    Differential Revision: D33409661
    
    Pulled By: mthrok
    
    fbshipit-source-id: bb2dffea25ccc4356d257b2ab4a6e88f7f4e2bb3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -199,7 +199,7 @@ html_logo = "_static/img/pytorch-logo-dark.svg"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css"]
+html_css_files = ["https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css", "css/custom.css"]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
