13132344cfa1a485dfb9590e930f11624e4630dd
Werner Chao <wernerc@fb.com>
2022-01-05 10:51:17 -0500

    Drop support for python 3.6 and update documentation, issue 2051.

--- setup.py
+++ setup.py
@@ -159,7 +159,6 @@ def _main():
             "Operating System :: Microsoft :: Windows",
             "Operating System :: POSIX",
             "Programming Language :: C++",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
