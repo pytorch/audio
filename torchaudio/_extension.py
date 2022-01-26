39fe9df6ab19f27b4b86fc226a3adfc4eb3f5f0a
moto <855818+mthrok@users.noreply.github.com>
2022-01-26 15:57:52 -0800

    Add `is_ffmpeg_available` in test (#2170)
    Summary:
    Part of https://github.com/pytorch/audio/issues/2164.
    To make the tests introduced in https://github.com/pytorch/audio/issues/2164 skippable if ffmpeg features are not available,
    this commit adds `is_ffmpeg_available`.
    
    The availability of the features depend on two factors;
    1. If it was enabled at build.
    2. If the ffmpeg libraries are found at runtime.
    
    A simple way (for OSS workflow) to detect these is simply checking if
    `libtorchaudio_ffmpeg` presents and can be loaded without a failure.
    
    To facilitate this, this commit changes the
    `torchaudio._extension._load_lib` to return boolean result.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2170
    
    Reviewed By: carolineechen
    
    Differential Revision: D33797695
    
    Pulled By: mthrok
    
    fbshipit-source-id: 85e767fc06350b8f99de255bc965b8c92b8cfe97

--- torchaudio/_extension.py
+++ torchaudio/_extension.py
@@ -14,15 +14,43 @@ def _get_lib_path(lib: str):
     return path
 
 
-def _load_lib(lib: str):
+def _load_lib(lib: str) -> bool:
+    """Load extension module
+
+    Note:
+        In case `torchaudio` is deployed with `pex` format, the library file
+        is not in a standard location.
+        In this case, we expect that `libtorchaudio` is available somewhere
+        in the search path of dynamic loading mechanism, so that importing
+        `_torchaudio` will have library loader find and load `libtorchaudio`.
+        This is the reason why the function should not raising an error when the library
+        file is not found.
+
+    Returns:
+        bool:
+            True if the library file is found AND the library loaded without failure.
+            False if the library file is not found (like in the case where torchaudio
+            is deployed with pex format, thus the shared library file is
+            in a non-standard location.).
+            If the library file is found but there is an issue loading the library,
+            (such as missing dependency) then this function raises the exception as-is.
+
+    Raises:
+        Exception:
+            If the library file is found, but there is an issue loading the library file,
+            (when underlying `ctype.DLL` throws an exception), this function will pass
+            the exception as-is, instead of catching it and returning bool.
+            The expected case is `OSError` thrown by `ctype.DLL` when a dynamic dependency
+            is not found.
+            This behavior was chosen because the expected failure case is not recoverable.
+            If a dependency is missing, then users have to install it.
+    """
     path = _get_lib_path(lib)
-    # In case `torchaudio` is deployed with `pex` format, this file does not exist.
-    # In this case, we expect that `libtorchaudio` is available somewhere
-    # in the search path of dynamic loading mechanism, and importing `_torchaudio`,
-    # which depends on `libtorchaudio` and dynamic loader will handle it for us.
-    if path.exists():
-        torch.ops.load_library(path)
-        torch.classes.load_library(path)
+    if not path.exists():
+        return False
+    torch.ops.load_library(path)
+    torch.classes.load_library(path)
+    return True
 
 
 def _init_extension():
