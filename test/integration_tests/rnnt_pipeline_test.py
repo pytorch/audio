11bd618279d22904247f20ddb009f4716a95a9ee
hwangjeff <iamjeffhwang@gmail.com>
2022-01-26 06:07:48 +0000

    install sentencepiece for tests

--- test/integration_tests/rnnt_pipeline_test.py
+++ test/integration_tests/rnnt_pipeline_test.py
@@ -4,20 +4,14 @@ from torchaudio.prototype.pipelines import EMFORMER_RNNT_BASE_LIBRISPEECH
 
 
 @pytest.mark.parametrize(
-    "bundle,lang,expected",
-    [
-        (
-            EMFORMER_RNNT_BASE_LIBRISPEECH,
-            "en",
-            # SentencePiece token ids for "i have that curiosity beside me at this moment"
-            [4096, 18, 135, 60, 3526, 1931, 113, 100, 138, 619],
-        )
-    ],
+    "bundle,lang,expected", [(EMFORMER_RNNT_BASE_LIBRISPEECH, "en", "i have that curiosity beside me at this moment")],
 )
 def test_rnnt(bundle, sample_speech, expected):
     feature_extractor = bundle.get_feature_extractor()
     decoder = bundle.get_decoder().eval()
+    token_processor = bundle.get_token_processor()
     waveform, _ = torchaudio.load(sample_speech)
     features, length = feature_extractor(waveform.squeeze())
     hypotheses = decoder(features, length, 10)
-    assert hypotheses[0].tokens == expected
+    text = token_processor(hypotheses[0].tokens)
+    assert text == expected
