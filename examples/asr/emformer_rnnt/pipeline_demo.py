194bce58c5673f8c839f50c18b6d36c23d4ac74e
Zhaoheng Ni <zni@fb.com>
2022-02-16 20:35:23 +0000

    add EMFORMER_RNNT_BASE_MUSTC into pipeline demo

--- examples/asr/emformer_rnnt/pipeline_demo.py
+++ examples/asr/emformer_rnnt/pipeline_demo.py
@@ -13,11 +13,11 @@ from typing import Callable
 
 import torch
 import torchaudio
-from common import MODEL_TYPE_LIBRISPEECH, MODEL_TYPE_TEDLIUM3
+from common import MODEL_TYPE_LIBRISPEECH, MODEL_TYPE_MUSTC, MODEL_TYPE_TEDLIUM3
+from mustc.dataset import MUSTC
 from torchaudio.pipelines import EMFORMER_RNNT_BASE_LIBRISPEECH
 from torchaudio.pipelines import RNNTBundle
-from torchaudio.prototype.pipelines import EMFORMER_RNNT_BASE_TEDLIUM3
-
+from torchaudio.prototype.pipelines import EMFORMER_RNNT_BASE_MUSTC, EMFORMER_RNNT_BASE_TEDLIUM3
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +32,10 @@ _CONFIGS = {
     MODEL_TYPE_LIBRISPEECH: Config(
         partial(torchaudio.datasets.LIBRISPEECH, url="test-clean"),
         EMFORMER_RNNT_BASE_LIBRISPEECH,
+    ),
+    MODEL_TYPE_MUSTC: Config(
+        partial(MUSTC, subset="tst-COMMON"),
+        EMFORMER_RNNT_BASE_MUSTC,
     ),
     MODEL_TYPE_TEDLIUM3: Config(
         partial(torchaudio.datasets.TEDLIUM, release="release3", subset="test"),
