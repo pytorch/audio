5959537ff93658a0125fdcb13bfa852ca3c2c400
Eli Uriegas <eliuriegas@fb.com>
2022-02-23 12:34:34 -0800

    add conda-forge back for just cuda 11.5
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -14,4 +14,9 @@ setup_visual_studio_constraint
 # nvidia channel included for cudatoolkit >= 11 however for 11.5 we use conda-forge
 # HACK HACK HACK: Remove PYTHON_VERSION check once https://github.com/pytorch/builder/pull/961 is merged
 export CUDATOOLKIT_CHANNEL="nvidia"
+# NOTE: This is needed because `cudatoolkit=11.5` has a dependency on conda-forge
+#       See: https://github.com/pytorch/audio/pull/2224#issuecomment-1049185550
+if [[ ${CU_VERSION} = "cu115" ]]; then
+    CONDA_CHANNEL_FLAGS="${CONDA_CHANNEL_FLAGS} -c conda-forge"
+fi
 conda build -c defaults -c $CUDATOOLKIT_CHANNEL ${CONDA_CHANNEL_FLAGS:-} --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
