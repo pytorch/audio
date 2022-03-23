750d902b2078495c758dc72e880b97d01c7ba9b5
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared33315.02.pnb3.facebook.com>
2022-03-23 04:33:44 -0700

    2022-03-23 nightly release (7444f56819f679f68eee8bf915bbb74be3da0e40)

--- torchaudio/utils/__init__.py
+++ torchaudio/utils/__init__.py
@@ -3,7 +3,13 @@ from torchaudio._internal import module_utils as _mod_utils
 from . import (
     sox_utils,
 )
-
+from .download import download_asset
 
 if _mod_utils.is_sox_available():
     sox_utils.set_verbosity(1)
+
+
+__all__ = [
+    "download_asset",
+    "sox_utils",
+]
