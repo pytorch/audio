9243fd85208cc75f0e2c76400813490c853bb2db
moto <855818+mthrok@users.noreply.github.com>
2022-03-31 23:41:54 -0700

    fixup! wip: adding new version

--- torchaudio/_kaldi/utils.py
+++ torchaudio/_kaldi/utils.py
@@ -2,17 +2,6 @@ import torch
 from torch import Tensor
 
 
-def _get_num_hops(num_frames, window_size, window_shift, snip_edges):
-    if snip_edges:
-        if num_frames < window_size:
-            return 0
-        return 1 + (num_frames - window_size) // window_shift
-
-    # The added window_shift // 2 is to so that the result of the
-    # final division become the closest integer of `num_frames / window_shift`
-    return (num_frames + (window_shift // 2)) // window_shift
-
-
 def fold(
         waveform: Tensor,
         window_size: int,
@@ -34,9 +23,17 @@ def fold(
 
     """
     assert waveform.ndim == 2
-    num_frames = waveform.shape[1]
+    batch_size, num_frames = waveform.shape
+    assert num_frames > 0
 
-    # The index of the first frame and the number of hops
+    device, dtype = waveform.device, waveform.dtype
+
+    # Figure out the index of the first frame and the number of hops
+    #
+    # This is the simplification of the following two functions.
+    #
+    # https://github.com/kaldi-asr/kaldi/blob/dd107fd594ac58af962031c1689abfdc10f84452/src/feat/feature-window.cc#L30
+    # https://github.com/kaldi-asr/kaldi/blob/dd107fd594ac58af962031c1689abfdc10f84452/src/feat/feature-window.cc#L42
     #
     # If snip_edges == True, then the fold starts at index 0 and all folds contain
     # valid frames. The remaining frames at the end are dropped.
@@ -45,28 +42,101 @@ def fold(
     # first fold is centered at offset of shift // 2. The number of hops are an
     # integer closest to num_frames // window_shift.
     #
-    # https://github.com/kaldi-asr/kaldi/blob/dd107fd594ac58af962031c1689abfdc10f84452/src/feat/feature-window.cc#L30
-    i_start = 0 if snip_edges else window_shift // 2 - window_size // 2
-    # https://github.com/kaldi-asr/kaldi/blob/dd107fd594ac58af962031c1689abfdc10f84452/src/feat/feature-window.cc#L42
-    num_hops = _get_num_hops(num_frames, window_size, window_shift, snip_edges)
+    if snip_edges:
+        i_start = 0
+        num_hops = 0 if num_frames < window_size else 1 + (num_frames - window_size) // window_shift
+    else:
+        i_start = window_shift // 2 - window_size // 2
+        num_hops = (num_frames + (window_shift // 2)) // window_shift
 
     i_end = i_start + num_hops * window_shift + window_size
+    # when snip_edges == false
+    # i_end <= num_frames + window_shift + window_size // 2 [ + 1 ]
 
-    if i_start < 0 or num_frames < i_end:
-        # extraction starts/ends at outside of existing frames.
-        left_pad = - i_start if i_start < 0 else 0
-        right_pad = i_end - num_frames if num_frames < i_end else 0
-        pad = (0, 0, left_pad, right_pad)
-        waveform = torch.nn.functional.pad(waveform, pad, mode='reflect')
+    if i_start < - (num_frames + 1) or i_end > num_frames * 2:
+        raise ValueError(
+            "The requested window region is outside of the valid range. "
+            "The input waveform is too short or the configuration is invalid. "
+            "Perhaps, set snip_edges=True, or "
+            "decrease the value of window_size and/or window_shift."
+        )
 
-        if i_start < 0:
-            i_end -= i_start
-            i_start -= i_start
+    if num_hops == 0:
+        # TODO: issue warning?
+        return torch.empty([batch_size, 0, window_size], device=device, dtype=dtype)
+
+    # if i_start < 0 or num_frames < i_end:
+    #     # extraction starts/ends at outside of existing frames.
+    #     left_pad = - i_start if i_start < 0 else 0
+    #     right_pad = i_end - num_frames if num_frames < i_end else 0
+    #     pad = (0, 0, left_pad, right_pad)
+    #     waveform = torch.nn.functional.pad(waveform, pad, mode='reflect')
+    #
+    #     if i_start < 0:
+    #         i_end -= i_start
+    #         i_start -= i_start
 
     folds = []
     waveform = waveform.unsqueeze(1)
+
+    # Note:
+    # torch.nn.functional.pad(mode="reflect") performs fixed end reflection.
+    # i.e. `1 2 3 2 1`,
+    # while Kaldi's implementation performs free end reflection.
+    # i.e. `1 2 3 3 2 1`.
+    #
+    # Kaldi's implementation allows multiple reflection if window length is
+    # much larger than number of available frames, even though this is
+    # mal-formed configuration.
+    # In this implementation, such configuration is not allowed.
     for i in range(num_hops):
         s = i_start + i * window_shift
         e = s + window_size
-        folds.append(waveform[:, :, s:e])
+
+        if s < 0:
+            if e < 0:
+                #                       s     e
+                # tensor index      -4 -3 -2 -1 0 1 2 3 4
+                #                      ^^^^^ xx
+                # corresponding      3  2  1  0 0 1 2 3 4
+                # mirrored index        ^^^^
+                #
+                # resulting slice   waveform[:, :, 1:3].flip(-1)
+                fold = waveform[:, :, -e:-s].flip(-1)
+            else:
+                #                       s           e
+                # tensor index      -4 -3 -2 -1 0 1 2 3 4
+                #                      ^^^^^^^^^^^^ x
+                # corresponding      3  2  1  0 0 1 2 3 4
+                # mirrored index       ^^^^^^^^ ^^^
+                #
+                # resulting slice   torch.cat(waveform[:, :, :3].flip(-1), waveform[:, :, :2])
+                fold = torch.cat(waveform[:, :, :-s].flip(-1), waveform[:, :, :e], dim=-1)
+        elif s <= num_frames:
+            if e <= num_frames:
+                #                     s     e
+                # tensor index      0 1 2 3 4
+                #                     ^^^^^ x
+                fold = waveform[:, :, s:e]
+            else:
+                #            num_frames == 7
+                #                     s          |       e
+                # tensor index      0 1 2 3 4 5 6 7 8 9 10
+                #                     ^^^^^^^^^^^^^^^^^  x
+                # corresponding     0 1 2 3 4 5 6 6 5 4  3
+                # mirrored index      ^^^^^^^^^^^ ^^^^^  x
+                #
+                # resulting slice   torch.cat(waveform[:, :, 1:], waveform[:, :, 4:].flip(-1))
+                fold = torch.cat(waveform[:, :, s:], waveform[:, :, e - num_frames:].flip(-1), dim=-1)
+        else:
+            #            num_frames == 4
+            #                          |  s   e
+            # tensor index      0 1 2 3 4 5 6 7
+            #                             ^^^ x
+            # corresponding     0 1 2 3 3 2 1 0
+            # mirrored index              ^^^ x
+            #
+            # resulting slice   torch.cat(waveform[:, :, 1:3].flip(-1))
+            fold = torch.cat(waveform[:, :, s - num_frames:e - num_frames].flip(-1), dim=-1)
+        folds.append(fold)
     return torch.cat(folds, dim=1)
