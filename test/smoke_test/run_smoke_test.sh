787f90f1e98e19b83330b06402d38ca93b4e3e07
moto <855818+mthrok@users.noreply.github.com>
2022-05-05 09:26:03 -0700

    Refactor smoke test executions
    Originally, the CI was executing 'import torchaudio' as a smoke test,
    but this has to be executed from somewhere other than the root directory
    of the repo.
    
    This commit introduces common scripts to perform that.

---
+++ test/smoke_test/run_smoke_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script is used by CI to perform smoke tests on installed binaries.
+
+# When `import torchaudio` is executed from the root directory of the repo,
+# the source `torchaudio` directory will shadow the actual installation.
+# Changing to this directory to avoid that.
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+python smoke_test.py
