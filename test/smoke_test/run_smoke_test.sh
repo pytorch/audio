694bad57a71b196bc2d12e2875fea99245a8c0bc
moto <855818+mthrok@users.noreply.github.com>
2022-05-06 10:21:19 -0700

    set -e

--- test/smoke_test/run_smoke_test.sh
+++ test/smoke_test/run_smoke_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eux
+
 # This script is used by CI to perform smoke tests on installed binaries.
 
 # When `import torchaudio` is executed from the root directory of the repo,
