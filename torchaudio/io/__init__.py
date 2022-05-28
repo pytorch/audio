65ab62e6b73a941d09d8b45b23b389a6fae46f5a
moto <855818+mthrok@users.noreply.github.com>
2022-05-28 13:09:26 -0700

    Update I/O initialization (#2417)
    Summary:
    Attempt to load ffmpeg extension at the top level import
    
    Preparation to use ffmpeg-based I/O as a fallback for sox_io backend.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2417
    
    Reviewed By: carolineechen
    
    Differential Revision: D36736989
    
    Pulled By: mthrok
    
    fbshipit-source-id: 0beb6f459313b5ea91597393ccb12571444c54d9

--- torchaudio/io/__init__.py
+++ torchaudio/io/__init__.py
@@ -1,4 +1,5 @@
-_INITIALIZED = False
+import torchaudio
+
 _LAZILY_IMPORTED = [
     "StreamReader",
     "StreamReaderSourceStream",
@@ -8,32 +9,10 @@ _LAZILY_IMPORTED = [
 ]
 
 
-def _init_extension():
-    import torch
-    import torchaudio
-
-    try:
-        torchaudio._extension._load_lib("libtorchaudio_ffmpeg")
-        import torchaudio._torchaudio_ffmpeg
-    except OSError as err:
-        raise ImportError(
-            "Stream API requires FFmpeg libraries (libavformat and such). Please install FFmpeg 4."
-        ) from err
-    try:
-        torch.ops.torchaudio.ffmpeg_init()
-    except RuntimeError as err:
-        raise RuntimeError(
-            "Stream API requires FFmpeg binding. Please set USE_FFMPEG=1 when building from source."
-        ) from err
-
-    global _INITIALIZED
-    _INITIALIZED = True
-
-
 def __getattr__(name: str):
     if name in _LAZILY_IMPORTED:
-        if not _INITIALIZED:
-            _init_extension()
+
+        torchaudio._extension._init_ffmpeg()
 
         from . import _stream_reader
 
