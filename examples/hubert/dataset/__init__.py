1412566c08c8e00b11ded66574be1bf38f3dff8f
Zhaoheng Ni <zni@fb.com>
2022-06-07 13:25:35 -0700

    Add HuBERT fine-tuning recipe (#2352)
    Summary:
    The PR contains the CTC fine-tuning recipe of HuBERT Base model.
    The files include:
    - lightning module
    - training script
    - README and the result table
    - evaluation scripts
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2352
    
    Reviewed By: hwangjeff
    
    Differential Revision: D36915712
    
    Pulled By: nateanl
    
    fbshipit-source-id: 0249635ad5e81a8aa2d228c1d5fe84d78b62a15b

--- examples/hubert/dataset/__init__.py
+++ examples/hubert/dataset/__init__.py
@@ -1,8 +1,18 @@
-from .hubert_dataset import BucketizeBatchSampler, CollateFnHubert, HuBERTDataSet
+from .hubert_dataset import (
+    _get_lengths_librilightlimited,
+    _get_lengths_librispeech,
+    BucketizeBatchSampler,
+    CollateFnHubert,
+    CollateFnLibriLightLimited,
+    HuBERTDataSet,
+)
 
 
 __all__ = [
+    "_get_lengths_librilightlimited",
+    "_get_lengths_librispeech",
     "BucketizeBatchSampler",
     "CollateFnHubert",
+    "CollateFnLibriLightLimited",
     "HuBERTDataSet",
 ]
