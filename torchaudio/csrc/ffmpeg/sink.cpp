0dd57236b63341b91e6915da2cf3a3beab752c22
moto <855818+mthrok@users.noreply.github.com>
2022-06-27 19:43:42 -0700

    Refactor FilterGraph interface (#2508)
    Summary:
    FilterGraph is necessary for StreamWriter when saving video as
    Tensor array format cannot express commonot video formats like yub420.
    
    The current implementation of FilterGraph is specific to StreamReader,
    as it takes AVCodecParameters object. Not individual parameters.
    
    This PR refactor FilterGraph interface so that it can be constructed
    from more primitive information.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2508
    
    Reviewed By: hwangjeff
    
    Differential Revision: D37466033
    
    Pulled By: mthrok
    
    fbshipit-source-id: 8414e985da7579c2dfe260b4dccd2afe113bb573

--- torchaudio/csrc/ffmpeg/sink.cpp
+++ torchaudio/csrc/ffmpeg/sink.cpp
@@ -23,18 +23,54 @@ std::unique_ptr<Buffer> get_buffer(
           av_get_media_type_string(type));
   }
 }
+
+std::unique_ptr<FilterGraph> get_filter_graph(
+    AVRational input_time_base,
+    AVCodecParameters* codecpar,
+    const std::string& filter_description) {
+  auto p = std::make_unique<FilterGraph>(codecpar->codec_type);
+
+  switch (codecpar->codec_type) {
+    case AVMEDIA_TYPE_AUDIO:
+      p->add_audio_src(
+          static_cast<AVSampleFormat>(codecpar->format),
+          input_time_base,
+          codecpar->sample_rate,
+          codecpar->channel_layout);
+      break;
+    case AVMEDIA_TYPE_VIDEO:
+      p->add_video_src(
+          static_cast<AVPixelFormat>(codecpar->format),
+          input_time_base,
+          codecpar->width,
+          codecpar->height,
+          codecpar->sample_aspect_ratio);
+      break;
+    default:
+      throw std::runtime_error("Only audio/video are supported.");
+  }
+  p->add_sink();
+  p->add_process(filter_description);
+  p->create_filter();
+  return p;
+}
+
 } // namespace
 
 Sink::Sink(
-    AVRational input_time_base,
-    AVCodecParameters* codecpar,
+    AVRational input_time_base_,
+    AVCodecParameters* codecpar_,
     int frames_per_chunk,
     int num_chunks,
-    const c10::optional<std::string>& filter_description,
+    const c10::optional<std::string>& filter_description_,
     const torch::Device& device)
-    : filter(input_time_base, codecpar, filter_description),
+    : input_time_base(input_time_base_),
+      codecpar(codecpar_),
+      filter_description(filter_description_.value_or(
+          codecpar->codec_type == AVMEDIA_TYPE_AUDIO ? "anull" : "null")),
+      filter(get_filter_graph(input_time_base_, codecpar_, filter_description)),
       buffer(get_buffer(
-          codecpar->codec_type,
+          codecpar_->codec_type,
           frames_per_chunk,
           num_chunks,
           device)) {}
@@ -42,9 +78,9 @@ Sink::Sink(
 // 0: some kind of success
 // <0: Some error happened
 int Sink::process_frame(AVFrame* pFrame) {
-  int ret = filter.add_frame(pFrame);
+  int ret = filter->add_frame(pFrame);
   while (ret >= 0) {
-    ret = filter.get_frame(frame);
+    ret = filter->get_frame(frame);
     //  AVERROR(EAGAIN) means that new input data is required to return new
     //  output.
     if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
@@ -56,12 +92,16 @@ int Sink::process_frame(AVFrame* pFrame) {
   return ret;
 }
 
+std::string Sink::get_filter_description() const {
+  return filter_description;
+}
+
 bool Sink::is_buffer_ready() const {
   return buffer->is_ready();
 }
 
 void Sink::flush() {
-  filter.reset();
+  filter = get_filter_graph(input_time_base, codecpar, filter_description);
   buffer->flush();
 }
 
