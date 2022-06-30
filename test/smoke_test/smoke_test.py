3aa9e74e3deae7b1e472d98a18b98797cfc8f7f1
moto <855818+mthrok@users.noreply.github.com>
2022-06-30 10:49:38 -0700

    put back conda-forge

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -1,4 +1,13 @@
 """Run smoke tests"""
+import os
+import sys
+
+
+if os.name == "nt":
+    if sys.version_info[:2] >= (3, 8) and "FFMPEG_ROOT" in os.environ:
+        # FFMPEG_ROOT is expected to have the dlls like avcodec-58.dll
+        os.add_dll_directory(os.environ["FFMPEG_ROOT"])
+
 
 import torchaudio  # noqa: F401
 import torchaudio.compliance.kaldi  # noqa: F401
