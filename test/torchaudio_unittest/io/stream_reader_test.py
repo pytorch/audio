b2a90f9189505d3651fa882f993437fbabd3a63e
moto <855818+mthrok@users.noreply.github.com>
2022-07-06 21:08:37 -0700

    Add YUV444P support to StreamReader (#2516)
    Summary:
    This commit add support for `"yuv444p"` type as output format of StreamReader.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2516
    
    Reviewed By: hwangjeff
    
    Differential Revision: D37659715
    
    Pulled By: mthrok
    
    fbshipit-source-id: eae9b5590d8f138a6ebf3808c08adfe068f11a2b

--- test/torchaudio_unittest/io/stream_reader_test.py
+++ test/torchaudio_unittest/io/stream_reader_test.py
@@ -7,6 +7,8 @@ from torchaudio_unittest.common_utils import (
     get_wav_data,
     is_ffmpeg_available,
     nested_params,
+    rgb_to_gray,
+    rgb_to_yuv_ccir,
     save_image,
     save_wav,
     skipIfNoFFmpeg,
@@ -614,3 +616,29 @@ class StreamReaderImageTest(_MediaSourceMixin, TempDirMixin, TorchaudioTestCase)
         print("expected", expected)
         print("output", output)
         self.assertEqual(expected, output)
+
+    def test_png_yuv_read_out(self):
+        """Providing format prpoerly change the color space"""
+        rgb = torch.empty(1, 3, 256, 256, dtype=torch.uint8)
+        rgb[0, 0] = torch.arange(256, dtype=torch.uint8).reshape([1, -1])
+        rgb[0, 1] = torch.arange(256, dtype=torch.uint8).reshape([-1, 1])
+        for i in range(256):
+            rgb[0, 2] = i
+            path = self.get_temp_path(f"ref_{i}.png")
+            save_image(path, rgb[0], mode="RGB")
+
+            yuv = rgb_to_yuv_ccir(rgb)
+            bgr = rgb[:, [2, 1, 0], :, :]
+            gray = rgb_to_gray(rgb)
+
+            s = StreamReader(path)
+            s.add_basic_video_stream(frames_per_chunk=-1, format="yuv444p")
+            s.add_basic_video_stream(frames_per_chunk=-1, format="rgb24")
+            s.add_basic_video_stream(frames_per_chunk=-1, format="bgr24")
+            s.add_basic_video_stream(frames_per_chunk=-1, format="gray8")
+            s.process_all_packets()
+            output_yuv, output_rgb, output_bgr, output_gray = s.pop_chunks()
+            self.assertEqual(yuv, output_yuv, atol=1, rtol=0)
+            self.assertEqual(rgb, output_rgb, atol=0, rtol=0)
+            self.assertEqual(bgr, output_bgr, atol=0, rtol=0)
+            self.assertEqual(gray, output_gray, atol=1, rtol=0)
