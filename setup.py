00dad72cbccb0ba5b9564d6b30ddc504a0a1be21
moto <855818+mthrok@users.noreply.github.com>
2022-07-25 18:21:21 -0700

    fixup! Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ def _run_cmd(cmd):
 
 
 def _get_version(sha):
-    version = "0.12.1"
+    with open(ROOT_DIR / "version.txt", "r") as f:
+        version = f.read().strip()
     if os.getenv("BUILD_VERSION"):
         version = os.getenv("BUILD_VERSION")
     elif sha is not None:
