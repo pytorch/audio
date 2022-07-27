d9140a61aa51d5af0c03887bcad9c46036c33dad
Sean Kim <skim0514@fb.com>
2022-07-26 22:06:04 -0400

    change apply function and other changes

--- examples/tutorials/hybrid_demucs_tutorial.py
+++ examples/tutorials/hybrid_demucs_tutorial.py
@@ -74,8 +74,8 @@ print(torchaudio.__version__)
 # Pre-trained model weights and related pipeline components are bundled as
 # :py:func:`torchaudio.pipelines.HDEMUCS_HIGH_MUSDB_PLUS`. This is a
 # HDemucs model trained on
-# `MUSDB18-HQ <https://zenodo.org/record/3338373>`__ and other
-# alternative sources.
+# `MUSDB18-HQ <https://zenodo.org/record/3338373>`__ and other alternative
+# sources.
 # 
 
 from torchaudio.prototype import pipelines
@@ -104,13 +104,14 @@ print(f"Sample rate: {sample_rate}")
 # 
 
 from torch.nn import functional as F
+from torchaudio.transforms import Fade
 
 # just leave overlap and segmenent, If device is provided it can be useful in some cases when separating very long tracks, cross fading at the 
 def apply_model(model, mix, segment=10.,
-                overlap=0.25, device=None,
+                overlap=0.1, device=None,
                 ):
     """
-    Apply model to a given mixture.
+    Apply model to a given mixture. Use fade, and add segments together in order to add model segment by segment. 
 
     Args:
         segment (int): What the segment length will be in seconds
@@ -124,117 +125,38 @@ def apply_model(model, mix, segment=10.,
     else:
         device = torch.device(device)
 
-    kwargs = {
-        'segment': segment,
-        'overlap': overlap,
-        'device': device,
-    }
-
     model.to(device)
     batch, channels, length = mix.shape
-    if segment != 0:
-        kwargs['segment'] = 0
-        out = torch.zeros(batch, 4, channels, length, device=mix.device)
-        sum_weight = torch.zeros(length, device=mix.device)
-        segment = int(sample_rate * segment)
-        stride = int((1 - overlap) * segment)
-        offsets = range(0, length, stride)
-        scale = stride / sample_rate
-        # We start from a triangle shaped weight, with maximal weight in the middle
-        # of the segment.
-        weight = torch.cat([torch.arange(1, segment // 2 + 1, device=device),
-                         torch.arange(segment - segment // 2, 0, -1, device=device)])
-        assert len(weight) == segment
-        weight = (weight / weight.max())
-        futures = []
-        for offset in offsets:
-            chunk = TensorChunk(mix, offset, segment)
-            future = apply_model(model, chunk, **kwargs)
-            futures.append((future, offset))
-            offset += segment
-        for future, offset in futures:
-            chunk_out = future
-            chunk_length = chunk_out.shape[-1]
-            out[..., offset:offset + segment] += (weight[:chunk_length] * chunk_out).to(mix.device)
-            sum_weight[offset:offset + segment] += weight[:chunk_length].to(mix.device)
-        assert sum_weight.min() > 0
-        out /= sum_weight
-        return out
-    else:
-        valid_length = length
-        mix = tensor_chunk(mix)
-        padded_mix = mix.padded(valid_length).to(device)
-        with torch.no_grad():
-            out = model.forward(padded_mix)
-        return center_trim(out, length)
-
-class TensorChunk:
-    def __init__(self, tensor, offset=0, length=None):
-        total_length = tensor.shape[-1]
-        assert offset >= 0
-        assert offset < total_length
-
-        if length is None:
-            length = total_length - offset
-        else:
-            length = min(total_length - offset, length)
-
-        self.tensor = tensor
-        self.offset = offset
-        self.length = length
-        self.device = tensor.device
-
-    @property
-    def shape(self):
-        shape = list(self.tensor.shape)
-        shape[-1] = self.length
-        return shape
-
-    def padded(self, target_length):
-        delta = target_length - self.length
-        total_length = self.tensor.shape[-1]
-        assert delta >= 0
-
-        start = self.offset - delta // 2
-        end = start + target_length
-
-        correct_start = max(0, start)
-        correct_end = min(total_length, end)
-
-        pad_left = correct_start - start
-        pad_right = end - correct_end
-
-        out = F.pad(self.tensor[..., correct_start:correct_end], (pad_left, pad_right))
-        assert out.shape[-1] == target_length
-        return out
-
-
-def tensor_chunk(tensor_or_chunk):
-    if isinstance(tensor_or_chunk, TensorChunk):
-        return tensor_or_chunk
-    else:
-        assert isinstance(tensor_or_chunk, torch.Tensor)
-        return TensorChunk(tensor_or_chunk)
     
-def center_trim(tensor: torch.Tensor, reference: typing.Union[torch.Tensor, int]):
-    """
-    Center trim `tensor` with respect to `reference`, along the last dimension.
-    `reference` can also be a number, representing the length to trim to.
-    If the size difference != 0 mod 2, the extra sample is removed on the right side.
-    """
-    ref_size: int
-    if isinstance(reference, torch.Tensor):
-        ref_size = reference.size(-1)
-    else:
-        ref_size = reference
-    delta = tensor.size(-1) - ref_size
-    if delta < 0:
-        raise ValueError("tensor must be larger than reference. " f"Delta is {delta}.")
-    if delta:
-        tensor = tensor[..., delta // 2:-(delta - delta // 2)]
-    return tensor
+    chunk_len = int(sample_rate * segment * (1 + overlap))
+    prev = 0
+    index = prev + chunk_len
+    fade = Fade(fade_in_len = 0, fade_out_len = int(overlap * sample_rate), fade_shape='linear')
+    
+    final = torch.zeros(batch, 4, channels, length, device=device)
+    
+    print(prev, index)
+    while index < length:
+        chunk = mix[:,:,prev:index]
+        with torch.no_grad():
+            out = model.forward(chunk)
+            
+        if prev != 0:
+            fade = Fade(fade_in_len = int(overlap * sample_rate), fade_out_len = int(overlap * sample_rate), fade_shape='linear')
+        out = fade(out)
+        final[:,:,:,prev:index] += out
+        
+        prev = int(index - overlap * sample_rate)
+        index += chunk_len
+    chunk = mix[:,:,prev:]
+    with torch.no_grad():
+        out = model.forward(chunk)
+    fade = Fade(fade_in_len = int(overlap * sample_rate), fade_out_len = 0, fade_shape='linear')
+    out = fade(out)
+    final[:,:,:,prev:] += out
+    return final
 
-def plot_spectrogram(stft, title="Spectrogram", xlim=None):
+def plot_spectrogram(stft, title="Spectrogram"):
     magnitude = stft.abs()
     spectrogram = 20 * torch.log10(magnitude + 1e-8).numpy()
     figure, axis = plt.subplots(1, 1)
@@ -271,7 +193,7 @@ store = waveform
 
 #parameters
 segment: int = 10
-overlap = 0.25
+overlap = 0.1
 
 output_folder = "output"
 song_name = "test" #change accordingly
@@ -337,8 +259,16 @@ Audio(track, rate=sample_rate)
 #DRUMS
 
 track = audios["drums"]
-#will take a chunk of 10 seconds in the middle
+#will take a chunk of 10 seconds in the middle between seconds 10-20
 segment = track[:, 10*sample_rate: 20*sample_rate]
+
+#get original drums track
+drums_original = download_asset("tutorial-assets/hdemucs_drums.wav")
+waveform, sample_rate = torchaudio.load(drums_original)
+
+#calculate sdr score and print
+print("SDR score is:", mir_eval.separation.bss_eval_sources(waveform.detach().numpy(), segment.detach().numpy())[0].mean())
+
 plot_spectrogram(stft(segment)[0], "Spectrogram Drums")
 
 Audio(track, rate=sample_rate)
@@ -353,9 +283,16 @@ Audio(track, rate=sample_rate)
 
 #Bass
 track = audios["bass"]
-#will take a chunk of 5 seconds in the middle between seconds 20-25
-segment = track[:, 20*sample_rate: 25*sample_rate]
+#will take a chunk of 10 seconds in the middle between seconds 10-20
+segment = track[:, 10*sample_rate: 20*sample_rate]
 plot_spectrogram(stft(segment)[0], "Spectrogram Bass")
+
+#get original bass track
+bass_original = download_asset("tutorial-assets/hdemucs_bass.wav")
+waveform, sample_rate = torchaudio.load(bass_original)
+
+#calculate sdr score and print
+print("SDR score is:", mir_eval.separation.bss_eval_sources(waveform.detach().numpy(), segment.detach().numpy())[0].mean())
 
 Audio(track, rate=sample_rate)
 
@@ -370,9 +307,16 @@ Audio(track, rate=sample_rate)
 #Other
 track = audios["other"]
 
-#will take a chunk of 5 seconds in the middle between seconds 20-25
-segment = track[:, 20*sample_rate: 25*sample_rate]
+#will take a chunk of 10 seconds in the middle between seconds 10-20
+segment = track[:, 10*sample_rate: 20*sample_rate]
 plot_spectrogram(stft(segment)[0], "Spectrogram Other")
+
+#get original other track
+other_original = download_asset("tutorial-assets/hdemucs_other.wav")
+waveform, sample_rate = torchaudio.load(other_original)
+
+#calculate sdr score and print
+print("SDR score is:", mir_eval.separation.bss_eval_sources(waveform.detach().numpy(), segment.detach().numpy())[0].mean())
 
 Audio(track, rate=sample_rate)
 
@@ -387,9 +331,16 @@ Audio(track, rate=sample_rate)
 #Vocals
 track = audios["vocals"]
 
-#will take a chunk of 5 seconds in the middle between seconds 20-25
-segment = track[:, 20*sample_rate: 25*sample_rate]
+#will take a chunk of 10 seconds in the middle between seconds 10-20
+segment = track[:, 10*sample_rate: 20*sample_rate]
+
+#get original bass track
+bass_original = download_asset("tutorial-assets/hdemucs_bass.wav")
+waveform, sample_rate = torchaudio.load(bass_original)
+
+#calculate sdr score and print
+print("SDR score is:", mir_eval.separation.bss_eval_sources(waveform.detach().numpy(), segment.detach().numpy())[0].mean())
+
 plot_spectrogram(stft(segment)[0], "Spectrogram Vocals")
 
 Audio(track, rate=sample_rate)
-
