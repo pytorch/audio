5e17af8403524aaaec925c86e1ad353fbde7a042
Javier Cardenete Morales <jcardenete@fb.com>
2022-07-28 20:02:11 -0700

    Replace 'runtime_error' exception with 'TORCH_CHECK' in TorchAudio sox (#2592)
    Summary:
    Pull Request resolved: https://github.com/pytorch/audio/pull/2592
    
    std::runtime_error does not preserve the C++ stack trace, so it is unclear to users what went wrong internally.
    
    PyTorch's TORCH_CHECK macro allows to print C++ stack trace when TORCH_SHOW_CPP_STACKTRACES environment variable is set to 1.
    
    Reviewed By: mthrok
    
    Differential Revision: D38219331
    
    fbshipit-source-id: 717d818a39024401c8ee69d56652018f5812f667

--- torchaudio/csrc/sox/effects.cpp
+++ torchaudio/csrc/sox/effects.cpp
@@ -20,16 +20,15 @@ void initialize_sox_effects() {
 
   switch (SOX_RESOURCE_STATE) {
     case NotInitialized:
-      if (sox_init() != SOX_SUCCESS) {
-        throw std::runtime_error("Failed to initialize sox effects.");
-      };
+      TORCH_CHECK(
+          sox_init() == SOX_SUCCESS, "Failed to initialize sox effects.");
       SOX_RESOURCE_STATE = Initialized;
       break;
     case Initialized:
       break;
     case ShutDown:
-      throw std::runtime_error(
-          "SoX Effects has been shut down. Cannot initialize again.");
+      TORCH_CHECK(
+          false, "SoX Effects has been shut down. Cannot initialize again.");
   }
 };
 
@@ -38,12 +37,10 @@ void shutdown_sox_effects() {
 
   switch (SOX_RESOURCE_STATE) {
     case NotInitialized:
-      throw std::runtime_error(
-          "SoX Effects is not initialized. Cannot shutdown.");
+      TORCH_CHECK(false, "SoX Effects is not initialized. Cannot shutdown.");
     case Initialized:
-      if (sox_quit() != SOX_SUCCESS) {
-        throw std::runtime_error("Failed to initialize sox effects.");
-      };
+      TORCH_CHECK(
+          sox_quit() == SOX_SUCCESS, "Failed to initialize sox effects.");
       SOX_RESOURCE_STATE = ShutDown;
       break;
     case ShutDown:
