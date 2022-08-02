9c54cd7a67053457475b8fdbba66919e449e536e
bshall <benji.l.shall@gmail.com>
2022-08-02 15:40:10 +0200

    Removed torchaudio_unittest module from loudness test

--- test/integration_tests/loudness_compliance_cpu_test.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torchaudio_unittest import common_utils
-
-from .loudness_compliance_test_impl import Loudness
-
-
-class TestLoudnessCPU(Loudness, common_utils.PytorchTestCase):
-    device = torch.device("cpu")
--- test/integration_tests/loudness_compliance_cuda_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchaudio_unittest import common_utils
-
-from .loudness_compliance_test_impl import Loudness
-
-
-@common_utils.skipIfNoCuda
-class TestLoudnessCUDA(Loudness, common_utils.PytorchTestCase):
-    device = torch.device("cuda")
