26c4bb5502a85c71c09f9c71ac0dbc797a9d155a
Nirav Mehta <niravmehta@fb.com>
2022-08-10 12:24:57 -0700

    Adding environment variables for version

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -2,7 +2,16 @@
 set -ex
 
 echo "Running pre build script..."
+export FFMPEG_ROOT=${PWD}/third_party/ffmpeg
+if [[ ! -d ${FFMPEG_ROOT} ]]; then
+    packaging/ffmpeg/build.sh
+fi
 echo FFMPEG_ROOT=${FFMPEG_ROOT}
+
+echo "Setting environment variables for versions..."
+export PYTHON_VERSION='3.7'
+export CU_VERSION=cu113
+
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
