5346c44107a421dbc47c1eaef4f86132d30a9a31
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared47064.03.ldc1.facebook.com>
2022-08-12 04:30:32 -0700

    2022-08-12 nightly release (f3bb30b8893b4700c668bd917389a28905831aaf)

--- test/torchaudio_unittest/prototype/functional/autograd_test_impl.py
+++ test/torchaudio_unittest/prototype/functional/autograd_test_impl.py
@@ -19,3 +19,15 @@ class AutogradTestImpl(TestBaseMixin):
         y = torch.rand(*leading_dims, L_y, dtype=self.dtype, device=self.device, requires_grad=True)
         self.assertTrue(gradcheck(fn, (x, y)))
         self.assertTrue(gradgradcheck(fn, (x, y)))
+
+    def test_add_noise(self):
+        leading_dims = (5, 2, 3)
+        L = 51
+
+        waveform = torch.rand(*leading_dims, L, dtype=self.dtype, device=self.device, requires_grad=True)
+        noise = torch.rand(*leading_dims, L, dtype=self.dtype, device=self.device, requires_grad=True)
+        lengths = torch.rand(*leading_dims, dtype=self.dtype, device=self.device, requires_grad=True)
+        snr = torch.rand(*leading_dims, dtype=self.dtype, device=self.device, requires_grad=True) * 10
+
+        self.assertTrue(gradcheck(F.add_noise, (waveform, noise, lengths, snr)))
+        self.assertTrue(gradgradcheck(F.add_noise, (waveform, noise, lengths, snr)))
