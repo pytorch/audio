cf5414fb410a3931301a98726b4a2515c4c61524
moto <855818+mthrok@users.noreply.github.com>
2022-08-23 19:18:38 -0700

    Add StreamWriter (#2628)
    Summary:
    This commit adds FFmpeg-based encoder StreamWriter class.
    StreamWriter is pretty much the opposite of StreamReader class, and
    it supports;
    
    * Encoding audio / still image / video
    * Exporting to local file / streaming protocol / devices etc...
    * File-like object support (in later commit)
    * HW video encoding (in later commit)
    
    See also: https://fburl.com/gslide/z85kn5a9 (Meta internal)
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2628
    
    Reviewed By: nateanl
    
    Differential Revision: D38816650
    
    Pulled By: mthrok
    
    fbshipit-source-id: fc5ce7c9c621deff8f96c5dce3b00a476602156b

--- torchaudio/io/__init__.py
+++ torchaudio/io/__init__.py
@@ -1,6 +1,6 @@
 import torchaudio
 
-_LAZILY_IMPORTED = [
+_STREAM_READER = [
     "StreamReader",
     "StreamReaderSourceStream",
     "StreamReaderSourceAudioStream",
@@ -8,15 +8,28 @@ _LAZILY_IMPORTED = [
     "StreamReaderOutputStream",
 ]
 
+_STREAM_WRITER = [
+    "StreamWriter",
+]
+
+
+_LAZILY_IMPORTED = _STREAM_READER + _STREAM_WRITER
+
 
 def __getattr__(name: str):
     if name in _LAZILY_IMPORTED:
-
         torchaudio._extension._init_ffmpeg()
 
-        from . import _stream_reader
+        if name in _STREAM_READER:
+            from . import _stream_reader
 
-        item = getattr(_stream_reader, name)
+            item = getattr(_stream_reader, name)
+
+        else:
+            from . import _stream_writer
+
+            item = getattr(_stream_writer, name)
+
         globals()[name] = item
         return item
     raise AttributeError(f"module {__name__} has no attribute {name}")
