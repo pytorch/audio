62c7cfd9091d56888e2c17d55d3ebd02b33936e5
Caroline Chen <carolinechen@fb.com>
2022-09-12 11:39:46 -0400

    address comment

--- torchaudio/prototype/models/hdemucs/__init__.py
+++ torchaudio/prototype/models/hdemucs/__init__.py
@@ -1,8 +1,10 @@
 import torchaudio
 
+functions = ["HDemucs", "hdemucs_high", "hdemucs_medium", "hdemucs_low"]
+
 
 def __getattr__(name: str):
-    if name in ["HDemucs", "hdemucs_high", "hdemucs_medium", "hdemucs_low"]:
+    if name in functions:
         import warnings
 
         warnings.warn(
@@ -10,11 +12,10 @@ def __getattr__(name: str):
             DeprecationWarning,
         )
 
-        demucs = eval(f"torchaudio.models.{name}")
-        return demucs
+        return getattr(torchaudio.models, name)
 
     raise AttributeError(f"module {__name__} has no attribute {name}")
 
 
 def __dir__():
-    return ["HDemucs", "hdemucs_high", "hdemucs_medium", "hdemucs_low"]
+    return functions
