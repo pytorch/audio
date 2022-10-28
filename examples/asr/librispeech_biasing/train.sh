e063b336c84d96bb531b013f546121812d962279
G. Sun <gs534@login-e-1.data.cluster>
2022-10-28 11:33:00 +0100

    code for inference with TCPGen

--- examples/asr/librispeech_biasing/train.sh
+++ examples/asr/librispeech_biasing/train.sh
@@ -1,6 +1,6 @@
 . /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate torchaudio
 export LD_PRELOAD=/lib64/libgsm.so
-expdir="./experiments/librispeech_clean100_suffix_attndeepbiasing"
+expdir="./experiments/librispeech_clean100_suffix_attndeepbiasing_sche50"
 # expdir="./experiments/librispeech_clean100_suffix_baseline"
 mkdir -p $expdir
 python train.py \
@@ -9,7 +9,7 @@ python train.py \
     --global-stats-path ./global_stats_100.json \
     --sp-model-path ./spm_unigram_1023_100suffix.model \
     --biasinglist ./blists/rareword_f30.txt \
-    --droprate 0.3 \
-    --maxsize 1000 \
+    --droprate 0.1 \
+    --maxsize 500 \
     --epochs 160 \
-    # --resume $expdir/checkpoints/epoch\=51-step\=101556.ckpt \
+    --resume $expdir/checkpoints/epoch\=44-step\=87885.ckpt \
--- torchaudio/prototype/models/__init__.py
+++ torchaudio/prototype/models/__init__.py
@@ -3,6 +3,7 @@ from .conv_tasnet import conv_tasnet_base
 from .hdemucs import HDemucs, hdemucs_high, hdemucs_low, hdemucs_medium
 from .rnnt import conformer_rnnt_base, conformer_rnnt_model
 from .rnnt import conformer_rnnt_biasing, conformer_rnnt_biasing_base
+from .rnnt_decoder import Hypothesis, RNNTBeamSearchBiasing
 
 __all__ = [
     "conformer_rnnt_base",
@@ -15,4 +16,6 @@ __all__ = [
     "hdemucs_high",
     "hdemucs_medium",
     "hdemucs_low",
+    "Hypothesis",
+    "RNNTBeamSearchBiasing"
 ]
