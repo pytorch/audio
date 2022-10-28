1c06c620bbc567e04147f5687c97c53fd63581b3
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared51702.04.ncg1.facebook.com>
2022-10-28 14:17:30 -0700

    2022-10-28 nightly release (86d596d37d0e591d34c86375d516243c21a8ce8e)

--- test/torchaudio_unittest/prototype/functional/batch_consistency_test.py
+++ test/torchaudio_unittest/prototype/functional/batch_consistency_test.py
@@ -6,17 +6,20 @@ from torchaudio_unittest.common_utils import nested_params, TorchaudioTestCase
 class BatchConsistencyTest(TorchaudioTestCase):
     @nested_params(
         [F.convolve, F.fftconvolve],
+        ["full", "valid", "same"],
     )
-    def test_convolve(self, fn):
+    def test_convolve(self, fn, mode):
         leading_dims = (2, 3)
         L_x, L_y = 89, 43
         x = torch.rand(*leading_dims, L_x, dtype=self.dtype, device=self.device)
         y = torch.rand(*leading_dims, L_y, dtype=self.dtype, device=self.device)
 
-        actual = fn(x, y)
+        actual = fn(x, y, mode)
         expected = torch.stack(
             [
-                torch.stack([fn(x[i, j].unsqueeze(0), y[i, j].unsqueeze(0)).squeeze(0) for j in range(leading_dims[1])])
+                torch.stack(
+                    [fn(x[i, j].unsqueeze(0), y[i, j].unsqueeze(0), mode).squeeze(0) for j in range(leading_dims[1])]
+                )
                 for i in range(leading_dims[0])
             ]
         )
