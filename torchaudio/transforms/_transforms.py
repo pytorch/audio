73fcea027a6013361b38f8a8552ab58e555bc995
Julian Arias <judaarlo@gmail.com>
2022-11-01 20:39:18 +0100

    fix Format files mistakes (pytorch#2103)

--- torchaudio/transforms/_transforms.py
+++ torchaudio/transforms/_transforms.py
@@ -844,7 +844,7 @@ class BarkSpectrogram(torch.nn.Module):
 
     Sources
         * https://www.fon.hum.uva.nl/praat/manual/BarkSpectrogram.html
-        * Traunmüller, Hartmut. "Analytical Expressions for the Tonotopic Sensory Scale." Journal of the Acoustical 
+        * Traunmüller, Hartmut. "Analytical Expressions for the Tonotopic Sensory Scale." Journal of the Acoustical
         * Society of America. Vol. 88, Issue 1, 1990, pp. 97–100.
         * https://ccrma.stanford.edu/courses/120-fall-2003/lecture-5.html
 
