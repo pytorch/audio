f631190268900833dc99474f34f90e9ed4f586ff
Grigory Sizov <grigorysizov@fb.com>
2022-11-03 20:07:16 +0100

    Add bundle for Base+ WavLM model

--- torchaudio/pipelines/_wav2vec2/impl.py
+++ torchaudio/pipelines/_wav2vec2/impl.py
@@ -1213,8 +1213,52 @@ WAVLM_BASE = Wav2Vec2Bundle(
     },
     _sample_rate=16000,
 )
-WAVLM_BASE.__doc__ = """WavLM model ("base" architecture),
+WAVLM_BASE.__doc__ = """WavLM Base model ("base" architecture),
 pre-trained on 960 hours of unlabeled audio from *LibriSpeech* dataset :cite:`7178964`, not fine-tuned.
+
+Originally published by the authors of *WavLM* :cite:`wavlm2021` under MIT License and
+redistributed with the same license.
+[`License <https://github.com/microsoft/unilm/blob/65f15af2a307ebb64cfb25adf54375b002e6fe8d/LICENSE>`__,
+`Source https://github.com/microsoft/unilm/tree/65f15af2a307ebb64cfb25adf54375b002e6fe8d/wavlm#pre-trained-models>`__]
+
+Please refer to :py:class:`torchaudio.pipelines.Wav2Vec2Bundle` for the usage.
+"""  # noqa: E501
+
+
+WAVLM_BASE_PLUS = Wav2Vec2Bundle(
+    "TODO.pth",
+    {
+        "extractor_mode": "group_norm",
+        "extractor_conv_layer_config": [
+            (512, 10, 5),
+            (512, 3, 2),
+            (512, 3, 2),
+            (512, 3, 2),
+            (512, 3, 2),
+            (512, 2, 2),
+            (512, 2, 2),
+        ],
+        "extractor_conv_bias": False,
+        "encoder_embed_dim": 768,
+        "encoder_projection_dropout": 0.1,
+        "encoder_pos_conv_kernel": 128,
+        "encoder_pos_conv_groups": 16,
+        "encoder_num_layers": 12,
+        "encoder_num_heads": 12,
+        "encoder_attention_dropout": 0.1,
+        "encoder_ff_interm_features": 3072,
+        "encoder_ff_interm_dropout": 0.0,
+        "encoder_dropout": 0.1,
+        "encoder_layer_norm_first": False,
+        "encoder_layer_drop": 0.05,
+        "aux_num_out": None,
+        "model_class": "WavLM",
+    },
+    _sample_rate=16000,
+)
+WAVLM_BASE_PLUS.__doc__ = """WavLM Base+ model ("base" architecture),
+pre-trained on 60,000 hours of Libri-Light dataset :cite:`librilight`, 10,000 hours of GigaSpeech :cite:`GigaSpeech2021`,
+and 24,000 hours of *VoxPopuli* :cite:`voxpopuli`, not fine-tuned.
 
 Originally published by the authors of *WavLM* :cite:`wavlm2021` under MIT License and
 redistributed with the same license.
@@ -1256,7 +1300,7 @@ WAVLM_LARGE = Wav2Vec2Bundle(
     },
     _sample_rate=16000,
 )
-WAVLM_LARGE.__doc__ = """WavLM model ("large" architecture),
+WAVLM_LARGE.__doc__ = """WavLM Large model ("large" architecture),
 pre-trained on 60,000 hours of Libri-Light dataset :cite:`librilight`, 10,000 hours of GigaSpeech :cite:`GigaSpeech2021`,
 and 24,000 hours of *VoxPopuli* :cite:`voxpopuli`, not fine-tuned.
 
