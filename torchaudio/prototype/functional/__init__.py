c9a6f0162467d594f3fd78e3717c3b46ca651154
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared7719.02.ldc5.facebook.com>
2022-11-18 04:30:43 -0800

    2022-11-18 nightly release (2bfb86480355e4231ec9d780ab39b7e4dbd0358f)

--- torchaudio/prototype/functional/__init__.py
+++ torchaudio/prototype/functional/__init__.py
@@ -1,8 +1,9 @@
-from ._dsp import oscillator_bank
+from ._dsp import adsr_envelope, oscillator_bank
 from .functional import add_noise, barkscale_fbanks, convolve, fftconvolve
 
 __all__ = [
     "add_noise",
+    "adsr_envelope",
     "barkscale_fbanks",
     "convolve",
     "fftconvolve",
