e8d0e4d24f459517d45a8f2a0f4fa2b5e97adeb7
Nicolas Hug <contact@nicolas-hug.com>
2022-11-23 14:46:07 +0000

    Remove unsused PRA

--- test/torchaudio_unittest/prototype/functional/functional_test_impl.py
+++ test/torchaudio_unittest/prototype/functional/functional_test_impl.py
@@ -6,9 +6,6 @@ from scipy import signal
 from torchaudio._internal import module_utils as _mod_utils
 from torchaudio_unittest.common_utils import nested_params, skipIfNoModule, TestBaseMixin
 
-if _mod_utils.is_module_available("pyroomacoustics"):
-    import pyroomacoustics as pra
-
 from .dsp_utils import oscillator_bank as oscillator_bank_np
 
 
