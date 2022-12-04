6157143c34e23ab8926b49026c538ec321700c06
G. Sun <gs534@login-e-4.data.cluster>
2022-12-04 17:29:07 +0000

    updated train.sh and eval.sh scripts

--- examples/asr/librispeech_biasing/train.sh
+++ examples/asr/librispeech_biasing/train.sh
@@ -1,16 +1,15 @@
 . /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate cuda113
 export LD_PRELOAD=/lib64/libgsm.so
-# expdir="./experiments/librispeech_clean100_suffix_attndeepbiasing_sche50"
-expdir="./experiments/librispeech_clean100_suffix_debug"
-# expdir="./experiments/librispeech_clean100_suffix_baseline"
+expdir="./experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop"
 mkdir -p $expdir
 python train.py \
     --exp-dir $expdir \
     --librispeech-path /home/gs534/rds/hpc-work/data/Librispeech/ \
     --global-stats-path ./global_stats_100.json \
-    --sp-model-path ./spm_unigram_1023_100suffix.model \
-    --biasinglist ./blists/rareword_f30.txt \
-    --droprate 0.1 \
+    --sp-model-path ./spm_unigram_600_100suffix.model \
+    --biasing true \
+    --biasinglist ./blists/rareword_f15.txt \
+    --droprate 0.0 \
     --maxsize 500 \
-    --epochs 160 \
-    # --resume $expdir/checkpoints/epoch\=44-step\=87885.ckpt \
+    --epochs 90 \
+    --resume experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop/checkpoints/epoch=45-step=89838.ckpt \
