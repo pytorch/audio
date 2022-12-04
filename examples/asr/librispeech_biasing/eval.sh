5076b0ae936f379acb008d659a47cf5f1d11fc2d
G. Sun <gs534@login-e-4.data.cluster>
2022-12-04 17:29:07 +0000

    updated train.sh and eval.sh scripts

--- examples/asr/librispeech_biasing/eval.sh
+++ examples/asr/librispeech_biasing/eval.sh
@@ -1,17 +1,16 @@
-. /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate torchaudio
+. /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate cuda113
 export LD_PRELOAD=/lib64/libgsm.so
-# expdir="./experiments/librispeech_clean100_suffix_baseline"
-expdir="./experiments/librispeech_clean100_suffix_attndeepbiasing"
-decode_dir=$expdir/decode_test_clean_b10
+expdir="./experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop"
+decode_dir=$expdir/decode_test_clean_b10_KB1000
 mkdir -p $decode_dir
-ckptpath=$expdir/checkpoints/epoch\=69-step\=136710.ckpt
-# ckptpath=$expdir/checkpoints/epoch\=78-step\=154287.ckpt
+ckptpath=$expdir/checkpoints/epoch=84-step=166005.ckpt 
 python eval.py \
     --checkpoint-path $ckptpath \
     --librispeech-path /home/gs534/rds/hpc-work/data/Librispeech/ \
-    --sp-model-path ./spm_unigram_1023_100suffix.model \
+    --sp-model-path ./spm_unigram_600_100suffix.model \
     --expdir $decode_dir \
     --use-cuda \
-    --biasinglist ./blists/rareword_f30.txt \
+    --biasing true \
+    --biasinglist ./blists/all_rare_words.txt \
     --droprate 0.0 \
     --maxsize 1000 \
