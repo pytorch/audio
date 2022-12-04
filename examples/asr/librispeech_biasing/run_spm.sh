2f9e012996758807ec32f5359f77e7d60b03e049
G. Sun <gs534@login-e-4.data.cluster>
2022-12-04 17:31:10 +0000

    changed from 1024 bpe to 600 bpe

--- examples/asr/librispeech_biasing/run_spm.sh
+++ examples/asr/librispeech_biasing/run_spm.sh
@@ -1,1 +1,1 @@
-python train_spm.py --librispeech-path /home/gs534/rds/hpc-work/data/Librispeech/ --output-file ./spm_unigram_1023_100suffix.model --suffix
+python train_spm.py --librispeech-path /home/gs534/rds/hpc-work/data/Librispeech/ --output-file ./spm_unigram_600_100suffix.model --suffix
