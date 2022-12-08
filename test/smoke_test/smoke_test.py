4c542cad72f633e95872b3eeae1f7543eacb7642
atalman <atalman@fb.com>
2022-12-08 13:09:45 -0800

    Address comments

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -11,19 +11,18 @@ import torchaudio.transforms  # noqa: F401
 import torchaudio.utils  # noqa: F401
 
 
-def streamreader_test():
+def ffmpeg_test():
     from torchaudio.io import StreamReader  # noqa: F401
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--streamreader", action="store_true")
-    parser.add_argument("--no-streamreader", dest="streamreader", action="store_false")
-    parser.set_defaults(streamreader=True)
+    parser.add_argument("--no-ffmpeg", dest="ffmpeg", action="store_false")
+    parser.set_defaults(ffmpeg=True)
 
     options = parser.parse_args()
-    if options.streamreader:
-        streamreader_test()
+    if options.ffmpeg:
+        ffmpeg_test()
 
 
 if __name__ == "__main__":
