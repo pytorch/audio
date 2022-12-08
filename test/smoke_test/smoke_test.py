c7d61d1a6f241176980d7ef3cf5f694408510d40
atalman <atalman@fb.com>
2022-12-08 12:44:39 -0800

    Toggle streamreader test if needed

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -9,4 +9,18 @@ import torchaudio.pipelines  # noqa: F401
 import torchaudio.sox_effects  # noqa: F401
 import torchaudio.transforms  # noqa: F401
 import torchaudio.utils  # noqa: F401
-from torchaudio.io import StreamReader  # noqa: F401
+
+def streamreader_test():
+    from torchaudio.io import StreamReader  # noqa: F401
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--streamreader', dest='streamreader', action='store_true')
+    parser.set_defaults(streamreader=True)
+
+    options = parser.parse_args()
+    if options.streamreader:
+        streamreader_test()
+
+if __name__ == "__main__":
+    main()
