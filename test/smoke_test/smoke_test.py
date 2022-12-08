aee587d91fff1a640d9299345ed99d2dba040f53
atalman <atalman@fb.com>
2022-12-08 12:55:38 -0800

    Fix lint

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -10,18 +10,21 @@ import torchaudio.sox_effects  # noqa: F401
 import torchaudio.transforms  # noqa: F401
 import torchaudio.utils  # noqa: F401
 
+
 def streamreader_test():
     from torchaudio.io import StreamReader  # noqa: F401
 
+
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--streamreader', action='store_true')
-    parser.add_argument('--no-streamreader', dest='streamreader', action='store_false')
+    parser.add_argument("--streamreader", action="store_true")
+    parser.add_argument("--no-streamreader", dest="streamreader", action="store_false")
     parser.set_defaults(streamreader=True)
 
     options = parser.parse_args()
     if options.streamreader:
         streamreader_test()
+
 
 if __name__ == "__main__":
     main()
