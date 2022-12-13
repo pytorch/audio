ad596b37ac7d5187e49b85f0d16d7c175c129748
DanilBaibak <danil.baibak@gmail.com>
2022-12-13 09:31:05 -0800

    Switch to nova Linux Wheel build (#2896)
    Summary:
    Switch to Nova Linux Wheel build.
    
    - [x] Disable the CircleCI builds for Linux Wheel.
    - [x] Enable the Nova workflow for Linux Wheel.
    
    ~The Linux Wheel Python3.8 build has been kept because it is a dependency for the `docstring_parameters_sync` job.~ As Omkar pointed out, Docstring Parameters Sync also runs on GHA (https://github.com/pytorch/audio/actions/runs/3638187635/jobs/6140090209). So, we completely switched to the Nova Linux Wheel build.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2896
    
    Reviewed By: mthrok
    
    Differential Revision: D41994993
    
    Pulled By: DanilBaibak
    
    fbshipit-source-id: 2bafdbe1b62ef8fa194ce96f4d4667a3ed3dc44f

--- .circleci/regenerate.py
+++ .circleci/regenerate.py
@@ -42,20 +42,22 @@ def build_workflows(prefix="", upload=False, filter_branch=None, indentation=6):
             for python_version in PYTHON_VERSIONS:
                 for cu_version in CU_VERSIONS_DICT[os_type]:
                     fb = filter_branch
-                    if cu_version.startswith("rocm") and btype == "conda":
+                    if (cu_version.startswith("rocm") and btype == "conda") or (
+                        os_type == "linux" and btype == "wheel"
+                    ):
                         continue
                     if not fb and (
                         os_type == "linux" and btype == "wheel" and python_version == "3.8" and cu_version == "cpu"
                     ):
                         # the fields must match the build_docs "requires" dependency
                         fb = "/.*/"
+
                     w += build_workflow_pair(btype, os_type, python_version, cu_version, fb, prefix, upload)
 
     if not filter_branch:
         # Build on every pull request, but upload only on nightly and tags
         w += build_doc_job("/.*/")
         w += upload_doc_job("nightly")
-        w += docstring_parameters_sync_job(None)
 
     return indent(indentation, w)
 
@@ -89,7 +91,6 @@ def build_workflow_pair(btype, os_type, python_version, cu_version, filter_branc
     w.append(generate_base_workflow(base_workflow_name, python_version, cu_version, filter_branch, os_type, btype))
 
     if upload:
-
         w.append(generate_upload_workflow(base_workflow_name, filter_branch, os_type, btype, cu_version))
 
     if os_type != "macos":
