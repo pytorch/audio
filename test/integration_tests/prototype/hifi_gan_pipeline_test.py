cbf6adc532972c64dc32edbd76abc871a45ca7bb
Grigory Sizov <grigorysizov@fb.com>
2023-01-02 14:46:27 +0100

    Use synthetic audio in HiFiGAN integration test

--- test/integration_tests/prototype/hifi_gan_pipeline_test.py
+++ test/integration_tests/prototype/hifi_gan_pipeline_test.py
@@ -1,29 +1,26 @@
-import pytest
+import math
 
 import torch
 import torchaudio
-import torchaudio.functional as F
+from torchaudio.prototype.functional import oscillator_bank
 from torchaudio.prototype.pipelines import HIFIGAN_VOCODER_V3_LJSPEECH
 
 
-@pytest.mark.parametrize(
-    "lang",
-    [
-        ("en"),
-        ("en2"),
-    ],
-)
-def test_hifi_gan_pretrained_weights(lang, sample_speech):
-    """Test that a waveform reconstructed from mel spectrogram by HiFiGAN bundle is close enough to the original"""
+def test_hifi_gan_pretrained_weights():
+    """Test that a waveform reconstructed from mel spectrogram by HiFiGAN bundle is close enough to the original.
+    The main transformations performed in this test can be represented as
+        - audio -> reference log mel spectrogram
+        - audio -> mel spectrogram -> audio -> estimated log mel spectrogram
+    In the end, we compare estimated log mel spectrogram to the reference one. See comments in code for details.
+    """
     bundle = HIFIGAN_VOCODER_V3_LJSPEECH
 
     # Get HiFiGAN-compatible transformation from waveform to mel spectrogram
     mel_transform = bundle.get_mel_transform()
     # Get HiFiGAN vocoder
     vocoder = bundle.get_vocoder()
-
-    ref_waveform, ref_sample_rate = torchaudio.load(sample_speech)
-    ref_waveform = F.resample(ref_waveform, orig_freq=ref_sample_rate, new_freq=bundle.sample_rate)
+    # Create a synthetic waveform
+    ref_waveform = get_sin_sweep(sample_rate=bundle.sample_rate, length=100000)
     ref_waveform = ref_waveform[:, : -(ref_waveform.shape[1] % mel_transform.hop_size)]
 
     # Generate mel spectrogram from waveform
@@ -44,7 +41,18 @@ def test_hifi_gan_pretrained_weights(lang, sample_speech):
     # Log mel spectrogram of the reference waveform
     ref_spectrogram = final_spec(ref_waveform)
     ref_spectrogram = torch.log(torch.clamp(ref_spectrogram, min=1e-5))
-    # Check that relative MSE is below 2%
+    # Check that relative MSE is below 4%
     mse = ((estimated_spectorogram - ref_spectrogram) ** 2).mean()
     mean_ref = ((ref_spectrogram) ** 2).mean()
-    assert mse / mean_ref < 0.02
+    print(mse / mean_ref)
+    assert mse / mean_ref < 0.04
+
+
+def get_sin_sweep(sample_rate, length):
+    """Create a waveform which changes frequency from 0 to the Nyquist frequency (half of the sample rate)"""
+    nyquist_freq = sample_rate / 2
+    freq = torch.logspace(0, math.log(0.99 * nyquist_freq, 10), length).unsqueeze(-1)
+    amp = torch.ones((length, 1))
+
+    waveform = oscillator_bank(freq, amp, sample_rate=sample_rate)
+    return waveform.unsqueeze(0)
--- test/torchaudio_unittest/prototype/hifi_gan/original/README.md
+++ test/torchaudio_unittest/prototype/hifi_gan/original/README.md
@@ -34,7 +34,6 @@ ported code has been are modified in a minimal way, namely:
  - Add `return_complex=False` argument to `torch.stft` call in `mel_spectrogram` in `meldataset.py`, to make code
 PyTorch 2.0 compatible
  - Remove the import statements required only for the removed functions.
- - Code was formatted to pass pre-commit checks using `pre-commit-hooks==4.0.1`, `ufmt==1.3.2`, `black==22.3`,
-  `usort==1.0.2`, `libcst==0.4.1`, and `flake8==4.0.1`.
+ - Code was formatted to pass pre-commit checks (see `.pre-commit-config.yaml` for configuration).
 
 Apart from the changes listed above, the implementation of the retained functions and classes is kept as-is.
