c9eb90de91a205be27c284687f5c5aac51fa88fd
pbialecki <piotr.bialecki@hotmail.de>
2023-01-03 11:50:14 -0800

    add CUDA 11.8 builds

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -28,7 +28,7 @@ else
     version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
 
     cuda_toolkit_pckg="cudatoolkit"
-    if [[ "$CU_VERSION" == cu116 || "$CU_VERSION" == cu117 ]]; then
+    if [[ "$CU_VERSION" == cu116 || "$CU_VERSION" == cu117 || "$CU_VERSION" == cu118 ]]; then
         cuda_toolkit_pckg="pytorch-cuda"
     fi
 
