fe5e62b44380612299dbdb7936a472e5b4b5dad5
moto <855818+mthrok@users.noreply.github.com>
2023-01-05 21:29:58 -0800

    Add utility functions to fetch available formats/devices/codecs/protocols. (#2958)
    Summary:
    This commit adds utility functions that fetch the available/supported formats/devices/codecs.
    
    These functions are mostly same with commands like `ffmpeg -decoders`. But the use of `ffmpeg` CLI can report different resutls if there are multiple installation of FFmpegs. Or, the CLI might not be available.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2958
    
    Reviewed By: hwangjeff
    
    Differential Revision: D42371640
    
    Pulled By: mthrok
    
    fbshipit-source-id: 55f711363dc141c1bd5c80c7033cef74c5136d0f

--- test/torchaudio_unittest/utils/ffmpeg_utils_test.py
+++ test/torchaudio_unittest/utils/ffmpeg_utils_test.py
@@ -25,3 +25,17 @@ class TestFFmpegUtils(PytorchTestCase):
         """`get_versions` does not crash"""
         versions = ffmpeg_utils.get_versions()
         assert set(versions.keys()) == {"libavutil", "libavcodec", "libavformat", "libavfilter", "libavdevice"}
+
+    def test_available_stuff(self):
+        """get_encoders|decoders|muxers|demuxers|devices function does not segfault"""
+
+        ffmpeg_utils.get_demuxers()
+        ffmpeg_utils.get_muxers()
+        ffmpeg_utils.get_audio_decoders()
+        ffmpeg_utils.get_audio_encoders()
+        ffmpeg_utils.get_video_decoders()
+        ffmpeg_utils.get_video_encoders()
+        ffmpeg_utils.get_input_devices()
+        ffmpeg_utils.get_output_devices()
+        ffmpeg_utils.get_input_protocols()
+        ffmpeg_utils.get_output_protocols()
