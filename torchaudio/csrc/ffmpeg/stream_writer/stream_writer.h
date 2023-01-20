f97df830291c13c08625623a3fe99278183633fd
moto <855818+mthrok@users.noreply.github.com>
2023-01-20 08:32:39 -0800

    Document StreamReader/Writer C++ code (#2997)
    Summary:
    Extraction from https://github.com/pytorch/audio/issues/2994
    
    Add docstrings to C++ StreamReader/Writer.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/2997
    
    Reviewed By: nateanl
    
    Differential Revision: D42628016
    
    Pulled By: mthrok
    
    fbshipit-source-id: c2fe25802c1a3b5c07e253db8fc104e652e0add3

--- torchaudio/csrc/ffmpeg/stream_writer/stream_writer.h
+++ torchaudio/csrc/ffmpeg/stream_writer/stream_writer.h
@@ -22,6 +22,9 @@ struct OutputStream {
   AVBufferRefPtr hw_frame_ctx;
 };
 
+///
+/// Encode and write audio/video streams chunk by chunk
+///
 class StreamWriter {
   AVFormatOutputContextPtr pFormatContext;
   AVBufferRefPtr pHWBufferRef;
@@ -38,13 +41,48 @@ class StreamWriter {
   // Query methods
   //////////////////////////////////////////////////////////////////////////////
  public:
-  // Print the configured outputs
+  /// @internal
+
+  /// Print the configured outputs
   void dump_format(int64_t i);
+
+  /// @endinternal
 
   //////////////////////////////////////////////////////////////////////////////
   // Configure methods
   //////////////////////////////////////////////////////////////////////////////
  public:
+  /// Add an output audio stream.
+  ///
+  /// @param sample_rate The sample rate.
+  /// @param num_channels The number of channels.
+  /// @param format Input sample format, which determines the dtype
+  /// of the input tensor.
+  /// @parblock
+  ///
+  /// - ``"u8"``: The input tensor must be ``torch.uint8`` type.
+  /// - ``"s16"``: The input tensor must be ``torch.int16`` type.
+  /// - ``"s32"``: The input tensor must be ``torch.int32`` type.
+  /// - ``"s64"``: The input tensor must be ``torch.int64`` type.
+  /// - ``"flt"``: The input tensor must be ``torch.float32`` type.
+  /// - ``"dbl"``: The input tensor must be ``torch.float64`` type.
+  ///
+  /// Default: ``"flt"``.
+  /// @endparblock
+  /// @param encoder The name of the encoder to be used.
+  /// @parblock
+  /// When provided, use the specified encoder instead of the default one.
+  ///
+  /// To list the available encoders, you can use ``ffmpeg -encoders`` command.
+  /// @endparblock
+  /// @param encoder_option Options passed to encoder.
+  /// To list encoder options for a encoder, you can use
+  /// ``ffmpeg -h encoder=<ENCODER>``.
+  /// @param encoder_format Format used to encode media.
+  /// When encoder supports multiple formats, passing this argument will
+  /// override the format used for encoding.
+  ///  To list supported formats for the encoder, you can use
+  /// ``ffmpeg -h encoder=<ENCODER>`` command.
   void add_audio_stream(
       int64_t sample_rate,
       int64_t num_channels,
@@ -52,6 +90,35 @@ class StreamWriter {
       const c10::optional<std::string>& encoder,
       const c10::optional<OptionDict>& encoder_option,
       const c10::optional<std::string>& encoder_format);
+  /// Add an output video stream.
+  ///
+  /// @param frame_rate Frame rate
+  /// @param width Width
+  /// @param height Height
+  /// @param format Input pixel format, which determines the
+  /// color channel order of the input tensor.
+  /// @parblock
+  ///
+  /// - ``"gray8"``: One channel, grayscale.
+  /// - ``"rgb24"``: Three channels in the order of RGB.
+  /// - ``"bgr24"``: Three channels in the order of BGR.
+  /// - ``"yuv444p"``: Three channels in the order of YUV.
+  ///
+  /// In either case, the input tensor has to be ``torch.uint8`` type and
+  /// the shape must be (frame, channel, height, width).
+  /// @endparblock
+  /// @param encoder See ``add_audio_stream()``.
+  /// @param encoder_option See ``add_audio_stream()``.
+  /// @param encoder_format See ``add_audio_stream()``.
+  /// @param hw_accel Enable hardware acceleration.
+  /// @parblock
+  /// When video is encoded on CUDA hardware, for example
+  /// `encoder="h264_nvenc"`, passing CUDA device indicator to `hw_accel`
+  /// (i.e. `hw_accel="cuda:0"`) will make StreamWriter expect video
+  /// chunk to be a CUDA Tensor. Passing CPU Tensor will result in an error.
+  ///
+  /// If `None`, the video chunk Tensor has to be a CPU Tensor.
+  /// @endparblock
   void add_video_stream(
       double frame_rate,
       int64_t width,
@@ -61,6 +128,8 @@ class StreamWriter {
       const c10::optional<OptionDict>& encoder_option,
       const c10::optional<std::string>& encoder_format,
       const c10::optional<std::string>& hw_accel);
+  /// Set file-level metadata
+  /// @param metadata metadata.
   void set_metadata(const OptionDict& metadata);
 
  private:
@@ -70,11 +139,26 @@ class StreamWriter {
   // Write methods
   //////////////////////////////////////////////////////////////////////////////
  public:
+  /// Open the output file / device and write the header.
+  ///
+  /// @param opt Private options for protocol, device and muxer.
   void open(const c10::optional<OptionDict>& opt);
+  /// Close the output file / device and finalize metadata.
   void close();
 
+  /// Write audio data
+  /// @param i Stream index.
+  /// @param chunk Waveform tensor. Shape: ``(frame, channel)``.
+  /// The ``dtype`` must match what was passed to ``add_audio_stream()`` method.
   void write_audio_chunk(int i, const torch::Tensor& chunk);
+  /// Write video data
+  /// @param i Stream index.
+  /// @param chunk Video/image tensor. Shape: ``(time, channel, height,
+  /// width)``. The ``dtype`` must be ``torch.uint8``. The shape ``(height,
+  /// width and the number of channels)`` must match what was configured when
+  /// calling ``add_video_stream()``.
   void write_video_chunk(int i, const torch::Tensor& chunk);
+  /// Flush the frames from encoders and write the frames to the destination.
   void flush();
 
  private:
