34f5cd8ee325497d37cff1223feeea2427bf93bc
G. Sun <gs534@gpu-q-70.data.cluster>
2023-01-22 09:28:10 +0000

    temporarily commit for debugging

--- examples/asr/librispeech_conformer_rnnt_biasing/train.sh
+++ examples/asr/librispeech_conformer_rnnt_biasing/train.sh
@@ -1,3 +1,5 @@
+. /home/gs534/rds/hpc-work/work/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate cuda113
+export LD_PRELOAD=/lib64/libgsm.so
 expdir="./experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop"
 mkdir -p $expdir
 python train.py \
@@ -10,4 +12,4 @@ python train.py \
     --droprate 0.0 \
     --maxsize 500 \
     --epochs 90 \
-    --resume experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop/checkpoints/epoch=45-step=89838.ckpt \
+    # --resume experiments/librispeech_clean100_suffix600_tcpgen500_sche30_nodrop/checkpoints/epoch=45-step=89838.ckpt \
