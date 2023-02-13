28b2abcbd674d7da985ceba5fd78b45b68c3aa46
G. Sun <gs534@login-e-3.data.cluster>
2023-02-13 22:00:57 +0000

    formatted files

--- torchaudio/prototype/models/__init__.py
+++ torchaudio/prototype/models/__init__.py
@@ -8,9 +8,9 @@ from ._conformer_wav2vec2 import (
 )
 from ._emformer_hubert import emformer_hubert_base, emformer_hubert_model
 from .conv_emformer import ConvEmformer
+from .hifi_gan import hifigan_vocoder, hifigan_vocoder_v1, hifigan_vocoder_v2, hifigan_vocoder_v3, HiFiGANVocoder
 from .rnnt import conformer_rnnt_base, conformer_rnnt_biasing, conformer_rnnt_biasing_base, conformer_rnnt_model
 from .rnnt_decoder import Hypothesis, RNNTBeamSearchBiasing
-from .hifi_gan import hifigan_vocoder, hifigan_vocoder_v1, hifigan_vocoder_v2, hifigan_vocoder_v3, HiFiGANVocoder
 
 __all__ = [
     "conformer_rnnt_base",
