90573693f0ab95afec6acaf7105f46c256aae377
hwangjeff <jeffhwang@meta.com>
2023-02-16 14:43:26 -0800

    Add guards to prevent ffmpeg failures during dispatcher import (#3073)
    Summary:
    With the introduction of the backend dispatcher, importing torchaudio fails when ffmpeg is not available. This PR adds guards to resolve these failures.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/3073
    
    Reviewed By: NivekT, mthrok
    
    Differential Revision: D43372870
    
    Pulled By: hwangjeff
    
    fbshipit-source-id: c1b97a4d0597f5bf339a780ce43fda02b1285361

--- torchaudio/backend/__init__.py
+++ torchaudio/backend/__init__.py
@@ -1,12 +1,12 @@
 # flake8: noqa
 import torchaudio
 
-from torchaudio._backend.utils import get_info_func, get_load_func, get_save_func
-
 from . import utils
 from .utils import _is_backend_dispatcher_enabled, get_audio_backend, list_audio_backends, set_audio_backend
 
 if _is_backend_dispatcher_enabled():
+    from torchaudio._backend.utils import get_info_func, get_load_func, get_save_func
+
     torchaudio.info = get_info_func()
     torchaudio.load = get_load_func()
     torchaudio.save = get_save_func()
