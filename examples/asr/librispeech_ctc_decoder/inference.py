e1c27ce583f3d10353f97406c1711db9419e2f90
moto <855818+mthrok@users.noreply.github.com>
2023-02-17 22:25:38 -0500

    wip

--- examples/asr/librispeech_ctc_decoder/inference.py
+++ examples/asr/librispeech_ctc_decoder/inference.py
@@ -1,19 +1,44 @@
-import argparse
+#!/usr/bin/env python3
+"""Evaluate ASR model with LibriSpeech dataset"""
+
 import logging
-from typing import Optional
-
-import torch
-import torchaudio
-from torchaudio.models.decoder import ctc_decoder, download_pretrained_files
-
 
 logger = logging.getLogger(__name__)
 
 
+def _get_model(model):
+    import torchaudio
+
+    bundle = getattr(torchaudio.pipelines, model)
+    return bundle.get_model()
+
+
+def _get_model_espnet(model):
+    import torch
+    from espnet2.bin.asr_inference import Speech2Text
+
+    class Wrapper(torch.nn.Module):
+        def __init__(self, model):
+            super().__init__()
+            self.model = model
+
+        def forward(self, speech):
+            length = speech.new_full([1], dtype=torch.long, fill_value=speech.size(1))
+            return self.model.asr_model.encode(speech=speech, speech_lengths=length)
+
+    model = Speech2Text.from_pretrained(model)
+    return Wrapper(model)
+
+
 def run_inference(args):
-    # get pretrained wav2vec2.0 model
-    bundle = getattr(torchaudio.pipelines, args.model)
-    model = bundle.get_model()
+    import torch
+    import torchaudio
+    from torchaudio.models.decoder import download_pretrained_files, ctc_decoder
+
+    if args.model_source == "torchaudio":
+        model = _get_model(args.model)
+    else:
+        model = _get_model_espnet(args.model)
 
     # get decoder files
     files = download_pretrained_files("librispeech-4-gram")
@@ -54,39 +79,63 @@ def run_inference(args):
 
 
 def _parse_args():
+    import argparse
+    from argparse import RawTextHelpFormatter, ArgumentDefaultsHelpFormatter
+
+    class Formatter(RawTextHelpFormatter, ArgumentDefaultsHelpFormatter):
+        pass
+
+    # fmt: off
     parser = argparse.ArgumentParser(
         description=__doc__,
-        formatter_class=argparse.RawTextHelpFormatter,
+        formatter_class=Formatter,
     )
     parser.add_argument(
-        "--librispeech_path",
-        type=str,
+        "--librispeech-path",
+        required=True,
         help="folder where LibriSpeech is stored",
     )
     parser.add_argument(
         "--split",
-        type=str,
         help="LibriSpeech dataset split",
         choices=["dev-clean", "dev-other", "test-clean", "test-other"],
         default="test-other",
     )
     parser.add_argument(
         "--model",
-        type=str,
         default="WAV2VEC2_ASR_BASE_960H",
         help="pretrained Wav2Vec2 model from torchaudio.pipelines",
     )
-    parser.add_argument("--nbest", type=int, default=1, help="number of best hypotheses to return")
     parser.add_argument(
-        "--beam-size", type=int, default=500, help="beam size for determining number of hypotheses to store"
+        "--model-source",
+        default="torchaudio",
+        choices=["torchaudio", "espnet2"],
+        help="project in which the model is defined",
+    )
+    parser.add_argument(
+        "--nbest",
+        type=int,
+        default=1,
+        help="number of best hypotheses to return",
+    )
+    parser.add_argument(
+        "--beam-size",
+        type=int,
+        default=500,
+        help="beam size for determining number of hypotheses to store",
     )
     parser.add_argument(
         "--beam-size-token",
-        type=Optional[int],
+        type=int,
         default=None,
         help="number of tokens to consider at each beam search step",
     )
-    parser.add_argument("--beam-threshold", type=int, default=50, help="beam threshold for pruning hypotheses")
+    parser.add_argument(
+        "--beam-threshold",
+        type=int,
+        default=50,
+        help="beam threshold for pruning hypotheses",
+    )
     parser.add_argument(
         "--lm-weight",
         type=float,
@@ -99,9 +148,24 @@ def _parse_args():
         default=0.52,
         help="word insertion score",
     )
-    parser.add_argument("--unk_score", type=float, default=float("-inf"), help="unknown word insertion score")
-    parser.add_argument("--sil_score", type=float, default=0, help="silence insertion score")
-    parser.add_argument("--debug", action="store_true", help="whether to use debug level for logging")
+    parser.add_argument(
+        "--unk-score",
+        type=float,
+        default=float("-inf"),
+        help="unknown word insertion score",
+    )
+    parser.add_argument(
+        "--sil-score",
+        type=float,
+        default=0,
+        help="silence insertion score",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="whether to use debug level for logging",
+    )
+    # fmt: on
     return parser.parse_args()
 
 
