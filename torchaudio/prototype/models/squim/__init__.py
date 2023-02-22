9238a86b4dc688d9be606605eb3af275f5edad73
Zhaoheng Ni <zni@meta.com>
2023-02-22 15:08:40 -0500

    fix lint

--- torchaudio/prototype/models/squim/__init__.py
+++ torchaudio/prototype/models/squim/__init__.py
@@ -1,4 +1,4 @@
-from .objective import SquimObjective, squim_objective_base, squim_objective_model
+from .objective import squim_objective_base, squim_objective_model, SquimObjective
 
 __all__ = [
     "squim_objective_base",
