e0f8cc02dfdfa9295e51fe201fcca0e39b9c0794
Zhaoheng Ni <zni@meta.com>
2023-02-24 12:53:40 -0500

    remove prototype features

--- examples/asr/emformer_rnnt/pipeline_demo.py
+++ examples/asr/emformer_rnnt/pipeline_demo.py
@@ -16,7 +16,6 @@ import torchaudio
 from common import MODEL_TYPE_LIBRISPEECH, MODEL_TYPE_MUSTC, MODEL_TYPE_TEDLIUM3
 from mustc.dataset import MUSTC
 from torchaudio.pipelines import EMFORMER_RNNT_BASE_LIBRISPEECH, RNNTBundle
-from torchaudio.prototype.pipelines import EMFORMER_RNNT_BASE_MUSTC, EMFORMER_RNNT_BASE_TEDLIUM3
 
 logger = logging.getLogger(__name__)
 
--- torchaudio/prototype/transforms/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._transforms import BarkScale, BarkSpectrogram, InverseBarkScale
-
-__all__ = [
-    "BarkScale",
-    "BarkSpectrogram",
-    "InverseBarkScale",
-]
