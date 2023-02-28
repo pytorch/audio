fd24af003cd4bfcfaee9acfd2245ede4985c54b4
Moto Hira <moto@meta.com>
2023-02-27 18:35:07 -0800

    Use null filter in case no filter is used (#3109)
    Summary:
    Pull Request resolved: https://github.com/pytorch/audio/pull/3109
    
    Change the logic around StreamWriter preprocessing.
    Currently, no preprocessing is expressed as `nullptr` to `unique_ptr<FilterGraph>`.
    
    This commit changes it to `[a]null` filter, which is just a pass through.
    This makes a code a bit simpler, and serves better preparation for adding
    filters for CUDA process.
    
    Reviewed By: xiaohui-zhang
    
    Differential Revision: D43593321
    
    fbshipit-source-id: 9ca71c2c8bf652384a0f56b4c41b32d908f61201

--- torchaudio/csrc/ffmpeg/stream_writer/output_stream.cpp
+++ torchaudio/csrc/ffmpeg/stream_writer/output_stream.cpp
@@ -5,7 +5,7 @@ namespace torchaudio::io {
 OutputStream::OutputStream(
     AVFormatContext* format_ctx,
     AVCodecContext* codec_ctx_,
-    std::unique_ptr<FilterGraph>&& filter_)
+    FilterGraph&& filter_)
     : codec_ctx(codec_ctx_),
       encoder(format_ctx, codec_ctx),
       filter(std::move(filter_)),
@@ -13,13 +13,9 @@ OutputStream::OutputStream(
       num_frames(0) {}
 
 void OutputStream::process_frame(AVFrame* src) {
-  if (!filter) {
-    encoder.encode(src);
-    return;
-  }
-  int ret = filter->add_frame(src);
+  int ret = filter.add_frame(src);
   while (ret >= 0) {
-    ret = filter->get_frame(dst_frame);
+    ret = filter.get_frame(dst_frame);
     if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
       if (ret == AVERROR_EOF) {
         encoder.encode(nullptr);
