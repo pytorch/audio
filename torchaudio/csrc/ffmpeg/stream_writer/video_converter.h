fd883198c3ad4757417223a959a2fc9882080b79
mthrok <855818+mthrok@users.noreply.github.com>
2023-03-01 05:10:09 -0800

    Extract image conversions into separate class (#3120)
    Summary:
    Pull Request resolved: https://github.com/pytorch/audio/pull/3120
    
    This commits extract image conversion ops into ImageTensorConverter class, and make it independent from OutputStream class.
    
    ImageTensorConverter class implementes range-based for-loop interface, like
    
    ```
    for (auto const& frame : ImageTensorConverter::convert(...)) {
        post_process_with_avframe(frame);
    }
    ```
    
    This allows to decouple encoder from image conversion.
    
    Reviewed By: nateanl
    
    Differential Revision: D43666296
    
    fbshipit-source-id: e953db5688c51a1d8592d4e21730eadc766ec79b

---
+++ torchaudio/csrc/ffmpeg/stream_writer/video_converter.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <torchaudio/csrc/ffmpeg/stream_writer/converter.h>
+
+namespace torchaudio::io {
+
+////////////////////////////////////////////////////////////////////////////////
+// VideoTensorConverter
+////////////////////////////////////////////////////////////////////////////////
+// VideoTensorConverter is responsible for picking up the right set of
+// conversion process (InitFunc and ConvertFunc) based on the input pixel format
+// information, and own them.
+class VideoTensorConverter {
+ public:
+  // Initialization is one-time process applied to frames before the iteration
+  // starts. i.e. either convert to NHWC.
+  using InitFunc = std::function<torch::Tensor(const torch::Tensor&)>;
+
+ private:
+  enum AVPixelFormat src_fmt;
+  AVCodecContext* codec_ctx;
+  AVFramePtr buffer;
+
+  InitFunc init_func{};
+  Generator::ConvertFunc convert_func{};
+
+ public:
+  VideoTensorConverter(enum AVPixelFormat src_fmt, AVCodecContext* codec_ctx);
+  Generator convert(const torch::Tensor& frames);
+};
+} // namespace torchaudio::io
