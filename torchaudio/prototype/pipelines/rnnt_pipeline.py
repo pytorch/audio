ff82d7de862e2e6ca35d6a5b0747c44dbbf45bfa
moto <855818+mthrok@users.noreply.github.com>
2023-03-21 13:23:58 -0400

    Update prototype comformer doc and reduce line length to <120

--- torchaudio/prototype/pipelines/rnnt_pipeline.py
+++ torchaudio/prototype/pipelines/rnnt_pipeline.py
@@ -18,12 +18,17 @@ EMFORMER_RNNT_BASE_MUSTC = RNNTBundle(
     _segment_length=16,
     _right_context_length=4,
 )
-EMFORMER_RNNT_BASE_MUSTC.__doc__ = """Pre-trained Emformer-RNNT-based ASR pipeline capable of performing both streaming and non-streaming inference.
-    The underlying model is constructed by :py:func:`torchaudio.models.emformer_rnnt_base` and utilizes weights
-    trained on *MuST-C release v2.0* :cite:`CATTONI2021101155` dataset using training script ``train.py``
-    `here <https://github.com/pytorch/audio/tree/main/examples/asr/emformer_rnnt>`__ with ``num_symbols=501``.
-    Please refer to :py:class:`torchaudio.pipelines.RNNTBundle` for usage instructions.
-    """
+EMFORMER_RNNT_BASE_MUSTC.__doc__ = """Pre-trained Emformer-RNNT-based ASR pipeline capable of performing both
+streaming and non-streaming inference.
+
+The underlying model is constructed by :py:func:`torchaudio.models.emformer_rnnt_base`
+and utilizes weights trained on *MuST-C release v2.0* :cite:`CATTONI2021101155` dataset
+using training script ``train.py``
+`here <https://github.com/pytorch/audio/tree/main/examples/asr/emformer_rnnt>`__
+with ``num_symbols=501``.
+
+Please refer to :py:class:`torchaudio.pipelines.RNNTBundle` for usage instructions.
+"""
 
 
 EMFORMER_RNNT_BASE_TEDLIUM3 = RNNTBundle(
@@ -40,11 +45,14 @@ EMFORMER_RNNT_BASE_TEDLIUM3 = RNNTBundle(
     _segment_length=16,
     _right_context_length=4,
 )
-EMFORMER_RNNT_BASE_TEDLIUM3.__doc__ = """Pre-trained Emformer-RNNT-based ASR pipeline capable of performing both streaming and non-streaming inference.
+EMFORMER_RNNT_BASE_TEDLIUM3.__doc__ = """Pre-trained Emformer-RNNT-based ASR pipeline capable of performing both
+streaming and non-streaming inference.
 
-    The underlying model is constructed by :py:func:`torchaudio.models.emformer_rnnt_base`
-    and utilizes weights trained on TED-LIUM Release 3 dataset using training script ``train.py``
-    `here <https://github.com/pytorch/audio/tree/main/examples/asr/emformer_rnnt>`__ with ``num_symbols=501``.
+The underlying model is constructed by :py:func:`torchaudio.models.emformer_rnnt_base`
+and utilizes weights trained on *TED-LIUM Release 3* :cite:`rousseau2012tedlium` dataset
+using training script ``train.py``
+`here <https://github.com/pytorch/audio/tree/main/examples/asr/emformer_rnnt>`__
+with ``num_symbols=501``.
 
-    Please refer to :py:class:`torchaudio.pipelines.RNNTBundle` for usage instructions.
-    """
+Please refer to :py:class:`torchaudio.pipelines.RNNTBundle` for usage instructions.
+"""
