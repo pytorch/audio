c2f8b02208ad3fda9bb2bfafdf12d626a40dea6a
atalman <atalman@fb.com>
2023-03-21 17:35:25 -0700

    Simplify logic

--- packaging/ffmpeg/build.bat
+++ packaging/ffmpeg/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist "third_party\ffmpeg\" goto end
-if defined CIRCLECI (set PROJ_FOLDER="/c/Users/circleci/project") else (set PROJ_FOLDER=%cd%)
+set PROJ_FOLDER=%cd%
 
 choco install -y --no-progress msys2 --package-parameters "/NoUpdate"
 C:\tools\msys64\usr\bin\env MSYSTEM=MINGW64 /bin/bash -l -c "pacman -S --noconfirm --needed base-devel mingw-w64-x86_64-toolchain diffutils"
