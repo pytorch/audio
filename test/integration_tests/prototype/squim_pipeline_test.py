52ea6c49f84372ece44b1e9f585eed6a9756142a
Zhaoheng Ni <zni@meta.com>
2023-03-22 17:07:19 -0400

    add SquimSubjective pipeline

--- test/integration_tests/prototype/squim_pipeline_test.py
+++ test/integration_tests/prototype/squim_pipeline_test.py
@@ -1,6 +1,6 @@
 import pytest
 import torchaudio
-from torchaudio.prototype.pipelines import SQUIM_OBJECTIVE
+from torchaudio.prototype.pipelines import SQUIM_OBJECTIVE, SQUIM_SUBJECTIVE
 
 
 @pytest.mark.parametrize(
@@ -20,3 +20,24 @@ def test_squim_objective_pretrained_weights(lang, expected, sample_speech):
     scores = model(waveform)
     for i in range(3):
         assert abs(scores[i].item() - expected[i]) < 1e-5
+
+
+@pytest.mark.parametrize(
+    "task,expected",
+    [
+        ("speech_separation", [3.9257140159606934, 3.9391300678253174]),
+    ],
+)
+def test_squim_subjective_pretrained_weights(task, expected, mixture_source, clean_sources):
+    """Test that the metric scores estimated by SquimSubjective Bundle is identical to the expected result."""
+    bundle = SQUIM_SUBJECTIVE
+
+    # Get SquimObjective model
+    model = bundle.get_model()
+    # Load input mixture audio
+    waveform, sample_rate = torchaudio.load(mixture_source)
+    for i, source in enumerate(clean_sources):
+        # Load clean reference
+        clean_waveform, sample_rate = torchaudio.load(source)
+        score = model(waveform, clean_waveform)
+        assert abs(score.item() - expected[i]) < 1e-5
