b374846df9bf5d13cf6a12da4434e35fedb487ca
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared9932.03.pnb6.facebook.com>
2023-04-06 04:30:44 -0700

    2023-04-06 nightly release (f4d94cab0cc0af0c89565084998ecf2d6ba8a38a)

--- tools/setup_helpers/extension.py
+++ tools/setup_helpers/extension.py
@@ -37,7 +37,6 @@ _BUILD_SOX = False if platform.system() == "Windows" else _get_build("BUILD_SOX"
 _BUILD_KALDI = False if platform.system() == "Windows" else _get_build("BUILD_KALDI", True)
 _BUILD_RIR = _get_build("BUILD_RIR", True)
 _BUILD_RNNT = _get_build("BUILD_RNNT", True)
-_BUILD_CTC_DECODER = _get_build("BUILD_CTC_DECODER", False)
 _USE_FFMPEG = _get_build("USE_FFMPEG", False)
 _USE_ROCM = _get_build("USE_ROCM", torch.backends.cuda.is_built() and torch.version.hip is not None)
 _USE_CUDA = _get_build("USE_CUDA", torch.backends.cuda.is_built() and torch.version.hip is None)
@@ -55,14 +54,6 @@ def get_ext_modules():
             [
                 Extension(name="torchaudio.lib.libtorchaudio_sox", sources=[]),
                 Extension(name="torchaudio.lib._torchaudio_sox", sources=[]),
-            ]
-        )
-    if _BUILD_CTC_DECODER:
-        modules.extend(
-            [
-                Extension(name="torchaudio.lib.libflashlight-text", sources=[]),
-                Extension(name="torchaudio.lib.flashlight_lib_text_decoder", sources=[]),
-                Extension(name="torchaudio.lib.flashlight_lib_text_dictionary", sources=[]),
             ]
         )
     if _USE_FFMPEG:
@@ -119,7 +110,6 @@ class CMakeBuild(build_ext):
             f"-DBUILD_KALDI:BOOL={'ON' if _BUILD_KALDI else 'OFF'}",
             f"-DBUILD_RIR:BOOL={'ON' if _BUILD_RIR else 'OFF'}",
             f"-DBUILD_RNNT:BOOL={'ON' if _BUILD_RNNT else 'OFF'}",
-            f"-DBUILD_CTC_DECODER:BOOL={'ON' if _BUILD_CTC_DECODER else 'OFF'}",
             "-DBUILD_TORCHAUDIO_PYTHON_EXTENSION:BOOL=ON",
             f"-DUSE_ROCM:BOOL={'ON' if _USE_ROCM else 'OFF'}",
             f"-DUSE_CUDA:BOOL={'ON' if _USE_CUDA else 'OFF'}",
