ff96b303eb7bd7cef892184e6f567383f36eee51
Yuekai Zhang <zhangyuekai@foxmail.com>
2023-04-26 13:31:55 +0800

    fix typo

--- torchaudio/csrc/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
+++ torchaudio/csrc/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
@@ -612,8 +612,8 @@ int CTC_prob_first_step_V2(
   const int grid = bs;
 
   constexpr int Capacity = 16;
-  using FunType = decltype(bitonic_topk_multi_block_per_batch_kernel<
-                           threads_per_block0,
+  using FunType = decltype(first_matrix__bitonic_topk_kernel<
+                           threads_per_block,
                            Capacity>);
   static FunType* FirstMatrixFuns[5]{
       first_matrix__bitonic_topk_kernel<threads_per_block, 8>,
