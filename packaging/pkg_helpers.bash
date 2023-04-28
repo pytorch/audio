02294973916d5686e307a0a466052713ed73b377
pbialecki <piotr.bialecki@hotmail.de>
2023-04-28 13:58:07 -0700

    add CUDA 12.1 builds

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -46,13 +46,20 @@ setup_cuda() {
 
   # Now work out the CUDA settings
   case "$CU_VERSION" in
+    cu121)
+      if [[ "$OSTYPE" == "msys" ]]; then
+        export CUDA_HOME="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v12.1"
+      else
+        export CUDA_HOME=/usr/local/cuda-12.1/
+      fi
+      export TORCH_CUDA_ARCH_LIST="5.0+PTX;6.0;7.0;7.5;8.0;8.6;9.0"
     cu118)
       if [[ "$OSTYPE" == "msys" ]]; then
         export CUDA_HOME="C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8"
       else
         export CUDA_HOME=/usr/local/cuda-11.8/
       fi
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+      export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6;9.0"
       ;;
     cu117)
       if [[ "$OSTYPE" == "msys" ]]; then
@@ -246,6 +253,9 @@ setup_conda_cudatoolkit_constraint() {
     export CONDA_BUILD_VARIANT="cpu"
   else
     case "$CU_VERSION" in
+      cu121)
+        export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=12.1 # [not osx]"
+        ;;
       cu118)
         export CONDA_CUDATOOLKIT_CONSTRAINT="- pytorch-cuda=11.8 # [not osx]"
         ;;
