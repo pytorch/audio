598704b78a95a49ae1841be6e83f59dbd83fadde
Zhaoheng Ni <zni@meta.com>
2023-05-22 11:41:30 -0400

    fix

--- torchaudio/functional/functional.py
+++ torchaudio/functional/functional.py
@@ -2616,12 +2616,12 @@ def forced_align(
 
     Args:
         log_probs (torch.Tensor): log probability of CTC emission output.
-            Tensor with dimensions `(T, C)`. where `T` is the input length,
-            vocabulary is the number of characters in alphabet including blank.
-        targets (torch.Tensor): Target sequence. Tensor with dimension `(L,)`,
+            Tensor of shape `(T, C)`. where `T` is the input length,
+            `C` is the number of characters in alphabet including blank.
+        targets (torch.Tensor): Target sequence. Tensor of shape `(L,)`,
             where `L` is the target length.
-        input_lengths (torch.Tensor): Lengths of the inputs (max value must each be <= `T`). Tensor with dimension `()`.
-        target_lengths (torch.Tensor): Lengths of the targets. Tensor with dimension `()`.
+        input_lengths (torch.Tensor): Lengths of the inputs (max value must each be <= `T`). 0-D Tensor (scalar).
+        target_lengths (torch.Tensor): Lengths of the targets. 0-D Tensor (scalar).
         blank_id (int, optional): The index of blank symbol in CTC emission. (Default: 0)
 
     Returns:
@@ -2632,7 +2632,8 @@ def forced_align(
 
     Note:
         The sequence length of `log_probs` must satisfy:
-        
+
+
         .. math::
             L_{\\text{log\_probs}} \ge L_{\\text{label}} + N_{\\text{repeat}}
 
