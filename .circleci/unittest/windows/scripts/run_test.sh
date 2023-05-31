32e58b21959c637a16798ed0efb8ceec8f2ccd0e
moto <855818+mthrok@users.noreply.github.com>
2023-05-31 14:17:03 -0400

    Surface test failures on CI
    Set the directory of JUnitText XML file to the one where test-infra picks up
    and put them in summary.
    
    Example: https://github.com/pytorch/audio/actions/runs/5136305988

--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -12,5 +12,5 @@ python -m torch.utils.collect_env
 env | grep TORCHAUDIO || true
 
 cd test
-pytest --cov=torchaudio --junitxml=../test-results/junit.xml -v --durations 20 torchaudio_unittest
+pytest --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
 coverage html
