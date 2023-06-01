cc89f74328494b40c81f026bae671c59fa5934c5
Omkar Salpekar <osalpekar@fb.com>
2023-06-01 11:57:42 -0700

    [Nova] Deleting Remaining CircleCI jobs (#3399)
    Summary:
    This job completely deletes the CircleCI `config.yml`. Here is what was remaining in the config at the point of deletion:
    
    Used Jobs:
    * **Lint** - Now running on Nova - see https://github.com/pytorch/audio/actions/runs/5144082942 for an example run on the latest PR in trunk
    * **CircleCI Consistency** - Not needed anymore now if there is no CCI config.
    
    Unused Jobs:
    * **build-ffmpeg-$OS** - For the build jobs, we are already building FFMPEG from source as part of the Nova workflows.
    * **download-third-parties** - This is caching. We currently do not have caching in Nova jobs, but atalman is working on adding support for this as a future optimization.
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/3399
    
    Reviewed By: mthrok
    
    Differential Revision: D46363921
    
    Pulled By: osalpekar
    
    fbshipit-source-id: 8abf5b0c1612c3492908fb2f5797e6b0a3c70766

--- .circleci/smoke_test/docker/build_and_push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-datestr="$(date "+%Y%m%d")"
-image="pytorch/torchaudio_unittest_base:smoke_test-${datestr}"
-docker build -t "${image}" .
-docker push "${image}"
