da212020133cb7c14f4ed6400e757163d6e36195
moto <855818+mthrok@users.noreply.github.com>
2023-07-26 12:02:13 -0700

    Move env util (#3499)
    Summary: Pull Request resolved: https://github.com/pytorch/audio/pull/3499
    
    Differential Revision: D47803654
    
    Pulled By: mthrok
    
    fbshipit-source-id: 2b916fa66d84c91c01b4dfe6dd5ee3501159f451

--- torchaudio/_internal/module_utils.py
+++ torchaudio/_internal/module_utils.py
@@ -1,7 +1,27 @@
 import importlib.util
+import os
 import warnings
 from functools import wraps
 from typing import Optional
+
+
+def eval_env(var, default):
+    """Check if environment varable has True-y value"""
+    if var not in os.environ:
+        return default
+
+    val = os.environ.get(var, "0")
+    trues = ["1", "true", "TRUE", "on", "ON", "yes", "YES"]
+    falses = ["0", "false", "FALSE", "off", "OFF", "no", "NO"]
+    if val in trues:
+        return True
+    if val not in falses:
+        # fmt: off
+        raise RuntimeError(
+            f"Unexpected environment variable value `{var}={val}`. "
+            f"Expected one of {trues + falses}")
+        # fmt: on
+    return False
 
 
 def is_module_available(*modules: str) -> bool:
