06c6e4811e5d269d99c70252b3de9e662678f0a1
moto <855818+mthrok@users.noreply.github.com>
2023-09-04 18:56:43 -0400

    Fix backward compatibility layer in backend module
    The PR #3549 re-organized the backend implementations and deprecated
    the direct access to torchaudio.backend.
    
    The change was supposed to be BC-compatible while issuing a warning
    to users, but the implementation of module-level `__getattr__` was
    not quite right.
    
    See an issue https://github.com/pyannote/pyannote-audio/pull/1456.
    
    This commit fixes it so that the following imports work;
    
    ```python
    from torchaudio.backend.common import AudioMetaData
    
    from torchaudio.backend import sox_io_backend
    from torchaudio.backend.sox_io_backend import save, load, info
    
    from torchaudio.backend import no_backend
    from torchaudio.backend.no_backend import save, load, info
    
    from torchaudio.backend import soundfile_backend
    from torchaudio.backend.soundfile_backend import save, load, info
    ```

--- torchaudio/backend/__init__.py
+++ torchaudio/backend/__init__.py
@@ -3,35 +3,6 @@
 # New things should be added to `torchaudio._backend`.
 # Only things related to backward compatibility should be placed here.
 
+from . import common, no_backend, soundfile_backend, sox_io_backend  # noqa
 
-def __getattr__(name: str):
-    if name == "common":
-        from . import _common
-
-        return _common
-
-    if name in ["no_backend", "sox_io_backend", "soundfile_backend"]:
-        import warnings
-
-        warnings.warn(
-            "Torchaudio's I/O functions now support par-call bakcend dispatch. "
-            "Importing backend implementation directly is no longer guaranteed to work. "
-            "Please use `backend` keyword with load/save/info function, instead of "
-            "calling the udnerlying implementation directly.",
-            stacklevel=2,
-        )
-
-        if name == "sox_io_backend":
-            from . import _sox_io_backend
-
-            return _sox_io_backend
-        if name == "soundfile_backend":
-            from torchaudio._backend import soundfile_backend
-
-            return soundfile_backend
-
-        if name == "no_backend":
-            from . import _no_backend
-
-            return _no_backend
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+__all__ = []
