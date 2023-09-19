94aafd83de3c4e6f1b0e511d188afe48f9761806
moto <855818+mthrok@users.noreply.github.com>
2023-09-19 17:09:48 -0400

    Add wall implementation for RIR ray tracing (#3612)
    Extracted from #3604
    
    Add Wall helper class and C++ unit test

--- .github/scripts/unittest-windows/run_test.sh
+++ .github/scripts/unittest-windows/run_test.sh
@@ -11,6 +11,13 @@ source "$this_dir/set_cuda_envs.sh"
 python -m torch.utils.collect_env
 env | grep TORCHAUDIO || true
 
-cd test
-pytest --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
-coverage html
+(
+    cd build/temp*/test/cpp
+    ctest
+)
+
+(
+    cd test
+    pytest --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
+    coverage html
+)
