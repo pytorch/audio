265bc5ca97fbd6618af22c0c8d3915340847fe4c
Zhenyu Tang <roy.james0717@gmail.com>
2024-12-12 21:32:48 -0800

    Fix incorrect audio trimming with negative index
    Differential Revision: D67155973
    
    Pull Request resolved: https://github.com/pytorch/audio/pull/3860

--- src/torchaudio/functional/filtering.py
+++ src/torchaudio/functional/filtering.py
@@ -1665,6 +1665,6 @@ def vad(
     if not has_triggered:
         return waveform[..., :0].view(shape[:-1] + torch.Size([0]))
 
-    res = waveform[:, pos - samplesLen_ns + flushedLen_ns :]
+    res = waveform[:, max(pos - samplesLen_ns + flushedLen_ns, 0) :]
     # unpack batch
     return res.view(shape[:-1] + res.shape[-1:])
