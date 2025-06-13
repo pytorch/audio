7ffe80bae9fc5312afc6c3334717cc2acd906361
Sam Anklesaria <sanklesaria@openteams.com>
2025-06-13 15:06:28 +0000

    Use pytest instead of PytorchTestCase

--- test/torchaudio_unittest/deprecation_test.py
+++ test/torchaudio_unittest/deprecation_test.py
@@ -1,12 +1,14 @@
-from torchaudio_unittest.common_utils import PytorchTestCase
+import pytest
+
 from torchaudio._internal.module_utils import UNSUPPORTED
 
-
-class TestDeprecations(PytorchTestCase):
-    def test_deprecations(self):
-        for func in UNSUPPORTED:
-            with self.assertWarnsRegex(UserWarning, r"deprecated"):
-                try:
-                    func()
-                except:
-                    pass
+@pytest.mark.parametrize("func", UNSUPPORTED)
+def test_deprecations(func):
+    with pytest.warns(UserWarning, match="deprecated"):
+        try:
+            func()
+        except Exception as e:
+	     # Type or Runtime error because we call func() without proper parameters.
+             # The deprecation warning is still properly raised, since it is emitted before
+             # the underlying (deprecated) function is called.
+            assert isinstance(e, (TypeError, RuntimeError))
