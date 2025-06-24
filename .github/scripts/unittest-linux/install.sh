954a23313a804c53238b0d58432f6c44faa891c4
Sam Anklesaria <sanklesaria@openteams.com>
2025-06-24 14:16:55 +0000

    Patch fairseq after checkout

--- .github/scripts/unittest-linux/install.sh
+++ .github/scripts/unittest-linux/install.sh
@@ -103,8 +103,8 @@ fi
 # Install fairseq
 git clone https://github.com/pytorch/fairseq
 cd fairseq
+git checkout e47a4c8
 grep -rl 'np.float' ./ | xargs sed -i 's/np.float/float/g'
 sed -i s/omegaconf\<2.1/omegaconf/ setup.py
 sed -i s/hydra-core\<1.1/hydra-core/ setup.py
-git checkout e47a4c8
 pip install .
