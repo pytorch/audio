663df4f3c31f85c023fd480b4ef79d76ebd5114f
Sam Anklesaria <sanklesaria@openteams.com>
2025-06-24 18:17:07 +0000

    Add quote

--- .github/scripts/unittest-linux/run_test.sh
+++ .github/scripts/unittest-linux/run_test.sh
@@ -31,5 +31,5 @@ fi
 (
     cd test
     export CUBLAS_WORKSPACE_CONFIG=:16:8
-    pytest torchaudio_unittest -k "not backend and not /io/ and not prototype and not sox and not ffmpeg and not fairseq
+    pytest torchaudio_unittest -k "not backend and not /io/ and not prototype and not sox and not ffmpeg and not fairseq"
 )
