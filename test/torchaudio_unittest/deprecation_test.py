b70a16740b7ed39f5e5a4a19c188f5293c43f8ea
Nicolas Hug <contact@nicolas-hug.com>
2025-06-26 14:11:35 +0100

    Make sure deprecated prototypes are tested, add torchscript test

--- test/torchaudio_unittest/deprecation_test.py
+++ test/torchaudio_unittest/deprecation_test.py
@@ -1,6 +1,13 @@
 import pytest
 
+import torch
+
 from torchaudio._internal.module_utils import UNSUPPORTED
+from torchaudio.sox_effects import apply_effects_tensor
+
+# Importing prototype modules is needed to trigger the registration of the
+# corresponding APIs in the UNSUPPORTED register.
+from torchaudio.prototype import datasets, functional, models, pipelines, transforms
 
 @pytest.mark.parametrize("func", UNSUPPORTED)
 def test_deprecations(func):
@@ -8,7 +15,17 @@ def test_deprecations(func):
         try:
             func()
         except Exception as e:
-	     # Type or Runtime error because we call func() without proper parameters.
+	        # Type, Runtime or Value Error  error because we call func() without proper parameters.
              # The deprecation warning is still properly raised, since it is emitted before
              # the underlying (deprecated) function is called.
-            assert isinstance(e, (TypeError, RuntimeError))
+            assert isinstance(e, (TypeError, RuntimeError, ValueError))
+
+
+@pytest.mark.parametrize("scripted", (True, False))
+def test_torchscript_fails(scripted):
+    f = apply_effects_tensor
+    if scripted:
+        pytest.xfail("Deprecation decorator breaks torchscript")
+        f = torch.jit.script(f)
+    _, out_sample_rate = f(torch.rand(2, 1000), sample_rate=16_000, effects=[["rate", "8000"]])
+    assert out_sample_rate == 8000
