d9759edbb09155252dda084e9a385505c3bf990a
Sam Anklesaria <sanklesaria@openteams.com>
2025-06-27 16:09:04 +0000

    Switch deprecation message for extra deprecations

--- src/torchaudio/io/__init__.py
+++ src/torchaudio/io/__init__.py
@@ -1,5 +1,5 @@
 from torio.io import CodecConfig, StreamingMediaDecoder as StreamReader, StreamingMediaEncoder as StreamWriter
-from torchaudio._internal.module_utils import dropping_io_support
+from torchaudio._internal.module_utils import dropping_io_support, dropping_support
 
 from ._effector import AudioEffector
 from ._playback import play_audio as _play_audio
@@ -7,7 +7,7 @@ from ._playback import play_audio as _play_audio
 CodecConfig.__init__ = dropping_io_support(CodecConfig.__init__)
 StreamReader.__init__ = dropping_io_support(StreamReader.__init__)
 StreamWriter.__init__ = dropping_io_support(StreamWriter.__init__)
-AudioEffector.__init__ = dropping_io_support(AudioEffector.__init__)
+AudioEffector.__init__ = dropping_support(AudioEffector.__init__)
 play_audio = dropping_io_support(_play_audio)
 
 
