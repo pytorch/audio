45044bfb19eb0fa659cb14d8d1a373b90d35d1a8
Nicolas Hug <contact@nicolas-hug.com>
2025-07-02 11:27:13 +0100

    Add warnings to more tutorials

--- examples/tutorials/additive_synthesis_tutorial.py
+++ examples/tutorials/additive_synthesis_tutorial.py
@@ -5,6 +5,16 @@ Additive Synthesis
 
 **Author**: `Moto Hira <moto@meta.com>`__
 
+.. warning::
+    Starting with version 2.8, we are refactoring TorchAudio to transition it
+    into a maintenance phase. As a result:
+
+    - The APIs described in this tutorial are deprecated in 2.8 and will be removed in 2.9.
+    - The decoding and encoding capabilities of PyTorch for both audio and video
+      are being consolidated into TorchCodec.
+
+    Please see https://github.com/pytorch/audio/issues/3902 for more information.
+
 This tutorial is the continuation of
 `Oscillator and ADSR Envelope <./oscillator_tutorial.html>`__.
 
--- examples/tutorials/asr_inference_with_cuda_ctc_decoder_tutorial.py
+++ examples/tutorials/asr_inference_with_cuda_ctc_decoder_tutorial.py
@@ -4,6 +4,16 @@ ASR Inference with CUDA CTC Decoder
 
 **Author**: `Yuekai Zhang <yuekaiz@nvidia.com>`__
 
+.. warning::
+    Starting with version 2.8, we are refactoring TorchAudio to transition it
+    into a maintenance phase. As a result:
+
+    - The APIs described in this tutorial are deprecated in 2.8 and will be removed in 2.9.
+    - The decoding and encoding capabilities of PyTorch for both audio and video
+      are being consolidated into TorchCodec.
+
+    Please see https://github.com/pytorch/audio/issues/3902 for more information.
+
 This tutorial shows how to perform speech recognition inference using a
 CUDA-based CTC beam search decoder.
 We demonstrate this on a pretrained
