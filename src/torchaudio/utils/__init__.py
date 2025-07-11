78bbf70ceba8d249fc7acb4003f6e3a5431eb5be
Sam Anklesaria <sanklesaria@openteams.com>
2025-07-11 17:10:23 +0000

    Add options from old loader

--- src/torchaudio/utils/__init__.py
+++ src/torchaudio/utils/__init__.py
@@ -4,14 +4,17 @@ from . import sox_utils
 from .download import download_asset
 import os
 
-def load_torchcodec(file, **args):
+def load_torchcodec(file, normalize=True, channels_first=True, **args):
+    if not normalize:
+        raise Exception("Torchcodec does not support non-normalized file reading")
     from torchcodec.decoders import AudioDecoder
     decoder = AudioDecoder(file)
     if 'start_seconds' in args or 'stop_seconds' in args:
         samples = decoder.get_samples_played_in_range(**args)
     else:
         samples = decoder.get_all_samples()
-    return (samples.data, samples.sample_rate)
+    data = samples.data if channels_first else samples.data.T
+    return (data, samples.sample_rate)
 
 __all__ = [
     "load_torchcodec",
