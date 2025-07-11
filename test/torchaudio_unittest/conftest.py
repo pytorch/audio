1194ff887dcc7d83d0db00aa658a4380358bc6fa
Sam Anklesaria <sanklesaria@openteams.com>
2025-07-11 15:55:42 +0000

    Move failing test ids file to same directory

--- test/torchaudio_unittest/conftest.py
+++ test/torchaudio_unittest/conftest.py
@@ -1,8 +1,10 @@
 import pytest
-import csv
+import os
+
 
 def pytest_collection_modifyitems(config, items):
-    with open('ffmpeg_fail_ids.txt', 'r') as file:
+    fail_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ffmpeg_fail_ids.txt")
+    with open(fail_path, 'r') as file:
         fail_ids = set([f.strip() for f in file.readlines()])
 
     skip_marker = pytest.mark.skip(reason="FFMPEG incompatible with CI runner")
