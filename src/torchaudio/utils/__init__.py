4e83a7aee29d84d17a9ab34a8a8162efa514fadc
Sam Anklesaria <sanklesaria@openteams.com>
2025-07-12 13:19:12 +0000

    Allow keyword arguments to load_torchcodec

--- src/torchaudio/utils/__init__.py
+++ src/torchaudio/utils/__init__.py
@@ -4,18 +4,15 @@ from . import sox_utils
 from .download import download_asset
 import os
 
-def load_torchcodec(file, normalize=True, channels_first=True, **args):
+def load_torchcodec(file, normalize=True, channels_first=True, start_seconds=0.0, stop_seconds=None, **args):
     if not normalize:
         raise Exception("Torchcodec does not support non-normalized file reading")
     try:
         from torchcodec.decoders import AudioDecoder
     except:
          raise Exception("To use this feature, you must install torchcodec. See https://github.com/pytorch/torchcodec for installation instructions")
-    decoder = AudioDecoder(file)
-    if 'start_seconds' in args or 'stop_seconds' in args:
-        samples = decoder.get_samples_played_in_range(**args)
-    else:
-        samples = decoder.get_all_samples()
+    decoder = AudioDecoder(file, **args)
+    samples = decoder.get_samples_played_in_range(start_seconds, stop_seconds)
     data = samples.data if channels_first else samples.data.T
     return (data, samples.sample_rate)
 
