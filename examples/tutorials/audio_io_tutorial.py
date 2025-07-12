9a00ebbd45318f86188af19147eb8779a4779dae
Sam Anklesaria <sanklesaria@openteams.com>
2025-07-12 13:06:16 +0000

    Use urls for load_codec

--- examples/tutorials/audio_io_tutorial.py
+++ examples/tutorials/audio_io_tutorial.py
@@ -290,17 +290,15 @@ frame_offset, num_frames = 16000, 16000  # Fetch and decode the 1 - 2 seconds
 
 url = "https://download.pytorch.org/torchaudio/tutorial-assets/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
 print("Fetching all the data...")
-with requests.get(url, stream=True) as response:
-    waveform1, sample_rate1 = load_torchcodec(_hide_seek(response.raw))
-    waveform1 = waveform1[:, frame_offset : frame_offset + num_frames]
-    print(f" - Fetched {response.raw.tell()} bytes")
+waveform1, sample_rate1 = load_torchcodec(url)
+waveform1 = waveform1[:, frame_offset : frame_offset + num_frames]
+print(f" - Fetched {response.raw.tell()} bytes")
 
 print("Fetching until the requested frames are available...")
-with requests.get(url, stream=True) as response:
-    waveform2, sample_rate2 = load_torchcodec(
-        _hide_seek(response.raw), frame_offset=frame_offset, num_frames=num_frames
-    )
-    print(f" - Fetched {response.raw.tell()} bytes")
+waveform2, sample_rate2 = load_torchcodec(
+    url, frame_offset=frame_offset, num_frames=num_frames
+)
+print(f" - Fetched {response.raw.tell()} bytes")
 
 print("Checking the resulting waveform ... ", end="")
 assert (waveform1 == waveform2).all()
