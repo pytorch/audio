a1dee8578c6c73381f101de91ee8fccf27d4a5d9
Sam Anklesaria <61420+samanklesaria@users.noreply.github.com>
2025-07-28 03:42:55 -0500

    Do git rm -r test/torchaudio_unittest/prototype/ (#3981)
    Co-authored-by: Sam Anklesaria <sanklesaria@openteams.com>

--- test/torchaudio_unittest/prototype/transforms/autograd_cuda_test.py
+++
@@ -1,8 +0,0 @@
-from torchaudio_unittest.common_utils import PytorchTestCase, skipIfNoCuda
-
-from .autograd_test_impl import Autograd
-
-
-@skipIfNoCuda
-class AutogradCUDATest(Autograd, PytorchTestCase):
-    device = "cuda"
