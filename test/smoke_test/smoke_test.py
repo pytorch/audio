89cf2827b75e0e732670449cdd1a266a04fdc736
Sam Anklesaria <61420+samanklesaria@users.noreply.github.com>
2025-08-07 14:17:38 -0500

    Remove deprecated functionality from smoke_test (#4013)
    Co-authored-by: Sam Anklesaria <sanklesaria@openteams.com>

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -6,28 +6,15 @@ import logging
 
 def base_smoke_test():
     import torchaudio  # noqa: F401
-    import torchaudio.compliance.kaldi  # noqa: F401
     import torchaudio.datasets  # noqa: F401
     import torchaudio.functional  # noqa: F401
     import torchaudio.models  # noqa: F401
     import torchaudio.pipelines  # noqa: F401
-    import torchaudio.sox_effects  # noqa: F401
     import torchaudio.transforms  # noqa: F401
     import torchaudio.utils  # noqa: F401
 
-
-def ffmpeg_test():
-    from torchaudio.io import StreamReader  # noqa: F401
-
-
-def _run_smoke_test(check_ffmpeg):
+def _run_smoke_test():
     base_smoke_test()
-
-    if not check_ffmpeg:
-        print("Skipping ffmpeg test.")
-    else:
-        ffmpeg_test()
-
     print("Smoke test passed.")
 
 
@@ -38,13 +25,13 @@ def main(args=None) -> None:
         logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
     _chdir()
-    _run_smoke_test(options.ffmpeg)
+    _run_smoke_test()
 
 
 def _parse_args(args):
     parser = argparse.ArgumentParser()
 
-    # Warning: Please note this option should not be widely used, only use it when absolutely necessary
+    # Warning: Please note this option does nothing and only exists for backward compatibility.
     parser.add_argument("--no-ffmpeg", dest="ffmpeg", action="store_false")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging.")
 
