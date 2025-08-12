bc3de078e5a99d8f733b1e1cbfbdec112b3e0e06
Sam Anklesaria <61420+samanklesaria@users.noreply.github.com>
2025-08-12 14:07:11 -0500

    Remove comparison dependencies and skipIfs (#4031)

--- test/torchaudio_unittest/functional/kaldi_compatibility_test_impl.py
+++ test/torchaudio_unittest/functional/kaldi_compatibility_test_impl.py
@@ -1,6 +1,6 @@
 import torch
 import torchaudio.functional as F
-from torchaudio_unittest.common_utils import skipIfNoExec, TempDirMixin, TestBaseMixin, RequestMixin
+from torchaudio_unittest.common_utils import TempDirMixin, TestBaseMixin, RequestMixin
 from torchaudio_unittest.common_utils.kaldi_utils import convert_args, run_kaldi
 
 
@@ -9,7 +9,6 @@ class Kaldi(TempDirMixin, TestBaseMixin, RequestMixin):
         expected = expected.to(dtype=self.dtype, device=self.device)
         self.assertEqual(output, expected, rtol=rtol, atol=atol)
 
-    @skipIfNoExec("apply-cmvn-sliding")
     def test_sliding_window_cmn(self):
         """sliding_window_cmn should be numerically compatible with apply-cmvn-sliding"""
         kwargs = {
@@ -19,7 +18,8 @@ class Kaldi(TempDirMixin, TestBaseMixin, RequestMixin):
             "norm_vars": False,
         }
 
-        tensor = torch.randn(40, 10, dtype=self.dtype, device=self.device)
+        torch.manual_seed(0)
+        tensor = torch.randn(40, 10, dtype=self.dtype).to(self.device)
         result = F.sliding_window_cmn(tensor, **kwargs)
         command = ["apply-cmvn-sliding"] + convert_args(**kwargs) + ["ark:-", "ark:-"]
         kaldi_result = run_kaldi(self.request, command, "ark", tensor)
