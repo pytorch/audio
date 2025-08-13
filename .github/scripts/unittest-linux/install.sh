1ccac0e2d150fddf924d091aea7409a6bc4b6c08
Sam Anklesaria <61420+samanklesaria@users.noreply.github.com>
2025-08-13 14:49:47 -0500

    Remove unnecessary installations for linux CPU test (#4037)
    * Remove unnecessary installations
    
    * Set CTC Decoder skipping
    
    * Remove pillow install
    
    * Make the test env variable lower case
    
    * Add extra module skips

--- .github/scripts/unittest-linux/install.sh
+++ .github/scripts/unittest-linux/install.sh
@@ -98,14 +98,6 @@ if [[ "$(python --version)" = *3.9* || "$(python --version)" = *3.10* ]]; then
 fi
 (
     set -x
-    conda install -y -c conda-forge ${NUMBA_DEV_CHANNEL} libvorbis parameterized 'requests>=2.20'
-    pip install SoundFile coverage pytest pytest-cov scipy expecttest unidecode inflect Pillow sentencepiece pytorch-lightning 'protobuf<4.21.0' demucs tinytag pyroomacoustics flashlight-text git+https://github.com/kpu/kenlm
-
-    # TODO: might be better to fix the single call to `pip install` above
-    pip install pillow scipy "numpy>=1.26"
+    conda install -y -c conda-forge ${NUMBA_DEV_CHANNEL} parameterized 'requests>=2.20'
+    pip install coverage pytest pytest-cov scipy expecttest scipy "numpy>=1.26"
 )
-# Install fairseq
-git clone https://github.com/pytorch/fairseq
-cd fairseq
-git checkout e47a4c8
-pip install .
