93351a24194727341be4b203f6618c9baadbccc7
Sam Anklesaria <sanklesaria@openteams.com>
2025-08-14 15:58:18 +0000

    Fix bugs in torchcodec mock

--- test/torchcodec/decoders.py
+++ test/torchcodec/decoders.py
@@ -1,17 +1,12 @@
-import test.torchaudio_unittest.common_utils.wav_utils as wav_utils
+import torchaudio_unittest.common_utils.wav_utils as wav_utils
+from types import SimpleNamespace
 
 class AudioDecoder:
     def __init__(self, uri):
         self.uri = uri
+        data, sample_rate = wav_utils.load_wav(self.uri)
+        self.metadata = SimpleNamespace(sample_rate=sample_rate)
+        self.data = data
 
     def get_all_samples(self):
-        return wav_utils.load_wav(self.uri)
-
-
-class AudioEncoder:
-    def __init__(self, data, sample_rate):
-        self.data = data
-        self.sample_rate = sample_rate
-
-    def to_file(self, uri, bit_rate=None):
-        return wav_utils.save_wav(uri, self.data, self.sample_rate)
+        return SimpleNamespace(data=self.data)
--- test/torchcodec/encoders.py
+++ test/torchcodec/encoders.py
@@ -1,10 +1,10 @@
 import torchaudio_unittest.common_utils.wav_utils as wav_utils
+from types import SimpleNamespace
 
 class AudioEncoder:
     def __init__(self, data, sample_rate):
-        print("BEING CALLED")
         self.data = data
-        self.sample_rate = sample_rate
+        self.metadata = SimpleNamespace(sample_rate=sample_rate)
 
     def to_file(self, uri, bit_rate=None):
-        return wav_utils.save_wav(uri, self.data, self.sample_rate)
+        return wav_utils.save_wav(uri, self.data, self.metadata.sample_rate)
