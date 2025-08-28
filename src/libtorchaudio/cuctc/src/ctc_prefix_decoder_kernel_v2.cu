5617d10d74868d54e0bd72720af50142c7422dbb
Nicolas Hug <nicolashug@meta.com>
2025-08-28 08:53:32 +0100

    Use cuda::std::numeric_limits

--- src/libtorchaudio/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
+++ src/libtorchaudio/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
@@ -441,7 +441,7 @@ __launch_bounds__(BLOCK_SIZE) void topk_reduce_and_copy_list_per_batch_kernel(
       topk_values,
       beam,
       items_per_batch,
-      std::numeric_limits<float>::lowest(),
+      cuda::std::numeric_limits<float>::lowest(),
       block_topk_fun,
       set_key_value);
 
