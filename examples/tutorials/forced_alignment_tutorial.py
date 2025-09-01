f935fe50a60dcd04092012d3480820aae5b44f62
gerardo.dominguez.aldama <gerardod@DUT5044PVC.fm.intel.com>
2025-09-01 15:21:39 -0700

    Rolled back changes

--- examples/tutorials/forced_alignment_tutorial.py
+++ examples/tutorials/forced_alignment_tutorial.py
@@ -47,6 +47,7 @@ print(torch.__version__)
 print(torchaudio.__version__)
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
--- examples/tutorials/speech_recognition_pipeline_tutorial.py
+++ examples/tutorials/speech_recognition_pipeline_tutorial.py
@@ -38,6 +38,11 @@ pre-trained models from wav2vec 2.0
 import torch
 import torchaudio
 
+print(torch.__version__)
+print(torchaudio.__version__)
+
+torch.random.manual_seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print(device)
 
