d0f1dbed65eb755f86ecae45c19c7012fe5d295d
Pearu Peterson <pearu.peterson@gmail.com>
2025-09-03 16:38:16 +0300

    Add check path length

--- .github/scripts/unittest-windows/install_conda.bat
+++ .github/scripts/unittest-windows/install_conda.bat
@@ -1,2 +1,1 @@
-dir C:\
-start /wait "" "%miniconda_exe%" /S /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /D=%tmp_conda%
+start /wait "" "%miniconda_exe%" /S /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /CheckPathLength=1 /D=%tmp_conda%
