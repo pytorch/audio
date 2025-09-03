9cc16c7b26419a8a9a21214b1bf035766ce2c6b3
Pearu Peterson <pearu.peterson@gmail.com>
2025-09-03 17:04:23 +0300

    Check if curl is downloading anything

--- .github/scripts/unittest-windows/install_conda.bat
+++ .github/scripts/unittest-windows/install_conda.bat
@@ -1,1 +1,1 @@
-start /wait "" "%miniconda_exe%" /S /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /CheckPathLength=1 /D=%tmp_conda%
+start /wait "" "%miniconda_exe%" /S /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /D=%tmp_conda%
--- .github/scripts/unittest-windows/setup_env.sh
+++ .github/scripts/unittest-windows/setup_env.sh
@@ -21,6 +21,7 @@ if [ ! -d "${conda_dir}" ]; then
     echo "tmp_conda=$tmp_conda"
     export miniconda_exe="$(echo $root_dir | tr '/' '\\')\\miniconda.exe"
     curl --silent --output miniconda.exe https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe -O
+    ls -la $miniconda.exe
     # curl --silent --output miniconda.exe https://repo.anaconda.com/miniconda/Miniconda3-py39_24.11.1-0-Windows-x86_64.exe -O
     "$this_dir/install_conda.bat"
     echo "2: Content of tmp_conda: $(ls ${tmp_conda})"
