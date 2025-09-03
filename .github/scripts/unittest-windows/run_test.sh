140c0663097f6507ea9b80b8be9d83467192da8c
Sam Anklesaria <sanklesaria@openteams.com>
2025-09-03 02:01:42 +0000

    Try alternate conda run commands

--- .github/scripts/unittest-windows/run_test.sh
+++ .github/scripts/unittest-windows/run_test.sh
@@ -2,14 +2,17 @@
 
 set -euxo pipefail
 
-eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
+# eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$this_dir/set_cuda_envs.sh"
 
-conda run -p ./env python -m torch.utils.collect_env
+root_dir="$(git rev-parse --show-toplevel)"
+env_dir="${root_dir}/env"
+
+conda run -p "${env_dir}" python -m torch.utils.collect_env
 env | grep TORCHAUDIO || true
 
 cd test
-conda run -p ./env pytest --continue-on-collection-errors --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
-conda run -p ./env coverage html
+conda run -p "${env_dir}" pytest --continue-on-collection-errors --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
+conda run -p "${env_dir}" coverage html
