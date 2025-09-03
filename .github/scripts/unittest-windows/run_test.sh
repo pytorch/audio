b4194e698e3fe49b3d7ac2f141a8fe2de11f71ea
Sam Anklesaria <sanklesaria@openteams.com>
2025-09-03 04:28:45 +0000

    Disable torchscript tests for windows

--- .github/scripts/unittest-windows/run_test.sh
+++ .github/scripts/unittest-windows/run_test.sh
@@ -14,5 +14,5 @@ conda run -p "${env_dir}" python -m torch.utils.collect_env
 env | grep TORCHAUDIO || true
 
 cd test
-conda run -p "${env_dir}" pytest --continue-on-collection-errors --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest
+conda run -p "${env_dir}" pytest --continue-on-collection-errors --cov=torchaudio --junitxml=${RUNNER_TEST_RESULTS_DIR}/junit.xml -v --durations 20 torchaudio_unittest -k "not torchscript and not fairseq and not demucs"
 conda run -p "${env_dir}" coverage html
