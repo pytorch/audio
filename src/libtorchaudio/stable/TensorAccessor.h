f45375c2883ee7bc195538d57fa779de5ac3c8b9
Pearu Peterson <pearu.peterson@gmail.com>
2025-09-18 19:42:39 +0300

    Apply pre-commit

--- src/libtorchaudio/stable/TensorAccessor.h
+++ src/libtorchaudio/stable/TensorAccessor.h
@@ -27,20 +27,24 @@ struct RestrictPtrTraits {
 };
 #endif
 
-template<typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits, typename index_t = int64_t>
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits,
+    typename index_t = int64_t>
 class TensorAccessorBase {
-public:
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
 
   C10_HOST_DEVICE TensorAccessorBase(
       PtrType data_,
       const index_t* sizes_,
       const index_t* strides_)
-    : data_(data_) /*, sizes_(sizes_), strides_(strides_)*/ {
+      : data_(data_) /*, sizes_(sizes_), strides_(strides_)*/ {
     // Originally, TensorAccessor is a view of sizes and strides as
     // these are ArrayRef instances. Until torch::stable supports
     // ArrayRef-like features, we store copies of sizes and strides:
-    for (auto i=0; i < N; ++i) {
+    for (auto i = 0; i < N; ++i) {
       this->sizes_[i] = sizes_[i];
       this->strides_[i] = strides_[i];
     }
@@ -52,7 +56,8 @@ public:
   C10_HOST_DEVICE const PtrType data() const {
     return data_;
   }
-protected:
+
+ protected:
   PtrType data_;
   /*
     const index_t* sizes_;
@@ -64,48 +69,65 @@ protected:
   index_t strides_[N];
 };
 
-template<typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits, typename index_t = int64_t>
-class TensorAccessor : public TensorAccessorBase<T,N,PtrTraits,index_t> {
-public:
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits,
+    typename index_t = int64_t>
+class TensorAccessor : public TensorAccessorBase<T, N, PtrTraits, index_t> {
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
 
   C10_HOST_DEVICE TensorAccessor(
       PtrType data_,
       const index_t* sizes_,
       const index_t* strides_)
-      : TensorAccessorBase<T, N, PtrTraits, index_t>(data_,sizes_,strides_) {}
+      : TensorAccessorBase<T, N, PtrTraits, index_t>(data_, sizes_, strides_) {}
 
-  C10_HOST_DEVICE TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](index_t i) {
-    return TensorAccessor<T,N-1,PtrTraits,index_t>(this->data_ + this->strides_[0]*i,this->sizes_+1,this->strides_+1);
+  C10_HOST_DEVICE TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](
+      index_t i) {
+    return TensorAccessor<T, N - 1, PtrTraits, index_t>(
+        this->data_ + this->strides_[0] * i,
+        this->sizes_ + 1,
+        this->strides_ + 1);
   }
 
-  C10_HOST_DEVICE const TensorAccessor<T, N-1, PtrTraits, index_t> operator[](index_t i) const {
-    return TensorAccessor<T,N-1,PtrTraits,index_t>(this->data_ + this->strides_[0]*i,this->sizes_+1,this->strides_+1);
+  C10_HOST_DEVICE const TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](
+      index_t i) const {
+    return TensorAccessor<T, N - 1, PtrTraits, index_t>(
+        this->data_ + this->strides_[0] * i,
+        this->sizes_ + 1,
+        this->strides_ + 1);
   }
 };
 
-template<typename T, template <typename U> class PtrTraits, typename index_t>
-class TensorAccessor<T,1,PtrTraits,index_t> : public TensorAccessorBase<T,1,PtrTraits,index_t> {
-public:
+template <typename T, template <typename U> class PtrTraits, typename index_t>
+class TensorAccessor<T, 1, PtrTraits, index_t>
+    : public TensorAccessorBase<T, 1, PtrTraits, index_t> {
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
 
   C10_HOST_DEVICE TensorAccessor(
       PtrType data_,
       const index_t* sizes_,
       const index_t* strides_)
-      : TensorAccessorBase<T, 1, PtrTraits, index_t>(data_,sizes_,strides_) {}
-  C10_HOST_DEVICE T & operator[](index_t i) {
+      : TensorAccessorBase<T, 1, PtrTraits, index_t>(data_, sizes_, strides_) {}
+  C10_HOST_DEVICE T& operator[](index_t i) {
     // NOLINTNEXTLINE(clang-analyzer-core.NullDereference)
-    return this->data_[this->strides_[0]*i];
+    return this->data_[this->strides_[0] * i];
   }
-  C10_HOST_DEVICE const T & operator[](index_t i) const {
-    return this->data_[this->strides_[0]*i];
+  C10_HOST_DEVICE const T& operator[](index_t i) const {
+    return this->data_[this->strides_[0] * i];
   }
 };
 
-template<typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits, typename index_t = int64_t>
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits,
+    typename index_t = int64_t>
 class GenericPackedTensorAccessorBase {
-public:
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
   C10_HOST GenericPackedTensorAccessorBase(
       PtrType data_,
@@ -116,13 +138,15 @@ public:
     std::copy(strides_, strides_ + N, std::begin(this->strides_));
   }
 
-  template <typename source_index_t, class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
+  template <
+      typename source_index_t,
+      class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
   C10_HOST GenericPackedTensorAccessorBase(
       PtrType data_,
       const source_index_t* sizes_,
       const source_index_t* strides_)
       : data_(data_) {
-    for (auto i=0; i < N; ++i) {
+    for (auto i = 0; i < N; ++i) {
       this->sizes_[i] = sizes_[i];
       this->strides_[i] = strides_[i];
     }
@@ -134,7 +158,8 @@ public:
   C10_HOST_DEVICE const PtrType data() const {
     return data_;
   }
-protected:
+
+ protected:
   PtrType data_;
   // NOLINTNEXTLINE(*c-arrays*)
   index_t sizes_[N];
@@ -150,68 +175,101 @@ protected:
   }
 };
 
-template<typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits, typename index_t = int64_t>
-class GenericPackedTensorAccessor : public GenericPackedTensorAccessorBase<T,N,PtrTraits,index_t> {
-public:
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits,
+    typename index_t = int64_t>
+class GenericPackedTensorAccessor
+    : public GenericPackedTensorAccessorBase<T, N, PtrTraits, index_t> {
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
 
   C10_HOST GenericPackedTensorAccessor(
       PtrType data_,
       const index_t* sizes_,
       const index_t* strides_)
-      : GenericPackedTensorAccessorBase<T, N, PtrTraits, index_t>(data_, sizes_, strides_) {}
+      : GenericPackedTensorAccessorBase<T, N, PtrTraits, index_t>(
+            data_,
+            sizes_,
+            strides_) {}
 
   // if index_t is not int64_t, we want to have an int64_t constructor
-  template <typename source_index_t, class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
+  template <
+      typename source_index_t,
+      class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
   C10_HOST GenericPackedTensorAccessor(
       PtrType data_,
       const source_index_t* sizes_,
       const source_index_t* strides_)
-      : GenericPackedTensorAccessorBase<T, N, PtrTraits, index_t>(data_, sizes_, strides_) {}
+      : GenericPackedTensorAccessorBase<T, N, PtrTraits, index_t>(
+            data_,
+            sizes_,
+            strides_) {}
 
-  C10_DEVICE TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](index_t i) {
+  C10_DEVICE TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](
+      index_t i) {
     index_t* new_sizes = this->sizes_ + 1;
     index_t* new_strides = this->strides_ + 1;
-    return TensorAccessor<T,N-1,PtrTraits,index_t>(this->data_ + this->strides_[0]*i, new_sizes, new_strides);
+    return TensorAccessor<T, N - 1, PtrTraits, index_t>(
+        this->data_ + this->strides_[0] * i, new_sizes, new_strides);
   }
 
-  C10_DEVICE const TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](index_t i) const {
+  C10_DEVICE const TensorAccessor<T, N - 1, PtrTraits, index_t> operator[](
+      index_t i) const {
     const index_t* new_sizes = this->sizes_ + 1;
     const index_t* new_strides = this->strides_ + 1;
-    return TensorAccessor<T,N-1,PtrTraits,index_t>(this->data_ + this->strides_[0]*i, new_sizes, new_strides);
+    return TensorAccessor<T, N - 1, PtrTraits, index_t>(
+        this->data_ + this->strides_[0] * i, new_sizes, new_strides);
   }
 };
 
-template<typename T, template <typename U> class PtrTraits, typename index_t>
-class GenericPackedTensorAccessor<T,1,PtrTraits,index_t> : public GenericPackedTensorAccessorBase<T,1,PtrTraits,index_t> {
-public:
+template <typename T, template <typename U> class PtrTraits, typename index_t>
+class GenericPackedTensorAccessor<T, 1, PtrTraits, index_t>
+    : public GenericPackedTensorAccessorBase<T, 1, PtrTraits, index_t> {
+ public:
   typedef typename PtrTraits<T>::PtrType PtrType;
   C10_HOST GenericPackedTensorAccessor(
       PtrType data_,
       const index_t* sizes_,
       const index_t* strides_)
-      : GenericPackedTensorAccessorBase<T, 1, PtrTraits, index_t>(data_, sizes_, strides_) {}
+      : GenericPackedTensorAccessorBase<T, 1, PtrTraits, index_t>(
+            data_,
+            sizes_,
+            strides_) {}
 
-  template <typename source_index_t, class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
+  template <
+      typename source_index_t,
+      class = std::enable_if_t<std::is_same_v<source_index_t, int64_t>>>
   C10_HOST GenericPackedTensorAccessor(
       PtrType data_,
       const source_index_t* sizes_,
       const source_index_t* strides_)
-      : GenericPackedTensorAccessorBase<T, 1, PtrTraits, index_t>(data_, sizes_, strides_) {}
+      : GenericPackedTensorAccessorBase<T, 1, PtrTraits, index_t>(
+            data_,
+            sizes_,
+            strides_) {}
 
-  C10_DEVICE T & operator[](index_t i) {
+  C10_DEVICE T& operator[](index_t i) {
     return this->data_[this->strides_[0] * i];
   }
   C10_DEVICE const T& operator[](index_t i) const {
-    return this->data_[this->strides_[0]*i];
+    return this->data_[this->strides_[0] * i];
   }
-
 };
 
-template <typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits>
-using PackedTensorAccessor32 = GenericPackedTensorAccessor<T, N, PtrTraits, int32_t>;
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits>
+using PackedTensorAccessor32 =
+    GenericPackedTensorAccessor<T, N, PtrTraits, int32_t>;
 
-template <typename T, size_t N, template <typename U> class PtrTraits = DefaultPtrTraits>
-using PackedTensorAccessor64 = GenericPackedTensorAccessor<T, N, PtrTraits, int64_t>;
+template <
+    typename T,
+    size_t N,
+    template <typename U> class PtrTraits = DefaultPtrTraits>
+using PackedTensorAccessor64 =
+    GenericPackedTensorAccessor<T, N, PtrTraits, int64_t>;
 
-}  // namespace torchaudio::stable
+} // namespace torchaudio::stable
--- src/libtorchaudio/stable/dispatch.h
+++ src/libtorchaudio/stable/dispatch.h
@@ -15,8 +15,8 @@
   features implemented here.
 */
 
-#include <torch/headeronly/util/Exception.h>
 #include <torch/headeronly/core/ScalarType.h>
+#include <torch/headeronly/util/Exception.h>
 
 namespace torchaudio::stable {
 
@@ -27,49 +27,50 @@ namespace impl {
 template <ScalarType N>
 struct ScalarTypeToCPPType;
 
-#define SPECIALIZE_ScalarTypeToCPPType(cpp_type, scalar_type)   \
-  template <>                                                \
-  struct ScalarTypeToCPPType<ScalarType::scalar_type> { \
-  using type = cpp_type;                                     \
+#define SPECIALIZE_ScalarTypeToCPPType(cpp_type, scalar_type) \
+  template <>                                                 \
+  struct ScalarTypeToCPPType<ScalarType::scalar_type> {       \
+    using type = cpp_type;                                    \
   };
 
-  AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(SPECIALIZE_ScalarTypeToCPPType)
+AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(SPECIALIZE_ScalarTypeToCPPType)
 
 #undef SPECIALIZE_ScalarTypeToCPPType
 
 template <ScalarType N>
 using ScalarTypeToCPPTypeT = typename ScalarTypeToCPPType<N>::type;
-  
-}  // namespace impl
 
-}  // namespace torchaudio::stable
-  
-#define STABLE_DISPATCH_CASE(enum_type, ...) \
-  case enum_type: {                                                           \
-    using scalar_t [[maybe_unused]] = torchaudio::stable::impl::ScalarTypeToCPPTypeT<enum_type>; \
-    return __VA_ARGS__();                                                     \
+} // namespace impl
+
+} // namespace torchaudio::stable
+
+#define STABLE_DISPATCH_CASE(enum_type, ...)                       \
+  case enum_type: {                                                \
+    using scalar_t [[maybe_unused]] =                              \
+        torchaudio::stable::impl::ScalarTypeToCPPTypeT<enum_type>; \
+    return __VA_ARGS__();                                          \
   }
 
-#define STABLE_DISPATCH_SWITCH(TYPE, NAME, ...)                         \
-  [&] {                                                                 \
-    const auto& the_type = TYPE;                                        \
-    constexpr const char* at_dispatch_name = NAME;                      \
-    switch (the_type) {                                                 \
-      __VA_ARGS__                                                       \
-    default:                                                            \
-      STD_TORCH_CHECK(                                                  \
-            false,                                                          \
-            '"',                                                            \
-            at_dispatch_name,                                               \
-            "\" not implemented for '",                                     \
-            toString(the_type),                                                  \
-            "'");                                                           \
-    }                                                                       \
+#define STABLE_DISPATCH_SWITCH(TYPE, NAME, ...)    \
+  [&] {                                            \
+    const auto& the_type = TYPE;                   \
+    constexpr const char* at_dispatch_name = NAME; \
+    switch (the_type) {                            \
+      __VA_ARGS__                                  \
+      default:                                     \
+        STD_TORCH_CHECK(                           \
+            false,                                 \
+            '"',                                   \
+            at_dispatch_name,                      \
+            "\" not implemented for '",            \
+            toString(the_type),                    \
+            "'");                                  \
+    }                                              \
   }()
 
-#define STABLE_DISPATCH_CASE_FLOATING_TYPES_AND_HALF(...)   \
-  STABLE_DISPATCH_CASE(ScalarType::Double, __VA_ARGS__)        \
-  STABLE_DISPATCH_CASE(ScalarType::Float, __VA_ARGS__)  \
+#define STABLE_DISPATCH_CASE_FLOATING_TYPES_AND_HALF(...) \
+  STABLE_DISPATCH_CASE(ScalarType::Double, __VA_ARGS__)   \
+  STABLE_DISPATCH_CASE(ScalarType::Float, __VA_ARGS__)    \
   STABLE_DISPATCH_CASE(ScalarType::Half, __VA_ARGS__)
 
 #define STABLE_DISPATCH_FLOATING_TYPES_AND_HALF(TYPE, NAME, ...) \
