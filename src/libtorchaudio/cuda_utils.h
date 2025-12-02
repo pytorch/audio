b7ff02d66858b53d32171988c4711c717a1f1eb3
Pearu Peterson <pearu.peterson@gmail.com>
2025-12-02 21:02:08 +0200

    Add temporary shim functions.

--- src/libtorchaudio/cuda_utils.h
+++ src/libtorchaudio/cuda_utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <libtorchaudio/shim_temporary.h>
 #include <torch/csrc/stable/c/shim.h>
 #include <torch/csrc/stable/device.h>
 
@@ -20,15 +21,15 @@ inline cudaStream_t getCurrentCUDAStream(
 inline void setCurrentCUDAStream(
     cudaStream_t stream,
     torch::stable::DeviceIndex device_index = -1) {
-  TORCH_ERROR_CODE_CHECK(
-      torch_set_current_cuda_stream(static_cast<void*>(stream), device_index));
+  TORCH_ERROR_CODE_CHECK(tmp_torch_set_current_cuda_stream(
+      static_cast<void*>(stream), device_index));
 }
 
 inline cudaStream_t getStreamFromPool(
     const bool isHighPriority = false,
     torch::stable::DeviceIndex device_index = -1) {
   void* stream_ptr = nullptr;
-  TORCH_ERROR_CODE_CHECK(torch_get_cuda_stream_from_pool(
+  TORCH_ERROR_CODE_CHECK(tmp_torch_get_cuda_stream_from_pool(
       isHighPriority, device_index, &stream_ptr));
   return static_cast<cudaStream_t>(stream_ptr);
 }
@@ -36,8 +37,8 @@ inline cudaStream_t getStreamFromPool(
 inline void synchronize(
     cudaStream_t stream,
     torch::stable::DeviceIndex device_index = -1) {
-  TORCH_ERROR_CODE_CHECK(
-      torch_cuda_stream_synchronize(static_cast<void*>(stream), device_index));
+  TORCH_ERROR_CODE_CHECK(tmp_torch_cuda_stream_synchronize(
+      static_cast<void*>(stream), device_index));
 }
 
 } // namespace libtorchaudio::cuda
