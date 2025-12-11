6cfd191423cd57af123da7ba76a14c218855003a
Pearu Peterson <pearu.peterson@gmail.com>
2025-12-11 14:00:15 +0200

    Add multi-GPU support to forced_align

--- test/torchaudio_unittest/common_utils/case_utils.py
+++ test/torchaudio_unittest/common_utils/case_utils.py
@@ -249,6 +249,11 @@ disabledInCI = _skipIf(
     reason="Tests are failing on CI consistently. Disabled while investigating.",
     key="TEMPORARY_DISABLED",
 )
+skipIfSingleCuda = _skipIf(
+    not (torch.cuda.is_available() and torch.cuda.device_count() > 1),
+    reason="CUDA is not available or not a multi-GPU platform",
+    key="NO_MULTIGPU_CUDA",
+)
 
 
 def skipIfNoHWAccel(name):
